532c1543a3a972f1ec936f523d95ad2fd7f15668
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-03-16 04:01:34 +0000

    under APache license until not

--- trunk/spice/components/cli/src/java/org/realityforge/cli/CLOptionDescriptor.java
+++ trunk/spice/components/cli/src/java/org/realityforge/cli/CLOptionDescriptor.java
@@ -4,6 +4,10 @@
  * This software is published under the terms of the Apache Software License
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE.txt file.
+ *
+ * Portions of this software are based upon software originally
+ * developed as part of the Apache Avalon project under
+ * the Apache 1.1 License.
  */
 package org.realityforge.cli;
 
@@ -13,7 +17,7 @@ package org.realityforge.cli;
  * and passes it to {@link CLArgsParser#CLArgsParser(String[], CLOptionDescriptor[])}.
  *
  * @author <a href="mailto:peter at apache.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-03-01 07:06:31 $
+ * @version $Revision: 1.2 $ $Date: 2003-03-16 04:01:34 $
  * @since 4.0
  * @see CLArgsParser
  * @see CLUtil
