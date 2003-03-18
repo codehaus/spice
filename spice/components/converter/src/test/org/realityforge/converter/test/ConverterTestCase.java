5f39ebe4765c020fd2ff2e31c4ad84d5b275be45
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-03-18 09:14:24 +0000

    license

--- spice/components/converter/src/test/org/realityforge/converter/test/ConverterTestCase.java
+++ spice/components/converter/src/test/org/realityforge/converter/test/ConverterTestCase.java
@@ -1,9 +1,12 @@
 /*
- * Copyright  The Apache Software Foundation. All rights reserved.
+ * Copyright (C) The Spice Group. All rights reserved.
  *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE.txt file.
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ *
+ * This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/).
  */
 package org.realityforge.converter.test;
 
