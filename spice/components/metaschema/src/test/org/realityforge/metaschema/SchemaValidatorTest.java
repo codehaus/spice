529e2ba99f9197a2c4c0c1bc092e31eebbe29892
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-04-06 03:56:40 +0000

    add license, style and optimize imports

--- spice/components/metaschema/src/test/org/realityforge/metaschema/SchemaValidatorTest.java
+++ spice/components/metaschema/src/test/org/realityforge/metaschema/SchemaValidatorTest.java
@@ -1,20 +1,24 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
 package org.realityforge.metaschema;
 
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.Vector;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
-import org.realityforge.metaschema.*;
-import org.exteca.ontology.*;
-
 public class SchemaValidatorTest
     extends TestCase
 {
-	private static SchemaValidator validator = new SchemaValidator();
+    private static SchemaValidator validator = new SchemaValidator();
+
     public SchemaValidatorTest( final String name )
     {
         super( name );
@@ -35,36 +39,45 @@ public class SchemaValidatorTest
 
     public void testReadSchema()
     {
-		ClassLoader cl = SchemaValidator.class.getClassLoader(); 
-		InputStream is = cl.getResourceAsStream("etc/schema.xml");
-    	try{
-	    	validator.readSchema(is);
-    	} catch ( InvalidMetaDataException e ){
+        ClassLoader cl = SchemaValidator.class.getClassLoader();
+        InputStream is = cl.getResourceAsStream( "etc/schema.xml" );
+        try
+        {
+            validator.readSchema( is );
+        }
+        catch( InvalidMetaDataException e )
+        {
             e.printStackTrace();
             fail( e.getMessage() );
-    	}
+        }
     }
 
     public void testValidateAttributeByValue()
     {
-    	try{
-	    	validator.validateAttribute( "class/interface", "param", "param1 this is a parameter");
-    	} catch ( InvalidMetaDataException e ){
+        try
+        {
+            validator.validateAttribute( "class/interface", "param", "param1 this is a parameter" );
+        }
+        catch( InvalidMetaDataException e )
+        {
             e.printStackTrace();
             fail( e.getMessage() );
-    	}
+        }
     }
 
     public void testValidateAttributeByProperties()
     {
-    	try{
-    		Properties p = new Properties();
-    		p.setProperty( "type", "Foo" );
-    		p.setProperty( "context", "Boo" );
-	    	validator.validateAttribute( "class/interface", "avalon-service", p);
-    	} catch ( InvalidMetaDataException e ){
+        try
+        {
+            Properties p = new Properties();
+            p.setProperty( "type", "Foo" );
+            p.setProperty( "context", "Boo" );
+            validator.validateAttribute( "class/interface", "avalon-service", p );
+        }
+        catch( InvalidMetaDataException e )
+        {
             e.printStackTrace();
             fail( e.getMessage() );
-    	}
+        }
     }
 }
