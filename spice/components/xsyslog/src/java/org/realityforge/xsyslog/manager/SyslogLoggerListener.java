5e1f105b7c1d06488239e1c3b23e2c1bf9f6fcf5
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-04-16 10:56:27 +0000

    Interceptor to route messages to manager

---
+++ spice/components/xsyslog/src/java/org/realityforge/xsyslog/manager/SyslogLoggerListener.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.xsyslog.manager;
+
+import org.apache.log.util.LoggerListener;
+import org.apache.log.Logger;
+
+/**
+ * This class intercepts creation events from logger framework.
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-04-16 10:56:27 $
+ */
+class SyslogLoggerListener
+    extends LoggerListener
+{
+    /**
+     * The SyslogManager listener is associated with.
+     */
+    private final SyslogManager m_syslogManager;
+
+    /**
+     * Create adapter for specified SyslogManager.
+     *
+     * @param syslogManager the SyslogManager
+     */
+    public SyslogLoggerListener( final SyslogManager syslogManager )
+    {
+        if( null == syslogManager )
+        {
+            throw new NullPointerException( "syslogManager" );
+        }
+        m_syslogManager = syslogManager;
+    }
+
+    /**
+     * Route message via SyslogManager.
+     *
+     * @param channel the newly created channel name
+     * @param logger the logger
+     */
+    public void loggerCreated( final String channel,
+                               final Logger logger )
+    {
+        m_syslogManager.loggerCreated( channel, logger );
+    }
+}
