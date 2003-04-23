5f784077b8b77f21e71b0a00106d5622224b9398
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-04-23 01:32:18 +0000

    Add in server socket factory

---
+++ trunk/spice/components/netserve/src/java/org/realityforge/netserve/sockets/ServerSocketFactory.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.netserve.sockets;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+
+/**
+ * Service used to create server sockets. The factory is used so that
+ * the exact socket type and underlying transport is abstracted. The
+ * sockets created could be proxied, SSL enabled, TLS enabled etc.
+ * However clients just care that they return sockets.
+ *
+ * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-04-23 01:32:17 $
+ */
+public interface ServerSocketFactory
+{
+    /**
+     * Creates a socket on specified port.
+     *
+     * @param port the port (0 indicates any available port)
+     * @return the created ServerSocket
+     * @throws IOException if unable to create socket
+     */
+    ServerSocket createServerSocket( int port )
+        throws IOException;
+
+    /**
+     * Creates a socket on specified port with a specified backlog.
+     *
+     * @param port the port (0 indicates any available port)
+     * @param backlog the backlog
+     * @return the created ServerSocket
+     * @throws IOException if unable to create socket
+     */
+    ServerSocket createServerSocket( int port, int backlog )
+        throws IOException;
+
+    /**
+     * Creates a socket on a particular network interface on specified port
+     * with a specified backlog.
+     *
+     * @param port the port (0 indicates any available port)
+     * @param backlog the backlog
+     * @param address the network interface to bind to.
+     * @return the created ServerSocket
+     * @throws IOException if unable to create socket
+     */
+    ServerSocket createServerSocket( int port, int backlog, InetAddress address )
+        throws IOException;
+}
+
