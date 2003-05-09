c3b8942c6a0ea710a8755a808345ed6354d77fdd
maurotalevi <maurotalevi@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-05-09 21:35:02 +0000

    Reformatted

--- spice/components/loggerstore/src/java/org/realityforge/loggerstore/AbstractLoggerStore.java
+++ spice/components/loggerstore/src/java/org/realityforge/loggerstore/AbstractLoggerStore.java
@@ -14,103 +14,103 @@ import org.apache.avalon.framework.logger.LogEnabled;
 import org.apache.avalon.framework.logger.Logger;
 
 /**
- * AbstractLoggerStore is an abstract implementation of LoggerStore for the 
- * functionality common to all Loggers.  
+ * AbstractLoggerStore is an abstract implementation of LoggerStore for the
+ * functionality common to all Loggers.
  *
  * @author <a href="mailto:mauro.talevi at aquilonia.org">Mauro Talevi</a>
  */
-public abstract class AbstractLoggerStore
+public abstract class AbstractLoggerStore 
     implements LoggerStore, LogEnabled
 {
     /** Map of Loggers held in the store */
-    protected final Map m_loggers = new HashMap();
-
+    protected final Map m_loggers = new HashMap ();
+    
     /** The Logger used by LogEnabled. */
     private Logger m_logger;
-
+    
     /** The root Logger */
     private Logger m_rootLogger;
-  
+    
     /**
      * Provide a logger.
      *
      * @param logger the logger
      */
-    public void enableLogging( final Logger logger )
+    public void enableLogging ( final Logger logger )
     {
         m_logger = logger;
     }
-
-	/**
-	 * Retrieves the root Logger from the store.
-	 * @return the Logger 
-	 * @throws Exception if unable to retrieve Logger
-	 */
-	public Logger getLogger( )
-		throws Exception
-	{
-		return getRootLogger();
-	}
-
-	/**
-	 * Retrieves a Logger hierarchy from the store for a given category name.
-	 * @param categoryName the name of the logger category.  
-	 * @return the Logger 
-	 * @throws Exception if unable to retrieve Logger
-	 */
-	public Logger getLogger( final String categoryName )
-		throws Exception
+    
+    /**
+     * Retrieves the root Logger from the store.
+     * @return the Logger
+     * @throws Exception if unable to retrieve Logger
+     */
+    public Logger getLogger ( )
+        throws Exception
     {
-		if ( categoryName == null )
-		{
-			final String message = "categoryName cannot be null.  Use getLogger() for root Logger";
-			throw new Exception( message );
-		}
-        Logger logger = retrieveLogger( categoryName );
+        return getRootLogger ();
+    }
+    
+    /**
+     * Retrieves a Logger hierarchy from the store for a given category name.
+     * @param categoryName the name of the logger category.
+     * @return the Logger
+     * @throws Exception if unable to retrieve Logger
+     */
+    public Logger getLogger ( final String categoryName )
+        throws Exception
+    {
+        if ( categoryName == null )
+        {
+            final String message = "categoryName cannot be null.  Use getLogger() for root Logger";
+            throw new Exception ( message );
+        }
+        Logger logger = retrieveLogger ( categoryName );
         if ( logger == null )
         {
-            if( m_logger != null 
-             && m_logger.isDebugEnabled() )
+            if( m_logger != null
+            && m_logger.isDebugEnabled () )
             {
                 final String message = "Logger for category " + categoryName +
-                    " not defined in configuration. New Logger created and returned";
-                m_logger.debug( message );
+                " not defined in configuration. New Logger created and returned";
+                m_logger.debug ( message );
             }
-            logger = createLogger( categoryName );
-            storeLogger( categoryName, logger);
+            logger = createLogger ( categoryName );
+            storeLogger ( categoryName, logger);
         }
         return logger;
     }
     
-    /** 
-     *  Creates new Logger for the given category.  
-     *  This is logger-implementation specific and will be implemented in 
+    /**
+     *  Creates new Logger for the given category.
+     *  This is logger-implementation specific and will be implemented in
      *  concrete subclasses.
      */
-    protected abstract Logger createLogger( String categoryName );
+    protected abstract Logger createLogger ( String categoryName );
     
-   /**
+    /**
      * Sets the root Logger.
      */
-    protected void setRootLogger(Logger rootLogger)
+    protected void setRootLogger (Logger rootLogger)
     {
         m_rootLogger = rootLogger;
     }
     
-   /**
+    /**
      * Returns the root Logger.
      */
-    private Logger getRootLogger()  throws Exception
+    private Logger getRootLogger ()  throws Exception
     {
-        if( m_logger != null && m_logger.isDebugEnabled() )
+        if( m_logger != null && m_logger.isDebugEnabled () )
         {
             final String message = "Root Logger returned";
-            m_logger.debug( message );
+            m_logger.debug ( message );
         }
         if ( m_rootLogger == null )
         {
-        	final String message = "Root Logger is not defined";
-        	throw new Exception( message );
+            final String message = "Root Logger is not defined";
+            throw new Exception ( message );
         }
         return m_rootLogger;
     }
@@ -119,18 +119,18 @@ public abstract class AbstractLoggerStore
      *  Retrieve Logger from store map.
      *  @param categoryName the category of the Logger
      *  @return the Logger instance or <code>null</code> if not found in map.
-     */ 
-    private Logger retrieveLogger( final String categoryName )
+     */
+    private Logger retrieveLogger ( final String categoryName )
     {
-        Logger logger = (Logger)m_loggers.get( categoryName );
-
+        Logger logger = (Logger)m_loggers.get ( categoryName );
+        
         if( null != logger )
         {
-            if( m_logger.isDebugEnabled() )
+            if( m_logger.isDebugEnabled () )
             {
-                final String message = "Logger for category " + categoryName + 
-                    " retrieved";
-                m_logger.debug( message );
+                final String message = "Logger for category " + categoryName +
+                " retrieved";
+                m_logger.debug ( message );
             }
         }
         
@@ -141,11 +141,10 @@ public abstract class AbstractLoggerStore
      *  Stores Logger in map.
      *  @param categoryName the category of the Logger
      *  @param logger the Logger instance
-     */ 
-    private void storeLogger( final String categoryName,    
-                                         final Logger logger )
+     */
+    private void storeLogger ( final String categoryName, final Logger logger )
     {
-        m_loggers.put( categoryName, logger );
+        m_loggers.put ( categoryName, logger );
     }
-
+    
 }
