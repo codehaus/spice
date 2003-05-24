9af50e26d877c41aac0e2eec3dc7052c2f1c1d7a
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-05-24 23:46:43 +0000

    Readd converter

---
+++ spice/components/converter/src/java/org/realityforge/converter/ConverterFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.converter;
+
+public interface ConverterFactory
+{
+    Converter createConverter() throws Exception;
+}
