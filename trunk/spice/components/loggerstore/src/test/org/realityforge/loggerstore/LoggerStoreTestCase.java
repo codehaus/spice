a875967a5a3e91681cb0a404441ccc055192cbad
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-05-25 12:24:02 +0000

    Test the factorys

--- trunk/spice/components/loggerstore/src/test/org/realityforge/loggerstore/LoggerStoreTestCase.java
+++ trunk/spice/components/loggerstore/src/test/org/realityforge/loggerstore/LoggerStoreTestCase.java
@@ -13,11 +13,14 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Properties;
+import java.util.HashMap;
+import java.net.URL;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
 import junit.framework.TestCase;
 import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
+import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.container.ContainerUtil;
 import org.apache.avalon.framework.logger.ConsoleLogger;
 import org.apache.avalon.framework.logger.Logger;
@@ -81,6 +84,36 @@ public class LoggerStoreTestCase
         performConsoleTest( store, ConsoleLogger.LEVEL_DISABLED );
     }
 
+    public void testJDK14LoggerStoreFactoryInvalidInput()
+        throws Exception
+    {
+        runInvalidInputData( new Jdk14LoggerStoreFactory() );
+    }
+
+    public void testJDK14LoggerStoreFactoryWithProperties()
+        throws Exception
+    {
+        final Properties properties = new Properties();
+        properties.load( getResource( "logging.properties" ) );
+        final HashMap config = new HashMap();
+        config.put( Properties.class.getName(), properties );
+
+        runFactoryTest( new Jdk14LoggerStoreFactory(),
+                        ConsoleLogger.LEVEL_DEBUG,
+                        config,
+                        "jdk14" );
+    }
+
+    public void testJDK14LoggerStoreFactoryWithStreams()
+        throws Exception
+    {
+        runStreamBasedFactoryTest( "logging.properties",
+                                   new Jdk14LoggerStoreFactory(),
+                                   ConsoleLogger.LEVEL_DEBUG,
+                                   "jdk14",
+                                   new HashMap() );
+    }
+
     public void testJDK14Configuration()
         throws Exception
     {
@@ -103,6 +136,35 @@ public class LoggerStoreTestCase
         final LoggerStore store =
             new Jdk14LoggerStore( getResource( "logging.properties" ) );
         runLoggerTest( "jdk14", store );
+    }
+
+    public void testLogKitLoggerStoreFactoryInvalidInput()
+        throws Exception
+    {
+        runInvalidInputData( new LogKitLoggerStoreFactory() );
+    }
+
+    public void testLogKitLoggerStoreFactoryWithConfiguration()
+        throws Exception
+    {
+        final DefaultConfigurationBuilder builder = new DefaultConfigurationBuilder();
+        final HashMap config = new HashMap();
+        config.put( Configuration.class.getName(), builder.build( getResource( "logkit.xml" ) ) );
+
+        runFactoryTest( new LogKitLoggerStoreFactory(),
+                        ConsoleLogger.LEVEL_DEBUG,
+                        config,
+                        "logkit" );
+    }
+
+    public void testLogKitLoggerStoreFactoryWithStreams()
+        throws Exception
+    {
+        runStreamBasedFactoryTest( "logkit.xml",
+                                   new LogKitLoggerStoreFactory(),
+                                   ConsoleLogger.LEVEL_DEBUG,
+                                   "logkit",
+                                   new HashMap() );
     }
 
     public void testLogKitConfiguration()
@@ -130,6 +192,65 @@ public class LoggerStoreTestCase
         final LoggerStore store =
             new LogKitLoggerStore( builder.build( getResource( "logkit.xml" ) ) );
         runLoggerTest( "logkit", store );
+    }
+
+    public void testLog4jLoggerStoreFactoryInvalidInput()
+        throws Exception
+    {
+        runInvalidInputData( new Log4JLoggerStoreFactory() );
+    }
+
+    public void testLog4jLoggerStoreFactoryWithProperties()
+        throws Exception
+    {
+        final Properties properties = new Properties();
+        properties.load( getResource( "log4j.properties" ) );
+        final HashMap config = new HashMap();
+        config.put( Properties.class.getName(), properties );
+
+        runFactoryTest( new Log4JLoggerStoreFactory(),
+                        ConsoleLogger.LEVEL_DEBUG,
+                        config,
+                        "log4j-properties" );
+    }
+
+    public void testLog4jLoggerStoreFactoryWithElement()
+        throws Exception
+    {
+        final HashMap config = new HashMap();
+        final Element element =
+            buildElement( getResource( "log4j.xml" ),
+                          new org.apache.log4j.xml.Log4jEntityResolver(),
+                          null );
+        config.put( Element.class.getName(), element );
+
+        runFactoryTest( new Log4JLoggerStoreFactory(),
+                        ConsoleLogger.LEVEL_DEBUG,
+                        config,
+                        "log4j-xml" );
+    }
+
+    public void testLog4JLoggerStoreFactoryWithStreamsAsXML()
+        throws Exception
+    {
+        runStreamBasedFactoryTest( "log4j.xml",
+                                   new Log4JLoggerStoreFactory(),
+                                   ConsoleLogger.LEVEL_DEBUG,
+                                   "log4j-xml",
+                                   new HashMap() );
+    }
+
+    public void testLog4JLoggerStoreFactoryWithStreamsAsProperties()
+        throws Exception
+    {
+        final HashMap inputData = new HashMap();
+        inputData.put( Log4JLoggerStoreFactory.CONFIGURATION_TYPE,
+                       Log4JLoggerStoreFactory.PROPERTIES );
+        runStreamBasedFactoryTest( "log4j.properties",
+                                   new Log4JLoggerStoreFactory(),
+                                   ConsoleLogger.LEVEL_DEBUG,
+                                   "log4j-properties",
+                                   inputData );
     }
 
     public void testLog4JElementConfiguration()
@@ -267,6 +388,7 @@ public class LoggerStoreTestCase
             assertNull( "Third Line Contents for logger" + filename,
                         reader.readLine() );
             reader.close();
+            logFile.delete();
 
             if( !( store instanceof Jdk14LoggerStore ) )
             {
@@ -279,6 +401,8 @@ public class LoggerStoreTestCase
                               MESSAGE, reader.readLine() );
                 assertNull( "Second Line Contents for nejney logger" + filename,
                             reader.readLine() );
+                reader.close();
+                logFile2.delete();
             }
         }
         finally
@@ -348,5 +472,75 @@ public class LoggerStoreTestCase
         assertNotNull( "noExistLogger for console", noExistLogger );
         noExistLogger.info( MESSAGE2 );
         store.close();
+    }
+
+    private void runInvalidInputData( final LoggerStoreFactory factory )
+    {
+        try
+        {
+            factory.createLoggerStore( new HashMap() );
+            fail( "Expected createLoggerStore to fail with invalid input data" );
+        }
+        catch( Exception e )
+        {
+        }
+    }
+
+    private void runStreamBasedFactoryTest( final String inputFile,
+                                            final LoggerStoreFactory factory,
+                                            final int level,
+                                            final String outputFile,
+                                            final HashMap inputData )
+        throws Exception
+    {
+        //URL Should in file: format
+        final URL url = getClass().getResource( inputFile );
+        assertEquals( "URL is of file type", url.getProtocol(), "file" );
+
+        final HashMap config = new HashMap();
+        config.put( LoggerStoreFactory.URL_LOCATION,
+                    url.toExternalForm() );
+        config.putAll( inputData );
+        runFactoryTest( factory,
+                        level,
+                        config,
+                        outputFile );
+        final HashMap config2 = new HashMap();
+        config2.put( URL.class.getName(), url );
+        config2.putAll( inputData );
+        runFactoryTest( factory,
+                        level,
+                        config2,
+                        outputFile );
+        final String filename = url.toExternalForm().substring( 5 );
+        final HashMap config3 = new HashMap();
+        config3.put( LoggerStoreFactory.FILE_LOCATION, filename );
+        config3.putAll( inputData );
+        runFactoryTest( factory,
+                        level,
+                        config3,
+                        outputFile );
+        final HashMap config4 = new HashMap();
+        config4.put( File.class.getName(), new File( filename ) );
+        config4.putAll( inputData );
+        runFactoryTest( factory,
+                        level,
+                        config4,
+                        outputFile );
+        final HashMap config5 = new HashMap();
+        config5.put( InputStream.class.getName(),
+                     new FileInputStream( filename ) );
+        config5.putAll( inputData );
+        runFactoryTest( factory,
+                        level,
+                        config5,
+                        outputFile );
+    }
+
+    private void runFactoryTest( final LoggerStoreFactory factory, final int level, final HashMap config, final String filename ) throws Exception
+    {
+        ContainerUtil.enableLogging( factory, new ConsoleLogger( level ) );
+        final LoggerStore store = factory.createLoggerStore( config );
+        runLoggerTest( filename, store, level );
     }
 }
