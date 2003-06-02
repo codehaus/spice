f818a4acddbd449f397c23739496106615e07df3
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-06-02 09:15:25 +0000

    Dont try to upcast to a DefaultContext and place items in it as it violates Avalon contracts

--- spice/components/loggerstore/src/java/org/realityforge/loggerstore/SimpleLogKitLoggerStoreFactory.java
+++ spice/components/loggerstore/src/java/org/realityforge/loggerstore/SimpleLogKitLoggerStoreFactory.java
@@ -9,11 +9,9 @@ package org.realityforge.loggerstore;
 
 import java.io.File;
 import java.util.Map;
-
 import org.apache.avalon.excalibur.logger.LoggerManager;
 import org.apache.avalon.excalibur.logger.SimpleLogKitManager;
 import org.apache.avalon.framework.context.Context;
-import org.apache.avalon.framework.context.ContextException;
 import org.apache.avalon.framework.context.DefaultContext;
 
 /**
@@ -37,23 +35,16 @@ public class SimpleLogKitLoggerStoreFactory
     {
         final LoggerManager loggerManager = new SimpleLogKitManager();
         config.put( LoggerManager.class.getName(), loggerManager );
-        
+
         Context context = (Context)config.get( Context.class.getName() );
-        if ( context == null ) {
-            context = new DefaultContext();
+        if( context == null )
+        {
+            final DefaultContext newContext = new DefaultContext();
+            final File file = new File( "./" );
+            newContext.put( "app.home", file );
+            context = newContext;
         }
-		try
-		{
-			File file = (File) context.get("app.home");
-		} catch (ContextException e)
-		{
-			if ( context instanceof DefaultContext )
-			{
-				final File file = new File("./");
-				((DefaultContext) context).put("app.home", file);
-			}
-		}
         config.put( Context.class.getName(), context );
-        return super.doCreateLoggerStore( config );            
+        return super.doCreateLoggerStore( config );
     }
 }
