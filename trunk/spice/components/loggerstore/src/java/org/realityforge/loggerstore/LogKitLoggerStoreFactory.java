65510adae3005b00c362e8aab60e21eb54b7090d
maurotalevi <maurotalevi@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-06-02 20:50:35 +0000

    Added configurable LoggerManager class - which defaults to LogKitLoggerManager - if no LoggerManager in config.

--- trunk/spice/components/loggerstore/src/java/org/realityforge/loggerstore/LogKitLoggerStoreFactory.java
+++ trunk/spice/components/loggerstore/src/java/org/realityforge/loggerstore/LogKitLoggerStoreFactory.java
@@ -10,6 +10,7 @@ package org.realityforge.loggerstore;
 import java.io.InputStream;
 import java.util.Map;
 
+import org.apache.avalon.excalibur.logger.LogKitLoggerManager;
 import org.apache.avalon.excalibur.logger.LoggerManager;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
@@ -22,12 +23,24 @@ import org.apache.avalon.framework.logger.Logger;
  *
  * @author <a href="mailto:mauro.talevi at aquilonia.org">Mauro Talevi</a>
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.9 $ $Date: 2003-05-31 15:21:34 $
+ * @version $Revision: 1.10 $ $Date: 2003-06-02 20:50:35 $
  */
 public class LogKitLoggerStoreFactory
     extends AbstractLoggerStoreFactory
 {
     /**
+     * The LOGGER_MANAGER key.  Used to define the classname of the
+     * LoggerManager to use in creating a LogKitLoggerStore when not specified in the
+     * configuration map.
+     */
+    public static final String LOGGER_MANAGER = "org.realityforge.loggerstore.logkit.loggermanager";
+
+    /**
+     * The default LoggerManager class name
+     */
+    private static final String DEFAULT_LOGGER_MANAGER = LogKitLoggerManager.class.getName();
+
+	/**
      * Creates a LoggerStore from a given set of configuration parameters.
      *
      * @param config the Map of parameters for the configuration of the store
@@ -37,8 +50,18 @@ public class LogKitLoggerStoreFactory
     protected LoggerStore doCreateLoggerStore( final Map config )
         throws Exception
     {
-        final LoggerManager loggerManager =
+        LoggerManager loggerManager =
             (LoggerManager)config.get( LoggerManager.class.getName() );
+        if( null == loggerManager )
+        {
+            String type = (String)config.get( LOGGER_MANAGER );
+            if( null == type )
+            {
+                type = DEFAULT_LOGGER_MANAGER;
+            }
+            final ClassLoader classLoader = getClassLoader( config );                
+            loggerManager = createLoggerManager( type, classLoader );
+        }
             
         Logger logger =
             (Logger)config.get( Logger.class.getName() );
@@ -46,6 +69,7 @@ public class LogKitLoggerStoreFactory
         {
             logger = getLogger();
         }
+        
         final Context context =
             (Context)config.get( Context.class.getName() );
 
@@ -64,5 +88,56 @@ public class LogKitLoggerStoreFactory
         }
 
         return missingConfiguration();
+    }
+    
+    /**
+     * Retrieve the classloader from data map. If no classloader is specified
+     * then use ContextClassLoader. If ContextClassLoader not specified then
+     * use ClassLoader that loaded this class.
+     *
+     * @param data the configuration data
+     * @return a ClassLoader
+     */
+    private ClassLoader getClassLoader( final Map data )
+    {
+        ClassLoader loader = (ClassLoader)data.get( ClassLoader.class.getName() );
+        if( null == loader )
+        {
+            loader = Thread.currentThread().getContextClassLoader();
+            if( null == loader )
+            {
+                loader = LogKitLoggerStoreFactory.class.getClassLoader();
+            }
+        }
+        return loader;
+    }
+
+
+    /**
+     * Create a {@link LoggerManager} for specified type.
+     *
+     * @param type the type of the LoggerManager to use.
+     * @return the created {@link LoggerManager}
+     */
+    private LoggerManager createLoggerManager( final String type,
+                                                final ClassLoader classLoader )
+    {
+        if( null == type )
+        {
+            final String message = "No LoggerManager type specified.";
+            throw new IllegalStateException( message );
+        }
+
+        try
+        {
+            final Class clazz = classLoader.loadClass( type );
+            return (LoggerManager)clazz.newInstance();
+        }
+        catch( final Exception e )
+        {
+            final String message =
+                "Failed to created LoggerManager: " + type;
+            throw new IllegalArgumentException( message );
+        }
     }
 }
