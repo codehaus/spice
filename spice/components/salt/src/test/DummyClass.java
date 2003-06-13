f124c5aa30d3712c77a74902e539a27caf0b122c
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-06-13 01:48:29 +0000

    Add in class in "default" package

---
+++ spice/components/salt/src/test/DummyClass.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+
+/**
+ * a dummy class so we can get package resources for it in i18n tests.
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-06-13 01:48:29 $
+ */
+public class DummyClass
+{
+}
