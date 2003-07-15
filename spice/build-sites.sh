6c036b4e3e78f6967458c9a7f429ff81788f419f
maurotalevi <maurotalevi@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-07-15 23:18:18 +0000

    Corrected path to xsyslog

--- spice/build-sites.sh
+++ spice/build-sites.sh
@@ -3,7 +3,7 @@
 # this script is in the public domain
 
 # update these projects
-PROJECTS="site components/classman components/cli components/configkit components/converter components/jndikit components/loggerstore components/netserve components/salt components/threadpool components/xmlpolicy sandbox/metaclass sandbox/metaschema sandbox/nativekit sandbox/swingactions sandbox/xinvoke /xsyslog"
+PROJECTS="site components/classman components/cli components/configkit components/converter components/jndikit components/loggerstore components/netserve components/salt components/threadpool components/xmlpolicy sandbox/metaclass sandbox/metaschema sandbox/nativekit sandbox/swingactions sandbox/xinvoke sandbox/xsyslog"
 MULTI_PROCESS="no"
 ANT_COMMAND="maven site:generate site:deploy"
 
