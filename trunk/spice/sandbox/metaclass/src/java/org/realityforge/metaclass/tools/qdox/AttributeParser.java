24aad85e9ab3d824d61c1730b11a356c23dc6150
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-08-19 04:01:58 +0000

    Add in basic facade for regex access

---
+++ trunk/spice/sandbox/metaclass/src/java/org/realityforge/metaclass/tools/qdox/AttributeParser.java
@@ -0,0 +1,8 @@
+package org.realityforge.metaclass.tools.qdox;
+
+interface AttributeParser
+{
+   String PERL_PATTERN = "^[ \\t\\r\\n]*([a-zA-Z\\_][a-zA-Z0-9\\_]*=\\\"[^\\\"]*\\\"[ \\t\\r\\n]*)+";
+
+   String[] parseValueIntoParameterSpans( String value );
+}
