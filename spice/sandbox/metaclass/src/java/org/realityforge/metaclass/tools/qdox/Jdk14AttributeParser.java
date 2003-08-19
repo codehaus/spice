831817d76e74427e2f0db6131ec981b9bbf4bdd6
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-08-19 04:01:58 +0000

    Add in basic facade for regex access

---
+++ spice/sandbox/metaclass/src/java/org/realityforge/metaclass/tools/qdox/Jdk14AttributeParser.java
@@ -0,0 +1,28 @@
+package org.realityforge.metaclass.tools.qdox;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class Jdk14AttributeParser
+   implements AttributeParser
+{
+   public String[] parseValueIntoParameterSpans( String value )
+   {
+      final Pattern pattern = Pattern.compile( PERL_PATTERN );
+      final Matcher matcher = pattern.matcher( value );
+      if ( matcher.matches() )
+      {
+         final int count = matcher.groupCount();
+         final String[] results = new String[ count ];
+         for ( int i = 0; i < count; i++ )
+         {
+            results[ i ] = matcher.group( i ).trim();
+         }
+         return results;
+      }
+      else
+      {
+         return null;
+      }
+   }
+}
