54d13e3e67f4ec8c24c9c8f55dbb7c92f5b62910
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-08-22 05:42:37 +0000

    More unit testing

--- trunk/spice/sandbox/metaclass/src/test/org/realityforge/metaclass/tools/qdox/InfoBuilderTestCase.java
+++ trunk/spice/sandbox/metaclass/src/test/org/realityforge/metaclass/tools/qdox/InfoBuilderTestCase.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-08-22 05:27:57 $
+ * @version $Revision: 1.2 $ $Date: 2003-08-22 05:42:37 $
  */
 public class InfoBuilderTestCase
     extends TestCase
@@ -85,6 +85,73 @@ public class InfoBuilderTestCase
         throws Exception
     {
         final String value = "key1=\"value1\" key2=\"value1";
+        final QDoxDescriptorParser parser = new QDoxDescriptorParser();
+        final Properties parameters = parser.parseValueIntoParameters( value );
+        assertNull( "parameters", parameters );
+    }
+
+    public void testParseValueIntoParametersWithSingleKey()
+        throws Exception
+    {
+        final String value = "key1=\"value1\"";
+        final QDoxDescriptorParser parser = new QDoxDescriptorParser();
+        final Properties parameters = parser.parseValueIntoParameters( value );
+        assertNotNull( "parameters", parameters );
+        assertEquals( "parameters.lenght", 1, parameters.size() );
+        assertEquals( "parameters.get('key')",
+                      "value1",
+                      parameters.getProperty( "key1" ) );
+    }
+
+    public void testParseValueIntoParametersWithSingleKeySurroundedByWhitespace()
+        throws Exception
+    {
+        final String value = " \t \n key1=\"value1\"   \n\t";
+        final QDoxDescriptorParser parser = new QDoxDescriptorParser();
+        final Properties parameters = parser.parseValueIntoParameters( value );
+        assertNotNull( "parameters", parameters );
+        assertEquals( "parameters.lenght", 1, parameters.size() );
+        assertEquals( "parameters.get('key')",
+                      "value1",
+                      parameters.getProperty( "key1" ) );
+    }
+
+    public void testParseValueIntoParametersWithMultipleKey()
+        throws Exception
+    {
+        final String value = "key1=\"value1\" key2=\"value2\"";
+        final QDoxDescriptorParser parser = new QDoxDescriptorParser();
+        final Properties parameters = parser.parseValueIntoParameters( value );
+        assertNotNull( "parameters", parameters );
+        assertEquals( "parameters.lenght", 2, parameters.size() );
+        assertEquals( "parameters.get('key1')",
+                      "value1",
+                      parameters.getProperty( "key1" ) );
+        assertEquals( "parameters.get('key2')",
+                      "value2",
+                      parameters.getProperty( "key2" ) );
+    }
+
+    public void testParseValueIntoParametersWithMultipleKeySurroundedByWhitespace()
+        throws Exception
+    {
+        final String value = " \t \n key1=\"value1\"   \n key2=\"value2\"\t";
+        final QDoxDescriptorParser parser = new QDoxDescriptorParser();
+        final Properties parameters = parser.parseValueIntoParameters( value );
+        assertNotNull( "parameters", parameters );
+        assertEquals( "parameters.lenght", 2, parameters.size() );
+        assertEquals( "parameters.get('key1')",
+                      "value1",
+                      parameters.getProperty( "key1" ) );
+        assertEquals( "parameters.get('key2')",
+                      "value2",
+                      parameters.getProperty( "key2" ) );
+    }
+
+    public void testParseValueIntoParametersWithMultipleKeySurroundedByZeroWhitespace()
+        throws Exception
+    {
+        final String value = "key1=\"value1\"key2=\"value2\"";
         final QDoxDescriptorParser parser = new QDoxDescriptorParser();
         final Properties parameters = parser.parseValueIntoParameters( value );
         assertNull( "parameters", parameters );
