b75d80603803bd5d79665bd07463a77266cc95b5
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-08-23 05:19:46 +0000

    Test fields

--- trunk/spice/sandbox/metaclass/src/test/org/realityforge/metaclass/AttributesTestCase.java
+++ trunk/spice/sandbox/metaclass/src/test/org/realityforge/metaclass/AttributesTestCase.java
@@ -12,15 +12,21 @@ import org.realityforge.metaclass.model.Attribute;
 import org.realityforge.metaclass.model.ClassDescriptor;
 import org.realityforge.metaclass.model.FieldDescriptor;
 import org.realityforge.metaclass.model.MethodDescriptor;
+import java.lang.reflect.Field;
 
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.3 $ $Date: 2003-08-23 05:13:39 $
+ * @version $Revision: 1.4 $ $Date: 2003-08-23 05:19:46 $
  */
 public class AttributesTestCase
     extends TestCase
 {
+    /**
+     * Leave this fied in as it is used in unit tests.
+     */
+    protected int m_testField = 1;
+
     public void testGetAttributeByNameWithZeroAttributes()
     {
         final Attribute attribute =
@@ -166,9 +172,89 @@ public class AttributesTestCase
                                  MethodDescriptor.EMPTY_SET );
         MetaClassIntrospector.setAccessor( new MockAccessor( descriptor, null ) );
         MetaClassIntrospector.clearCompleteCache();
-        
+
         final Attribute result =
             Attributes.getAttribute( AttributesTestCase.class, name );
+        assertEquals( "attribute", attribute1, result );
+    }
+
+    public void testGetAttributesForField()
+    {
+        final String name = "name";
+        final Attribute attribute1 = new Attribute( name );
+        final Attribute attribute2 = new Attribute( name );
+        final Attribute[] attributes = new Attribute[]{attribute1, attribute2};
+        final Field field = AttributesTestCase.class.getDeclaredFields()[ 0 ];
+        final FieldDescriptor fieldDescriptor =
+            new FieldDescriptor( field.getName(),
+                                 field.getType().getName(),
+                                 field.getModifiers(),
+                                 attributes );
+        final ClassDescriptor descriptor =
+            new ClassDescriptor( AttributesTestCase.class.getName(),
+                                 0,
+                                 Attribute.EMPTY_SET,
+                                 new FieldDescriptor[]{fieldDescriptor},
+                                 MethodDescriptor.EMPTY_SET );
+        MetaClassIntrospector.setAccessor( new MockAccessor( descriptor, null ) );
+        MetaClassIntrospector.clearCompleteCache();
+        final Attribute[] results = Attributes.getAttributes( field );
+        assertEquals( "attributes.length", 2, results.length );
+        assertEquals( "attributes[ 0 ]", attribute1, results[ 0 ] );
+        assertEquals( "attributes[ 1 ]", attribute2, results[ 1 ] );
+    }
+
+    public void testGetAttributesForFieldWithName()
+    {
+        final String name = "name";
+        final Attribute attribute1 = new Attribute( name );
+        final Attribute attribute2 = new Attribute( "bleh" );
+        final Attribute[] attributes = new Attribute[]{attribute1, attribute2};
+
+        final Field field = AttributesTestCase.class.getDeclaredFields()[ 0 ];
+        final FieldDescriptor fieldDescriptor =
+            new FieldDescriptor( field.getName(),
+                                 field.getType().getName(),
+                                 field.getModifiers(),
+                                 attributes );
+        final ClassDescriptor descriptor =
+            new ClassDescriptor( AttributesTestCase.class.getName(),
+                                 0,
+                                 Attribute.EMPTY_SET,
+                                 new FieldDescriptor[]{fieldDescriptor},
+                                 MethodDescriptor.EMPTY_SET );
+        MetaClassIntrospector.setAccessor( new MockAccessor( descriptor, null ) );
+        MetaClassIntrospector.clearCompleteCache();
+        final Attribute[] results =
+            Attributes.getAttributes( field, name );
+        assertEquals( "attributes.length", 1, results.length );
+        assertEquals( "attributes[ 0 ]", attribute1, results[ 0 ] );
+    }
+
+    public void testGetAttributeForFieldWithName()
+    {
+        final String name = "name";
+        final Attribute attribute1 = new Attribute( name );
+        final Attribute attribute2 = new Attribute( "bleh" );
+        final Attribute[] attributes = new Attribute[]{attribute1, attribute2};
+
+                final Field field = AttributesTestCase.class.getDeclaredFields()[ 0 ];
+        final FieldDescriptor fieldDescriptor =
+            new FieldDescriptor( field.getName(),
+                                 field.getType().getName(),
+                                 field.getModifiers(),
+                                 attributes );
+        final ClassDescriptor descriptor =
+            new ClassDescriptor( AttributesTestCase.class.getName(),
+                                 0,
+                                 Attribute.EMPTY_SET,
+                                 new FieldDescriptor[]{fieldDescriptor},
+                                 MethodDescriptor.EMPTY_SET );
+        MetaClassIntrospector.setAccessor( new MockAccessor( descriptor, null ) );
+        MetaClassIntrospector.clearCompleteCache();
+
+        final Attribute result =
+            Attributes.getAttribute( field, name );
         assertEquals( "attribute", attribute1, result );
     }
 }
