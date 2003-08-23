28bd13bac835e8954bf4e250de3878e8a808aa23
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-08-23 23:51:26 +0000

    more tests for task

--- spice/sandbox/metaclass/src/test/org/realityforge/metaclass/tools/tasks/MetaGenerateTaskTestCase.java
+++ spice/sandbox/metaclass/src/test/org/realityforge/metaclass/tools/tasks/MetaGenerateTaskTestCase.java
@@ -7,18 +7,18 @@
  */
 package org.realityforge.metaclass.tools.tasks;
 
+import java.io.File;
 import junit.framework.TestCase;
 import org.apache.tools.ant.BuildException;
 
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-08-23 13:25:07 $
+ * @version $Revision: 1.2 $ $Date: 2003-08-23 23:51:26 $
  */
 public class MetaGenerateTaskTestCase
     extends TestCase
 {
-
     public void testNullDestDir()
         throws Exception
     {
@@ -27,11 +27,44 @@ public class MetaGenerateTaskTestCase
         {
             task.execute();
         }
-        catch( BuildException e )
+        catch( final BuildException e )
         {
-            e.printStackTrace();
+            assertEquals( "DestDir not specified", e.getMessage() );
             return;
         }
         fail( "Expected execute to fail as no destdir specified." );
+    }
+
+    public void testFileAsDestDir()
+        throws Exception
+    {
+        final MockMetaGenerateTask task = new MockMetaGenerateTask();
+        final File baseDirectory = getBaseDirectory();
+        final File destDir =
+            File.createTempFile( "mgtest", ".tmp", baseDirectory ).getCanonicalFile();
+        task.setDestDir( destDir );
+        try
+        {
+            task.execute();
+        }
+        catch( final BuildException e )
+        {
+            assertEquals( "DestDir (" + destDir + ") is not a directory.", e.getMessage() );
+            return;
+        }
+        fail( "Expected execute to fail as destdir specified by file." );
+    }
+
+    private static final File getBaseDirectory()
+    {
+        final String tempDir = System.getProperty( "java.io.tmpdir" );
+        final String baseDir = System.getProperty( "basedir", tempDir );
+
+        final File base = new File( baseDir ).getAbsoluteFile();
+        final String pathname =
+            base + File.separator + "target" + File.separator + "test-data";
+        final File dir = new File( pathname );
+        dir.mkdirs();
+        return dir;
     }
 }
