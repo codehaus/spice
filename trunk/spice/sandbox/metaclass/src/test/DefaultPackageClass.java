d60bf135e896bb51fb5fea6f6db43d595720a44e
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-08-23 04:49:11 +0000

    Add class in default package

---
+++ trunk/spice/sandbox/metaclass/src/test/DefaultPackageClass.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-08-23 04:49:11 $
+ */
+class DefaultPackageClass
+{
+}
