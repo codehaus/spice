db58d12f20559c24074984a6b084464f38e83429
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-08-29 05:48:47 +0000

    Change to use normal Threads rather than WorkerThreads

--- spice/components/threadpool/src/java/org/jcomponent/threadpool/impl/NullThreadPoolMonitor.java
+++ spice/components/threadpool/src/java/org/jcomponent/threadpool/impl/NullThreadPoolMonitor.java
@@ -5,22 +5,30 @@ import org.jcomponent.threadpool.ThreadPoolMonitor;
 /**
  * @author Mauro n Paul
  */
-public class NullThreadPoolMonitor implements ThreadPoolMonitor {
-    public void newThreadPool(String name, int priority, boolean daemon, int maxActive, int maxIdle) {
-    }
+public class NullThreadPoolMonitor
+   implements ThreadPoolMonitor
+{
+   public void newThreadPool( String name, int priority, boolean daemon, int maxActive, int maxIdle )
+   {
+   }
 
-    public void unexpectedThrowable(String reference, Throwable t) {
-    }
+   public void unexpectedThrowable( String reference, Throwable t )
+   {
+   }
 
-    public void threadRetrieved(WorkerThread worker) {
-    }
+   public void threadRetrieved( Thread worker )
+   {
+   }
 
-    public void threadReturned(WorkerThread worker) {
-    }
+   public void threadReturned( Thread worker )
+   {
+   }
 
-    public void threadCreated(WorkerThread worker) {
-    }
+   public void threadCreated( Thread worker )
+   {
+   }
 
-    public void threadDisposing(WorkerThread worker) {
-    }
+   public void threadDisposing( Thread worker )
+   {
+   }
 }
