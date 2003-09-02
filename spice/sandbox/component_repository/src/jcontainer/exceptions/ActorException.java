8625c1a37167f82b940f5dd2ee23d202f6a61643
nobody <nobody@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-09-02 04:17:29 +0000

    This commit was manufactured by cvs2svn to create tag 'XINVOKE_PRE_DELETE-tag'.

--- spice/sandbox/component_repository/src/jcontainer/exceptions/ActorException.java
+++
@@ -1,7 +0,0 @@
-package jcontainer.exceptions;
-
-public class ActorException extends JContainerException {
-    public ActorException(String s) {
-        super(s);
-    }
-}
--- spice/tools/lib/README.txt
+++
@@ -1,2 +0,0 @@
-Jars in this directory are strictly here for gump compilation.
-They are not used in the regular compilation process.
