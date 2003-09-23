fc3cb8392280bbe198304038d678c63dab9e22b3
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-09-23 08:19:54 +0000

    Remove the old version of component repository

--- trunk/spice/sandbox/component_repository/src/jcontainer/api/ComponentFactory.java
+++
@@ -1,7 +0,0 @@
-package jcontainer.api;
-
-import jcontainer.api.Component;
-
-public interface ComponentFactory {
-    public Component createComponent(String componentName, String version, String theInterface);
-}
--- trunk/spice/sandbox/component_repository/src/jcontainer/exceptions/SystemException.java
+++
@@ -1,7 +0,0 @@
-package jcontainer.exceptions;
-
-public class SystemException extends JContainerException {
-    public SystemException(final String message) {
-        super(message);
-    }
-}
