75e10278f215fda5df03866f66d1a971225b5a45
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-03 13:33:54 +0000

    Add license

--- spice/sandbox/metaclass/src/java/org/realityforge/metaclass/io/MetaClassAccessor.java
+++ spice/sandbox/metaclass/src/java/org/realityforge/metaclass/io/MetaClassAccessor.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
 package org.realityforge.metaclass.io;
 
 import org.realityforge.metaclass.MetaClassException;
@@ -8,7 +15,7 @@ import org.realityforge.metaclass.model.ClassDescriptor;
  * {@link ClassDescriptor}s for a particular class.
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.4 $ $Date: 2003-09-28 03:53:00 $
+ * @version $Revision: 1.5 $ $Date: 2003-10-03 13:33:54 $
  */
 public interface MetaClassAccessor
 {
