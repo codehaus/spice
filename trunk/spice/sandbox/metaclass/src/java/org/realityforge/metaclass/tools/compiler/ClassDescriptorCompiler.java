b375cd42b0f35ce82dcbca286d9ae890651ff5de
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-04 04:20:03 +0000

    Add in initial cut of separate compiler

---
+++ trunk/spice/sandbox/metaclass/src/java/org/realityforge/metaclass/tools/compiler/ClassDescriptorCompiler.java
@@ -0,0 +1,411 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.metaclass.tools.compiler;
+
+import com.thoughtworks.qdox.JavaDocBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.realityforge.metaclass.io.MetaClassIO;
+import org.realityforge.metaclass.model.ClassDescriptor;
+import org.realityforge.metaclass.tools.qdox.QDoxAttributeInterceptor;
+import org.realityforge.metaclass.tools.qdox.QDoxDescriptorParser;
+
+/**
+ * A bean that can create MetaClass descriptors by processing
+ * Java Source files with qdox.
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-04 04:20:03 $
+ */
+public class ClassDescriptorCompiler
+{
+    /**
+     * The utility class used to generate MetaClass object.
+     */
+    private static final QDoxDescriptorParser c_infoBuilder = new QDoxDescriptorParser();
+
+    /**
+     * Destination directory
+     */
+    private File m_destDir;
+
+    /**
+     * The IO object to use when writing descriptors.
+     */
+    private MetaClassIO m_metaClassIO;
+
+    /**
+     * The file extension to use for the attributes.
+     */
+    private String m_extension;
+
+    /**
+     * the monitor that receives messages about operation of compiler.
+     */
+    private CompilerMonitor m_monitor = new DefaultCompilerMonitor();
+
+    /**
+     * The source files to process.
+     */
+    private final List m_sourceFiles = new ArrayList();
+
+    /**
+     * The interceptors used to process source files.
+     */
+    private final List m_interceptors = new ArrayList();
+
+    /**
+     * The filters used to filter source files.
+     */
+    private final List m_filters = new ArrayList();
+
+    /**
+     * Set the monitor to receive messages about compiler operation.
+     *
+     * @param monitor the monitor.
+     */
+    public void setMonitor( final CompilerMonitor monitor )
+    {
+        if( null == monitor )
+        {
+            throw new NullPointerException( "monitor" );
+        }
+        m_monitor = monitor;
+    }
+
+    /**
+     * Add a sourceFile to process.
+     *
+     * @param sourceFile the sourceFile
+     */
+    public void addSourceFile( final File sourceFile )
+    {
+        if( null == sourceFile )
+        {
+            throw new NullPointerException( "sourceFile" );
+        }
+        m_sourceFiles.add( sourceFile );
+    }
+
+    /**
+     * Add a filter to process metadata.
+     *
+     * @param filter the filter
+     */
+    public void addFilter( final JavaClassFilter filter )
+    {
+        if( null == filter )
+        {
+            throw new NullPointerException( "filter" );
+        }
+        m_filters.add( filter );
+    }
+
+    /**
+     * Add an interceptor to process metadata.
+     *
+     * @param interceptor the interceptor
+     */
+    public void addInterceptor( final QDoxAttributeInterceptor interceptor )
+    {
+        if( null == interceptor )
+        {
+            throw new NullPointerException( "interceptor" );
+        }
+        m_filters.add( interceptor );
+    }
+
+    /**
+     * Set the destination directory for generated files.
+     *
+     * @param destDir the destination directory for generated files.
+     */
+    public void setDestDir( final File destDir )
+    {
+        m_destDir = destDir;
+    }
+
+    /**
+     * Set the IO object used to write descriptors.
+     *
+     * @param metaClassIO the IO object used to write descriptors.
+     */
+    public void setMetaClassIO( MetaClassIO metaClassIO )
+    {
+        if( null == metaClassIO )
+        {
+            throw new NullPointerException( "metaClassIO" );
+        }
+        m_metaClassIO = metaClassIO;
+    }
+
+    /**
+     * Set the file extension used when generating file descriptors.
+     *
+     * @param extension the file extension used when generating file descriptors.
+     */
+    public void setExtension( final String extension )
+    {
+        if( null == extension )
+        {
+            throw new NullPointerException( "extension" );
+        }
+        m_extension = extension;
+    }
+
+    /**
+     * Generate classes and output.
+     *
+     * @throws Exception if unable to compile descriptors
+     */
+    public void execute()
+        throws Exception
+    {
+        validate();
+        processSourceFiles();
+    }
+
+    /**
+     * Output the ClassDescriptors that are not filtered out.
+     */
+    protected void processSourceFiles()
+    {
+        final List classes = collectJavaClassesToSerialize();
+        m_monitor.javaClassObjectsLoaded( classes );
+
+        final List filteredClasses = filterJavaClassObjects( classes );
+        m_monitor.postFilterJavaClassList( classes );
+
+        final List descriptors = buildClassDescriptors( filteredClasses );
+        writeClassDescriptors( descriptors );
+    }
+
+    /**
+     * Build class descriptors from input JavaCLass objects.
+     *
+     * @param classes the list containing JavaClass objects.
+     * @return a list containing created ClassDescriptor objects.
+     */
+    private List buildClassDescriptors( final List classes )
+    {
+        final QDoxAttributeInterceptor[] interceptors =
+            (QDoxAttributeInterceptor[])m_interceptors.toArray( new QDoxAttributeInterceptor[ m_interceptors.size() ] );
+
+        final ArrayList descriptors = new ArrayList();
+        final Iterator iterator = classes.iterator();
+        while( iterator.hasNext() )
+        {
+            final JavaClass javaClass = (JavaClass)iterator.next();
+            final ClassDescriptor descriptor =
+                c_infoBuilder.buildClassDescriptor( javaClass, interceptors );
+            descriptors.add( descriptor );
+        }
+        return descriptors;
+    }
+
+    /**
+     * Output the specified ClassDescriptors.
+     *
+     * @param classes the list containing ClassDescriptor objects.
+     */
+    private void writeClassDescriptors( final List classes )
+    {
+        final Iterator iterator = classes.iterator();
+        while( iterator.hasNext() )
+        {
+            final ClassDescriptor descriptor = (ClassDescriptor)iterator.next();
+            writeClassDescriptor( descriptor );
+        }
+    }
+
+    /**
+     * Return the set of classes that will actually be serialized
+     * and have not been filtered out.
+     *
+     * @return list of classes to serialize
+     */
+    private List collectJavaClassesToSerialize()
+    {
+        final JavaDocBuilder builder = new JavaDocBuilder();
+        final int count = m_sourceFiles.size();
+        for( int i = 0; i < count; i++ )
+        {
+            final File file = (File)m_sourceFiles.get( i );
+            FileInputStream in = null;
+            try
+            {
+                builder.addSource( file );
+            }
+            catch( final FileNotFoundException fnfe )
+            {
+                m_monitor.missingSourceFile( file );
+            }
+            finally
+            {
+                shutdownStream( in );
+            }
+        }
+        final JavaSource[] sources = builder.getSources();
+        final ArrayList classes = new ArrayList();
+        for( int i = 0; i < sources.length; i++ )
+        {
+            final JavaClass[] javaClasses = sources[ i ].getClasses();
+            for( int j = 0; j < javaClasses.length; j++ )
+            {
+                classes.add( javaClasses[ j ] );
+            }
+        }
+        return classes;
+    }
+
+    /**
+     * Return the set of classes that will actually be serialized
+     * and have not been filtered out.
+     *
+     * @return list of classes to serialize
+     */
+    private List filterJavaClassObjects( final List input )
+    {
+        final JavaClassFilter[] filters = (JavaClassFilter[])m_filters.
+            toArray( new JavaClassFilter[ m_filters.size() ] );
+        final MulticastJavaClassFilter filter =
+            new MulticastJavaClassFilter( filters );
+
+        final List classes = new ArrayList();
+        final int classCount = input.size();
+        for( int i = 0; i < classCount; i++ )
+        {
+            final JavaClass candidate = (JavaClass)input.get( i );
+            final JavaClass javaClass = filter.filterClass( candidate );
+            if( null == javaClass )
+            {
+                continue;
+            }
+            classes.add( javaClass );
+        }
+        return classes;
+    }
+
+    /**
+     * Validate that the parameters are valid.
+     *
+     * @throws Exception if unable to validate settings
+     */
+    private void validate()
+        throws Exception
+    {
+        if( null == m_destDir )
+        {
+            final String message = "DestDir not specified";
+            throw new Exception( message );
+        }
+        if( m_destDir.exists() && !m_destDir.isDirectory() )
+        {
+            final String message =
+                "DestDir (" + m_destDir + ") is not a directory.";
+            throw new Exception( message );
+        }
+        if( !m_destDir.exists() && !m_destDir.mkdirs() )
+        {
+            final String message =
+                "DestDir (" + m_destDir + ") could not be created.";
+            throw new Exception( message );
+        }
+    }
+
+    /**
+     * Write ClassDescriptor out into a file.
+     *
+     * @param descriptor the ClassDescriptor object
+     */
+    private void writeClassDescriptor( final ClassDescriptor descriptor )
+    {
+        final String fqn = descriptor.getName();
+        OutputStream outputStream = null;
+        try
+        {
+            final File file = getOutputFileForClass( fqn );
+            file.getParentFile().mkdirs();
+            outputStream = new FileOutputStream( file );
+            m_metaClassIO.serializeClass( outputStream, descriptor );
+        }
+        catch( final Exception e )
+        {
+            m_monitor.errorWritingDescriptor( descriptor, e );
+        }
+        finally
+        {
+            shutdownStream( outputStream );
+        }
+    }
+
+    /**
+     * Determine the file for specified class.
+     *
+     * @param classname the fully qualified name of file to generate
+     * @return the file for info
+     * @throws IOException if unable to determine base file
+     */
+    private File getOutputFileForClass( final String classname )
+        throws IOException
+    {
+        final String filename =
+            classname.replace( '.', File.separatorChar ) + m_extension;
+        return new File( m_destDir, filename ).getCanonicalFile();
+    }
+
+    /**
+     * Close the specified output stream and swallow any exceptions.
+     *
+     * @param outputStream the output stream
+     */
+    void shutdownStream( final OutputStream outputStream )
+    {
+        if( null != outputStream )
+        {
+            try
+            {
+                outputStream.close();
+            }
+            catch( IOException e )
+            {
+                //Ignored
+            }
+        }
+    }
+
+    /**
+     * Close the specified input stream and swallow any exceptions.
+     *
+     * @param inputStream the input stream
+     */
+    void shutdownStream( final InputStream inputStream )
+    {
+        if( null != inputStream )
+        {
+            try
+            {
+                inputStream.close();
+            }
+            catch( IOException e )
+            {
+                //Ignored
+            }
+        }
+    }
+}
