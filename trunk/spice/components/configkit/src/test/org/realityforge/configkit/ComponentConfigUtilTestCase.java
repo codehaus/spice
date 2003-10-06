e9ef2ec2b87dd024dfeeb9a9b6fd8e234d0392a3
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-06 04:47:08 +0000

    Add more unit tests

--- trunk/spice/components/configkit/src/test/org/realityforge/configkit/ComponentConfigUtilTestCase.java
+++ trunk/spice/components/configkit/src/test/org/realityforge/configkit/ComponentConfigUtilTestCase.java
@@ -7,7 +7,12 @@
  */
 package org.realityforge.configkit;
 
+import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.net.URL;
+
 import junit.framework.TestCase;
+import org.xml.sax.InputSource;
 
 /**
  * Basic unit tests for the catalog handler class.
@@ -53,5 +58,75 @@ public final class ComponentConfigUtilTestCase
          ComponentConfigUtil.calculateLocation( "Baz", "Foo.xml" );
       assertEquals( "/Foo.xml", resource );
    }
-}
 
+   public void testGetSchemaInputSource()
+      throws Exception
+   {
+      final String resource = "foo.dtd";
+      final URL url = new File( "." ).toURL();
+      final ByteArrayInputStream stream =
+         new ByteArrayInputStream( new byte[ 0 ] );
+      final MockClassLoader classLoader = new MockClassLoader( url, stream );
+      final InputSource source =
+         ComponentConfigUtil.getSchemaInputSource( resource, classLoader );
+      assertNotNull( source );
+      assertEquals( "systemID", url.toExternalForm(), source.getSystemId() );
+   }
+
+   public void testGetSchemaInputSourceWithNonExistent()
+      throws Exception
+   {
+      final String resource = "foo.dtd";
+      final MockClassLoader classLoader = new MockClassLoader( null, null );
+      final InputSource source =
+         ComponentConfigUtil.getSchemaInputSource( resource, classLoader );
+      assertNull( source );
+   }
+
+   public void testGetComponentConfigValidator()
+      throws Exception
+   {
+      final ConfigValidator validator =
+         ComponentConfigUtil.getComponentConfigValidator( "org.realityforge.configkit.test.Baz",
+                                                          getClass().getClassLoader(),
+                                                          "assembly.dtd",
+                                                          null );
+      assertNotNull( validator );
+   }
+
+   public void testGetComponentConfigValidatorWithNullClassLoader()
+      throws Exception
+   {
+      try
+      {
+         ComponentConfigUtil.getComponentConfigValidator( "",
+                                                          null,
+                                                          "assembly.dtd",
+                                                          null );
+      }
+      catch ( final NullPointerException npe )
+      {
+         assertEquals( "npe.message", "classLoader", npe.getMessage() );
+         return;
+      }
+      fail( "Expected npe" );
+   }
+
+   public void testGetComponentConfigValidatorWithNullClassName()
+      throws Exception
+   {
+      try
+      {
+         ComponentConfigUtil.getComponentConfigValidator( null,
+                                                          getClass().getClassLoader(),
+                                                          "assembly.dtd",
+                                                          null );
+      }
+      catch ( final NullPointerException npe )
+      {
+         assertEquals( "npe.message", "classname", npe.getMessage() );
+         return;
+      }
+      fail( "Expected npe" );
+   }
+}
