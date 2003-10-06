ff4e7e43cc365f105ba366a84060e0b7e5683d1c
nobody <nobody@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-06 09:45:54 +0000

    This commit was manufactured by cvs2svn to create tag 'CONFIGKIT_1_1_3-tag'.

---
+++ tags/CONFIGKIT_1_1_3-tag/spice/components/configkit/src/java/org/realityforge/configkit/ComponentConfigUtil.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.configkit;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import org.realityforge.configkit.ConfigValidator;
+import org.realityforge.configkit.ConfigValidatorFactory;
+import org.xml.sax.InputSource;
+
+/**
+ * Utility class to get ConfigValidator objects for components.
+ * See {@link #getComponentConfigValidator} for a detailed
+ * explanation about how ConfigValidator objects are loaded.
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.4 $ $Date: 2003-10-06 04:46:58 $
+ */
+public class ComponentConfigUtil
+{
+   /**
+    * Postfix added to classname of component to look for schema.
+    */
+   private static final String DEFAULT_LOCATION_POSTFIX = "-schema.xml";
+
+   /**
+    * Return the ConfigValidator for specified component.
+    * The component is specified by classname and classloader.
+    * The ConfigValidator is loaded from specified location
+    * and has specified type. If the type is null then ConfigKit
+    * will attempt to guess the type based on schema location.
+    * The location parameter can be relative to the component (ie
+    * FooSchema.xml for class com.biz.Bar will load resource
+    * "/com/biz/FooSchema.xml" from classloader), absolute resource
+    * location in classloader (must start with "/") or null. If the
+    * location is null then it will assume resource name is the same
+    * name as class with the postfix "-schema.xml" added to classname.
+    * If no such resource is located in the ClassLoader then null is
+    * returned.
+    *
+    * @param classname the classname of component
+    * @param classLoader the classloader component loaded from
+    * @param location the location of schema
+    * @param type the type of schema
+    * @return the ConfigValidator
+    * @throws java.lang.Exception if error creating validator
+    */
+   public static ConfigValidator getComponentConfigValidator( final String classname,
+                                                              final ClassLoader classLoader,
+                                                              final String location,
+                                                              final String type )
+      throws Exception
+   {
+      if ( null == classname )
+      {
+         throw new NullPointerException( "classname" );
+      }
+      if ( null == classLoader )
+      {
+         throw new NullPointerException( "classLoader" );
+      }
+      final String actualLocation = calculateLocation( classname, location );
+      final InputSource inputSource =
+         getSchemaInputSource( actualLocation, classLoader );
+      return ConfigValidatorFactory.create( type, inputSource );
+   }
+
+   /**
+    * Determine the location of configuration schema for class.
+    * If the specified location is not null then that will
+    * be returned otherwise the location is the resource name
+    * of the with {@link #DEFAULT_LOCATION_POSTFIX} appended
+    * rather than ".class". ie If the classname was "com.biz.Foo"
+    * then the schema location would be at "/com/biz/Foo-schema.xml".
+    *
+    * @param classname the name of the class
+    * @param location the specified location of schema
+    * @return the actual location of schema
+    */
+   static String calculateLocation( final String classname,
+                                    final String location )
+   {
+      if ( null == location )
+      {
+         return "/" + classname.replace( '.', '/' ) +
+            DEFAULT_LOCATION_POSTFIX;
+      }
+      else if ( location.startsWith( "/" ) )
+      {
+         return location;
+      }
+      else
+      {
+         final int index = classname.lastIndexOf( '.' );
+         String packageName;
+         if ( -1 != index )
+         {
+            packageName = classname.substring( 0, index + 1 );
+         }
+         else
+         {
+            packageName = "";
+         }
+         return "/" + packageName.replace( '.', '/' ) + location;
+      }
+   }
+
+   /**
+    * Get the input source for schema specified for component.
+    *
+    * @param resource the resource location of schema
+    * @param classLoader the ClassLoader to load schema from
+    * @return the InputSource for schema
+    */
+   static InputSource getSchemaInputSource( final String resource,
+                                            final ClassLoader classLoader )
+   {
+      final URL url = classLoader.getResource( resource );
+      if ( null == url )
+      {
+         return null;
+      }
+
+      final InputStream inputStream = classLoader.getResourceAsStream( resource );
+      final InputSource inputSource = new InputSource( inputStream );
+      inputSource.setSystemId( url.toExternalForm() );
+      return inputSource;
+   }
+}
---
+++ tags/CONFIGKIT_1_1_3-tag/spice/components/configkit/src/java/org/realityforge/configkit/ConfigValidatorFactory.java
@@ -0,0 +1,267 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.configkit;
+
+import java.io.InputStream;
+import org.iso_relax.verifier.Schema;
+import org.iso_relax.verifier.VerifierFactory;
+import org.iso_relax.verifier.VerifierConfigurationException;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+
+/**
+ * The ConfigValidatorFactory is responsible for creating ConfigValidator
+ * objects to validate configuration according to specified schemas.
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.3 $ $Date: 2003-04-05 09:40:45 $
+ */
+public final class ConfigValidatorFactory
+{
+    /**
+     * A constant defining namespace of RELAX_NG schema language.
+     */
+    public static final String RELAX_NG = "http://relaxng.org/ns/structure/1.0";
+
+    /**
+     * A constant defining namespace of W3C XMLSchema language.
+     */
+    public static final String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+
+    /**
+     * The Class object that points at MSV class.
+     */
+    private static Class c_msvClass;
+
+    /**
+     * Create a ConfigValidator and attempt to guess Schema Type. The config validator
+     * loads the schema from specified publicID, systemID, classloader
+     * combination using the {@link ResolverFactory}.
+     *
+     * @param publicID the publicID of schema (may be null)
+     * @param systemID the systemID of schema (may be null)
+     * @param classLoader the classloader from which to load schema
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final String publicID,
+                                          final String systemID,
+                                          final ClassLoader classLoader )
+        throws Exception
+    {
+        return create( null, publicID, systemID, classLoader );
+    }
+
+    /**
+     * Create a ConfigValidator with specified type. The config validator
+     * loads the schema from specified publicID, systemID, classloader
+     * combination using the {@link ResolverFactory}.
+     *
+     * @param schemaType the type of the schema.
+     *        (Usually a URL such as "http://relaxng.org/ns/structure/1.0")
+     * @param publicID the publicID of schema (may be null)
+     * @param systemID the systemID of schema (may be null)
+     * @param classLoader the classloader from which to load schema
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final String schemaType,
+                                          final String publicID,
+                                          final String systemID,
+                                          final ClassLoader classLoader )
+        throws Exception
+    {
+        if( null == publicID && null == systemID )
+        {
+            throw new NullPointerException( "publicID" );
+        }
+
+        final EntityResolver resolver = ResolverFactory.createResolver( classLoader );
+        final InputSource inputSource = resolver.resolveEntity( publicID, systemID );
+        if( null == inputSource )
+        {
+            final String message =
+                "Unable to locate schema with id " + publicID + "/" + systemID;
+            throw new Exception( message );
+        }
+
+        return create( schemaType, inputSource, resolver );
+    }
+
+    /**
+     * Create a ConfigValidator and guess Schema type. The schema is loaded
+     * from specified stream.
+     *
+     * @param inputStream the stream to load schema from
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final InputStream inputStream )
+        throws Exception
+    {
+        return create( null, inputStream );
+    }
+
+    /**
+     * Create a ConfigValidator with specified type. The schema is loaded
+     * from specified stream.
+     *
+     * @param schemaType the type of the schema.
+     *        (Usually a URL such as "http://relaxng.org/ns/structure/1.0")
+     * @param inputStream the stream to load schema from
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final String schemaType,
+                                          final InputStream inputStream )
+        throws Exception
+    {
+        if( null == inputStream )
+        {
+            throw new NullPointerException( "inputStream" );
+        }
+
+        final InputSource inputSource = new InputSource( inputStream );
+        return create( schemaType, inputSource );
+    }
+
+    /**
+     * Create a ConfigValidator with specified type. The schema is loaded
+     * from specified stream.
+     *
+     * @param schemaType the type of the schema.
+     *        (Usually a URL such as "http://relaxng.org/ns/structure/1.0")
+     * @param inputStream the stream to load schema from
+     * @param resolver a resolver used to resolve entitys for input data
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final String schemaType,
+                                          final InputStream inputStream,
+                                          final EntityResolver resolver )
+        throws Exception
+    {
+        if( null == inputStream )
+        {
+            throw new NullPointerException( "inputStream" );
+        }
+
+        final InputSource inputSource = new InputSource( inputStream );
+        return create( schemaType, inputSource, resolver );
+    }
+
+    /**
+     * Create a ConfigValidator and guess Schema type. The schema is loaded
+     * from specified source.
+     *
+     * @param inputSource the source to load schema from
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final InputSource inputSource )
+        throws Exception
+    {
+        return create( null, inputSource, null );
+    }
+
+    /**
+     * Create a ConfigValidator and guess Schema type. The schema is loaded
+     * from specified source.
+     *
+     * @param inputSource the source to load schema from
+     * @param resolver a resolver used to resolve entitys for input data
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final InputSource inputSource,
+                                          final EntityResolver resolver )
+        throws Exception
+    {
+        return create( null, inputSource, resolver );
+    }
+
+    /**
+     * Create a ConfigValidator with specified type. The schema is loaded
+     * from specified source.
+     *
+     * @param schemaType the type of the schema.
+     *        (Usually a URL such as "http://relaxng.org/ns/structure/1.0")
+     * @param inputSource the source to load schema from
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final String schemaType,
+                                          final InputSource inputSource )
+        throws Exception
+    {
+        return create( schemaType, inputSource, null );
+    }
+
+    /**
+     * Create a ConfigValidator with specified type. The schema is loaded
+     * from specified source. Also specified is entity resolver used when
+     * loading files to validate.
+     *
+     * @param schemaType the type of the schema.
+     *        (Usually a URL such as "http://relaxng.org/ns/structure/1.0")
+     * @param inputSource the source to load schema from
+     * @param entityResolver a resolver used to resolve entitys for input data
+     * @return the ConfigValidatorthat conforms to input
+     * @throws Exception if unable to create validator
+     */
+    public static ConfigValidator create( final String schemaType,
+                                          final InputSource inputSource,
+                                          final EntityResolver entityResolver )
+        throws Exception
+    {
+        if( null == inputSource )
+        {
+            throw new NullPointerException( "inputSource" );
+        }
+        final VerifierFactory factory = createFactory( schemaType );
+        final Schema schema = factory.compileSchema( inputSource );
+        return new ConfigValidator( schema, entityResolver );
+    }
+
+    /**
+     * Create a VerifierFactory according to specified schemaType.
+     * If the schemaType is not specified then
+     *
+     * against DTDs, XML Schema, RelaxNG, Relax or TREX.
+     *
+     * @param schemaType the type of schema (may be null)
+     * @return the VerifierFactory created
+     * @throws VerifierConfigurationException if unable to get factory
+     */
+    private static VerifierFactory createFactory( final String schemaType )
+        throws VerifierConfigurationException
+    {
+        if( null == schemaType )
+        {
+            try
+            {
+                if( null == c_msvClass )
+                {
+                    c_msvClass = Class.forName( "com.sun.msv.verifier.jarv.TheFactoryImpl" );
+                }
+                return (VerifierFactory)c_msvClass.newInstance();
+            }
+            catch( final Exception e )
+            {
+                final String message =
+                    "Unable to load MSV factory and thus can " +
+                    "not auto-discover schema type.";
+                throw new VerifierConfigurationException( message, e );
+            }
+        }
+        else
+        {
+            return VerifierFactory.newInstance( schemaType );
+        }
+    }
+}
