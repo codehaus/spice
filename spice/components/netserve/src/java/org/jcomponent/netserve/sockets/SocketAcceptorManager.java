cf3da7cbc3e22e7ba74c7f0c4f5d4fa9fd9cf7a3
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-08 02:00:22 +0000

    Add in service interfaces for SocketAcceptors

---
+++ spice/components/netserve/src/java/org/jcomponent/netserve/sockets/SocketAcceptorManager.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcomponent.netserve.sockets;
+
+import java.net.ServerSocket;
+
+/**
+ * This service is used to manage network acceptors.
+ * The service takes a ServerSocket and ConnectionHandler and
+ * anytime a new connection is accepted the handler is called
+ * with the new socket connection.
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-08 02:00:20 $
+ */
+public interface SocketAcceptorManager
+{
+    /**
+     * Start accepting connections from a socket and passing connections
+     * to specified handler.
+     *
+     * @param name the name of connection. This serves as a key used to
+     *        shutdown acceptor.
+     * @param socket the ServerSocket from which connections are accepted
+     * @throws Exception if unable to initiate connection management. This could
+     *         be due to the key already being used for another acceptor,
+     *        the serversocket being closed, the handler being null etc.
+     */
+    void connect( String name,
+                  ServerSocket socket,
+                  SocketConnectionHandler handler )
+        throws Exception;
+
+    /**
+     * This shuts down acceptor and the associated ServerSocket.
+     *
+     * @param name the name of connection
+     * @throws Exception if error occurs shutting down a handler or connection.
+     */
+    void disconnect( String name )
+        throws Exception;
+}
