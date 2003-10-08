ee94b0d9a3f343ae31d3575d60d3513a59fc6f5f
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-08 08:37:02 +0000

    Rework as the acceptor code moved to new acceptor service

--- spice/components/netserve/src/java/org/jcomponent/netserve/connection/impl/ConnectionAcceptor.java
+++ spice/components/netserve/src/java/org/jcomponent/netserve/connection/impl/ConnectionAcceptor.java
@@ -8,33 +8,28 @@
 package org.jcomponent.netserve.connection.impl;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 import java.util.Vector;
 import org.jcomponent.netserve.connection.ConnectionHandler;
 import org.jcomponent.netserve.connection.ConnectionHandlerManager;
+import org.jcomponent.netserve.sockets.SocketConnectionHandler;
 import org.jcomponent.threadpool.ThreadPool;
 
 /**
  * A helper class that manages acceptor for a single ServerSocket.
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.4 $ $Date: 2003-08-31 02:18:53 $
+ * @version $Revision: 1.5 $ $Date: 2003-10-08 08:37:02 $
  */
 class ConnectionAcceptor
-    implements Runnable
+    implements SocketConnectionHandler
 {
     /**
      * The name of the connection.
      */
     private final String m_name;
-
-    /**
-     * The ServerSocket that we are accepting connections from.
-     */
-    private final ServerSocket m_serverSocket;
 
     /**
      * The ConnectionHandlerManager that we create ConnectionHandlers from.
@@ -58,21 +53,9 @@ class ConnectionAcceptor
     private final List m_runners = new Vector();
 
     /**
-     * The thread in which the main accept loop is running.
-     * Setup at start of thread and set to null to shutdown
-     * acceptor.
-     */
-    private Thread m_thread;
-
-    /**
      * The id of next handler created. Used to decorate the names of connections.
      */
     private long m_id;
-
-    /**
-     * True when acceptor is finished.
-     */
-    private boolean m_done;
 
     /**
      * Create the acceptor.
@@ -105,7 +88,6 @@ class ConnectionAcceptor
             throw new NullPointerException( "monitor" );
         }
         m_name = name;
-        m_serverSocket = serverSocket;
         m_handlerManager = handlerManager;
         m_monitor = monitor;
         m_threadPool = threadPool;
@@ -121,83 +103,15 @@ class ConnectionAcceptor
      */
     void close( final int waitTime, final boolean forceShutdown )
     {
-        m_monitor.acceptorClosed( m_name );
-
         final ConnectionRunner[] runners;
         synchronized( this )
         {
-            stopRunning( waitTime, forceShutdown );
             runners =
                 (ConnectionRunner[])m_runners.toArray( new ConnectionRunner[ m_runners.size() ] );
         }
         for( int i = 0; i < runners.length; i++ )
         {
             runners[ i ].close( waitTime, forceShutdown );
-        }
-    }
-
-    /**
-     * The main accept & handle loop for acceptor.
-     */
-    public void run()
-    {
-        //Setup thread to indicate that we are currently running
-        synchronized( this )
-        {
-            m_thread = Thread.currentThread();
-        }
-        while( isRunning() )
-        {
-            m_monitor.serverSocketListening( m_name );
-            try
-            {
-                final Socket socket = m_serverSocket.accept();
-                if( !isRunning() )
-                {
-                    shutdown( socket );
-                }
-                else
-                {
-                    startConnection( socket );
-                }
-            }
-            catch( final InterruptedIOException iioe )
-            {
-                //Consume exception
-            }
-            catch( final IOException ioe )
-            {
-                final String message =
-                    "Exception accepting connection due to: " + ioe;
-                m_monitor.unexpectedError( message, ioe );
-            }
-        }
-
-        m_thread = null;
-        shutdownServerSocket();
-
-        synchronized( this )
-        {
-            notifyAll();
-        }
-    }
-
-    /**
-     * Utility method to shutdown serverSocket.
-     */
-    private synchronized void shutdownServerSocket()
-    {
-        m_monitor.serverSocketClosing( m_name );
-        m_done = true;
-        try
-        {
-            m_serverSocket.close();
-        }
-        catch( final IOException ioe )
-        {
-            final String message =
-                "Error closing ServerSocket on " + m_name + " due to: " + ioe;
-            m_monitor.unexpectedError( message, ioe );
         }
     }
 
@@ -219,14 +133,15 @@ class ConnectionAcceptor
      *
      * @param runner the ConnectionRunner
      */
-    void disposeRunner( final ConnectionRunner runner )
+    void disposeRunner( final ConnectionRunner runner,
+                        final String name )
     {
-        m_monitor.runnerDisposing( runner );
+        m_monitor.disposingHandler( name, runner.getSocket() );
         synchronized( this )
         {
             if( !m_runners.remove( runner ) )
             {
-                m_monitor.runnerAlreadyDisposed( runner );
+                m_monitor.errorHandlerAlreadyDisposed( name, runner.getSocket() );
                 return;
             }
         }
@@ -250,18 +165,18 @@ class ConnectionAcceptor
             }
             catch( final IOException ioe )
             {
-                final String message = "Error closing socket " + socket + ".";
-                m_monitor.unexpectedError( message, ioe );
+                m_monitor.errorClosingSocket( m_name, socket, ioe );
             }
         }
     }
 
     /**
-     * Start a connection on specified socket.
+     * Handle a socket on specified socket.
      *
      * @param socket the socket
      */
-    private void startConnection( final Socket socket )
+    public void handleConnection( final Socket socket )
+        throws IOException
     {
         final ConnectionHandler handler;
         try
@@ -270,24 +185,16 @@ class ConnectionAcceptor
         }
         catch( final Exception e )
         {
-            try
-            {
-                socket.close();
-            }
-            catch( IOException ioe )
-            {
-            }
-            final String message =
-                "Unable to create ConnectionHandler due to: " + e;
-            m_monitor.unexpectedError( message, e );
+            shutdown( socket );
+            m_monitor.errorAquiringHandler( m_name, e );
             return;
         }
 
         final String name = m_name + m_id;
         m_id++;
 
-        m_monitor.runnerCreating( name );
-        final ConnectionRunner runner = new ConnectionRunner( name, socket, handler, this, m_monitor );
+        final ConnectionRunner runner =
+            new ConnectionRunner( name, socket, handler, this, m_monitor );
         synchronized( this )
         {
             m_runners.add( runner );
@@ -298,54 +205,8 @@ class ConnectionAcceptor
         }
         else
         {
-            final Thread thread = new Thread( runner, name );
+            final Thread thread = new Thread( runner, toString() );
             thread.start();
         }
-    }
-
-    /**
-     * Utility method that tells the acceptor to stop running.
-     *
-     * @param waitTime the time to wait for graceful shutdown
-     * @param forceShutdown true if when we timeout we should forcefully
-     *                      shutdown connection
-     */
-    private synchronized void stopRunning( final int waitTime,
-                                           final boolean forceShutdown )
-    {
-        m_monitor.acceptorStopping( m_name );
-        m_done = true;
-        final Thread thread = m_thread;
-        m_thread = null;
-        if( null != thread )
-        {
-            thread.interrupt();
-        }
-
-        if( !m_serverSocket.isClosed() )
-        {
-            try
-            {
-                wait( waitTime );
-            }
-            catch( InterruptedException e )
-            {
-            }
-        }
-
-        if( forceShutdown && !m_serverSocket.isClosed() )
-        {
-            shutdownServerSocket();
-        }
-    }
-
-    /**
-     * Return true if the acceptor is currently running.
-     *
-     * @return true if the acceptor is currently running.
-     */
-    private synchronized boolean isRunning()
-    {
-        return !m_done;
     }
 }
