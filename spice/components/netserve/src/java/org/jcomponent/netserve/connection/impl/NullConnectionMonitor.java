8291b43c607aa73009f4a26c981c8e9d3a89e49e
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-08 07:41:12 +0000

    Fixup Null monitor

--- spice/components/netserve/src/java/org/jcomponent/netserve/connection/impl/NullConnectionMonitor.java
+++ spice/components/netserve/src/java/org/jcomponent/netserve/connection/impl/NullConnectionMonitor.java
@@ -7,61 +7,69 @@
  */
 package org.jcomponent.netserve.connection.impl;
 
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+
 /**
  * Noop implementation of ConnectionMonitor.
  *
  * @author <a href="mailto:mauro.talevi at aquilonia.org">Mauro Talevi</a>
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.4 $ $Date: 2003-10-08 07:41:12 $
  */
 class NullConnectionMonitor
     implements ConnectionMonitor
 {
-    public void acceptorClosed( final String name )
+    public void acceptorDisconnecting( String name, boolean tearDown )
     {
     }
 
-    public void acceptorCreated( final ConnectionAcceptor acceptor )
+    public void connectionEnding( String name, Socket socket )
     {
     }
 
-    public void acceptorDisconnected( final ConnectionAcceptor acceptor,
-                                      boolean tearDown )
+    public void connectionStarting( String name, Socket socket )
     {
     }
 
-    public void acceptorStopping( final String name )
+    public void disposingHandler( String name, Socket socket )
     {
     }
 
-    public void connectionEnding( final String name, final String hostAddress )
+    public void errorHandlerAlreadyDisposed( String name, Socket socket )
     {
     }
 
-    public void connectionStarting( final String name, final String hostAddress )
+    public void errorClosingSocket( String name, Socket socket, Exception e )
     {
     }
 
-    public void runnerAlreadyDisposed( final ConnectionRunner runner )
+    public void errorAquiringHandler( String name, Exception e )
     {
     }
 
-    public void runnerCreating( final String name )
+    public void errorHandlingConnection( String name, Socket socket, Exception e )
     {
     }
 
-    public void runnerDisposing( final ConnectionRunner runner )
+    public void acceptorCreated( String name, ServerSocket serverSocket )
     {
     }
 
-    public void serverSocketClosing( final String name )
+    public void acceptorClosing( String name, ServerSocket serverSocket )
     {
     }
 
-    public void serverSocketListening( final String name )
+    public void serverSocketListening( String name, ServerSocket serverSocket )
     {
     }
 
-    public void unexpectedError( final String message,
-                                 final Throwable t )
+    public void errorAcceptingConnection( String name, IOException ioe )
+    {
+    }
+
+    public void errorClosingServerSocket( String name, IOException ioe )
     {
     }
 }
