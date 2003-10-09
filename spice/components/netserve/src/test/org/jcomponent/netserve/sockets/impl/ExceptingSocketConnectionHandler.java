e81b051ce8932e4dcb84ec0606230e8dfc200a2c
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-09 08:30:59 +0000

    Add handler to test exception case

---
+++ spice/components/netserve/src/test/org/jcomponent/netserve/sockets/impl/ExceptingSocketConnectionHandler.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcomponent.netserve.sockets.impl;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import org.jcomponent.netserve.sockets.SocketConnectionHandler;
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-09 08:30:59 $
+ */
+class ExceptingSocketConnectionHandler
+    implements SocketConnectionHandler
+{
+    static final IOException EXCEPTION = new IOException( "You can not do that Dave!");
+
+    public void handleConnection( final Socket connection )
+        throws IOException
+    {
+        throw EXCEPTION;
+    }
+}
