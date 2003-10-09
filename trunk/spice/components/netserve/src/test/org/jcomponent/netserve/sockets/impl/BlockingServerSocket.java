a2bcdb9697cc3716e950fe66b5c729570b5dd989
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-09 02:39:46 +0000

    Add socket that will block unti told to unlock

---
+++ trunk/spice/components/netserve/src/test/org/jcomponent/netserve/sockets/impl/BlockingServerSocket.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcomponent.netserve.sockets.impl;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-09 02:39:46 $
+ */
+class BlockingServerSocket
+    extends ServerSocket
+{
+    static final MockSocket SOCKET = new MockSocket();
+
+    private int m_lockCount;
+    private int m_acceptCount;
+
+    public BlockingServerSocket()
+        throws IOException
+    {
+    }
+
+    public Socket accept()
+        throws IOException
+    {
+        m_acceptCount++;
+        while( true )
+        {
+            synchronized( this )
+            {
+                if( m_acceptCount <= m_lockCount )
+                {
+                    break;
+                }
+                try
+                {
+                    wait();
+                }
+                catch( InterruptedException e )
+                {
+                }
+            }
+        }
+        return SOCKET;
+    }
+
+    synchronized void unlock()
+    {
+        m_lockCount++;
+        notifyAll();
+    }
+}
