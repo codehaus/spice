53405ba6d0dfe16adce3c58965cba7f135b9ddde
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-09 08:16:35 +0000

    Make sure output is flushed and then close down the stream

--- trunk/spice/components/netserve/src/test/org/jcomponent/netserve/sockets/impl/ClosingSocketConnectionHandler.java
+++ trunk/spice/components/netserve/src/test/org/jcomponent/netserve/sockets/impl/ClosingSocketConnectionHandler.java
@@ -10,11 +10,12 @@ package org.jcomponent.netserve.sockets.impl;
 import org.jcomponent.netserve.sockets.SocketConnectionHandler;
 import java.net.Socket;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-10-09 07:54:01 $
+ * @version $Revision: 1.2 $ $Date: 2003-10-09 08:16:35 $
  */
 class ClosingSocketConnectionHandler
     implements SocketConnectionHandler
@@ -24,7 +25,12 @@ class ClosingSocketConnectionHandler
     public void handleConnection( final Socket connection )
         throws IOException
     {
-        connection.getOutputStream().write( MESSAGE.getBytes() );
+        final OutputStream outputStream = connection.getOutputStream();
+        outputStream.write( MESSAGE.getBytes() );
+        outputStream.flush();
+        outputStream.close();
+        connection.shutdownInput();
+        connection.shutdownOutput();
         connection.close();
     }
 }
