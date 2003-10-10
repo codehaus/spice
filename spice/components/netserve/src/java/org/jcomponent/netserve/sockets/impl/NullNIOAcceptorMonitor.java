5a077cd057cb93038a32a30076b25acd6ca110b0
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-10 02:44:48 +0000

    Add noop monitor

---
+++ spice/components/netserve/src/java/org/jcomponent/netserve/sockets/impl/NullNIOAcceptorMonitor.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcomponent.netserve.sockets.impl;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+/**
+ * A noop monitor.
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-10 02:44:48 $
+ */
+public class NullNIOAcceptorMonitor
+    implements NIOAcceptorMonitor
+{
+    /**
+     * @see NIOAcceptorMonitor#acceptorCreated
+     */
+    public void acceptorCreated( final String name,
+                                 final ServerSocket serverSocket )
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#acceptorClosing
+     */
+    public void acceptorClosing( final String name )
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#errorAcceptingConnection
+     */
+    public void errorAcceptingConnection( final String name,
+                                          final IOException ioe )
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#selectorShutdown
+     */
+    public void selectorShutdown()
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#errorClosingSelector
+     */
+    public void errorClosingSelector( final IOException ioe )
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#exitingSelectorLoop
+     */
+    public void exitingSelectorLoop()
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#enteringSelect
+     */
+    public void enteringSelect()
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#selectCompleted
+     */
+    public void selectCompleted( final int count )
+    {
+    }
+
+    /**
+     * @see NIOAcceptorMonitor#handlingConnection
+     */
+    public void handlingConnection( final String name,
+                                    final ServerSocket serverSocket,
+                                    final Socket socket )
+    {
+    }
+}
