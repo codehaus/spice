5ffdcde8ff9177ecd3e6cc100b0c79e9f51a2ea0
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-13 23:55:19 +0000

    Add Mock accessor to help during testing

---
+++ spice/sandbox/metaclass-jmx/src/test/org/realityforge/metaclass/jmx/MockAccessor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.metaclass.jmx;
+
+import org.realityforge.metaclass.introspector.MetaClassAccessor;
+import org.realityforge.metaclass.introspector.MetaClassException;
+import org.realityforge.metaclass.model.ClassDescriptor;
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-13 23:55:19 $
+ */
+class MockAccessor
+    implements MetaClassAccessor
+{
+    private final ClassDescriptor m_classDescriptor;
+
+    MockAccessor( final ClassDescriptor classDescriptor )
+    {
+        m_classDescriptor = classDescriptor;
+    }
+
+    public ClassDescriptor getClassDescriptor( String classname,
+                                               ClassLoader classLoader )
+        throws MetaClassException
+    {
+        return m_classDescriptor;
+    }
+}
