34babc637b0d9e02c68f33a1b985ea6b4031cb8f
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-14 04:12:43 +0000

    Update implementations to use underlying AcceptorManager

--- spice/components/netserve/src/java/org/jcomponent/netserve/connection/impl/PicoConnectionManager.java
+++ spice/components/netserve/src/java/org/jcomponent/netserve/connection/impl/PicoConnectionManager.java
@@ -8,11 +8,13 @@
 package org.jcomponent.netserve.connection.impl;
 
 import org.jcomponent.netserve.connection.ConnectionManager;
+import org.jcomponent.netserve.sockets.SocketAcceptorManager;
+import org.jcomponent.netserve.sockets.impl.DefaultAcceptorManager;
 import org.jcomponent.threadpool.ThreadPool;
 
 /**
- * An implementation of {@link ConnectionManager} which is PicoContainer compatible. 
- * 
+ * An implementation of {@link ConnectionManager} which is PicoContainer compatible.
+ *
  * <p>A sample of configuration parameters for the component are given below. Note that on some OS/JVM
  * combinations <tt>soTimeout</tt> must be set to non-0 value or else the ServerSocket will
  * never get out of accept() system call and we wont be able to shutdown the server
@@ -28,74 +30,71 @@ import org.jcomponent.threadpool.ThreadPool;
  * </ul>
  *
  * @author <a href="mailto:mauro.talevi at aquilonia.org">Mauro Talevi</a>
- * @version $Revision: 1.1 $ $Date: 2003-08-31 10:37:53 $
- * @phoenix.component
- * @phoenix.service type="ConnectionManager"
+ * @version $Revision: 1.2 $ $Date: 2003-10-14 04:12:43 $
  */
 public class PicoConnectionManager
     extends AbstractConnectionManager
     implements ConnectionManager
 {
-
     public static class Default
-       extends PicoConnectionManager
+        extends PicoConnectionManager
     {
-       public Default( final ThreadPool defaultThreadPool )
-       {
-          super( new NullConnectionMonitor(),
-                 defaultThreadPool,
-                 1000,
-                 false,
-                 200 );
-       }
+        public Default( final ThreadPool defaultThreadPool )
+        {
+            super( new NullConnectionMonitor(),
+                   defaultThreadPool,
+                   new DefaultAcceptorManager(),
+                   false,
+                   200 );
+        }
     }
 
     public static class WithMonitor
-       extends PicoConnectionManager
+        extends PicoConnectionManager
     {
-       public WithMonitor( final ConnectionMonitor monitor,
+        public WithMonitor( final ConnectionMonitor monitor,
                             final ThreadPool defaultThreadPool )
-       {
-          super( monitor,
-                 defaultThreadPool,
-                 1000,
-                 false,
-                 200 );
-       }
+        {
+            super( monitor,
+                   defaultThreadPool,
+                   new DefaultAcceptorManager(),
+                   false,
+                   200 );
+        }
     }
 
     public static class WithMonitorAndConfig
         extends PicoConnectionManager
     {
-       public WithMonitorAndConfig( final ConnectionMonitor monitor,
+        public WithMonitorAndConfig( final ConnectionMonitor monitor,
                                      final ThreadPool defaultThreadPool,
-                                     final int soTimeout, 
+                                     final SocketAcceptorManager acceptorManager,
                                      final boolean forceShutdown,
-                                     final int shutdownTimeout ) 
-       {
-            super( monitor, defaultThreadPool, soTimeout, forceShutdown, shutdownTimeout );
-       }
+                                     final int shutdownTimeout )
+        {
+            super( monitor, defaultThreadPool, acceptorManager, forceShutdown, shutdownTimeout );
+        }
     }
 
     /**
-     * Constructor.  
-     * 
+     * Constructor.
+     *
      * @param monitor the ConnectionMonitor
-     * @param defaultThreadPool the ThreadPool used as default is not set in the 
-     *         {@link ConnectionManager#connect(String name, ServerSocket socket, ConnectionHandlerManager handlerManager, ThreadPool threadPool )}
-     * @param soTimeout the value set for SO_TIMEOUT in milliseconds
+     * @param defaultThreadPool the ThreadPool used as default is not set in the
+     *         {@link ConnectionManager#connect}
+     * @param acceptorManager the AcceptorManager
      * @param forceShutdown boolean <code>true</code> if we need to force connections shutdown when they don't gracefully in specified time-period.
      * @param shutdownTimeout  the number of milliseconds to wait for connection to shutdown gracefully.
      */
     protected PicoConnectionManager( final ConnectionMonitor monitor,
-                                      final ThreadPool defaultThreadPool,
-                                      final int soTimeout, 
-                                      final boolean forceShutdown,
-                                      final int shutdownTimeout )
+                                     final ThreadPool defaultThreadPool,
+                                     final SocketAcceptorManager acceptorManager,
+                                     final boolean forceShutdown,
+                                     final int shutdownTimeout )
     {
         setMonitor( monitor );
+        setAcceptorManager( acceptorManager );
         setDefaultThreadPool( defaultThreadPool );
-        setSoTimeout( soTimeout );
         setForceShutdown( forceShutdown );
         setShutdownTimeout( shutdownTimeout );
     }
@@ -108,6 +107,4 @@ public class PicoConnectionManager
     {
         shutdownAcceptors();
     }
-
-
 }
