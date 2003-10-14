f99eaa9b6cf0f02d3bdf38397c86f6187acc6805
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-14 00:58:42 +0000

    Add other value

--- trunk/spice/sandbox/metaclass-jmx/src/test/org/realityforge/metaclass/jmx/TestBean.java
+++ trunk/spice/sandbox/metaclass-jmx/src/test/org/realityforge/metaclass/jmx/TestBean.java
@@ -10,11 +10,12 @@ package org.realityforge.metaclass.jmx;
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-10-14 00:42:13 $
+ * @version $Revision: 1.2 $ $Date: 2003-10-14 00:58:42 $
  */
 public class TestBean
 {
     private int m_value;
+    private int m_otherValue;
 
     public int getValue()
     {
@@ -24,5 +25,15 @@ public class TestBean
     public void setValue( int value )
     {
         m_value = value;
+    }
+
+    public int getOtherValue()
+    {
+        return m_otherValue;
+    }
+
+    public void setOtherValue( int otherValue )
+    {
+        m_otherValue = otherValue;
     }
 }
