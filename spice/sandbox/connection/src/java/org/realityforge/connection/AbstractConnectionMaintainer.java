74b0958a7d247eec5562f934d64bff232bb1915a
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-20 23:51:09 +0000

    Add some javadocs

--- spice/sandbox/connection/src/java/org/realityforge/connection/AbstractConnectionMaintainer.java
+++ spice/sandbox/connection/src/java/org/realityforge/connection/AbstractConnectionMaintainer.java
@@ -3,15 +3,31 @@ package org.realityforge.connection;
 public abstract class AbstractConnectionMaintainer
 {
    /**
-    * A Flag indicating whether the connection
+    * A flag indicating whether the connection
     * is "active".
     */
    private boolean _active;
 
+   /**
+    * Time at which last transmission occured.
+    */
    private long _lastTxTime;
+
+   /**
+    * The last message transmitted. May be null.
+    * Simply used to display status on the web page.
+    */
    private Object _lastTxMessage;
 
+   /**
+    * Time at which last receive occured.
+    */
    private long _lastRxTime;
+
+   /**
+    * The last message received. May be null.
+    * Simply used to display status on the web page.
+    */
    private Object _lastRxMessage;
 
 }
