27880f37076002b9ab18d859bd83a269e841781d
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-21 00:52:58 +0000

    Javadocs

--- spice/sandbox/connection/src/java/org/realityforge/connection/ConnectorMonitor.java
+++ spice/sandbox/connection/src/java/org/realityforge/connection/ConnectorMonitor.java
@@ -6,11 +6,33 @@ package org.realityforge.connection;
  */
 public interface ConnectorMonitor
 {
+   /**
+    * Notify that connection attempt about to start.
+    * The ability to "veto" the event is given
+    * by returning false.
+    *
+    * @return true to continue to attempt connection
+    */
    boolean attemptingConnection();
 
+   /**
+    * Notify monitor that connection has been established.
+    */
    void connectionEstablished();
 
+   /**
+    * Notify monitor that there was an error
+    * connecting.
+    *
+    * @param t the error
+    */
    void errorConnecting( Throwable t );
 
+   /**
+    * Notify monitor that there was an error
+    * disconnecting.
+    *
+    * @param t the error
+    */
    void errorDisconnecting( Throwable t );
 }
