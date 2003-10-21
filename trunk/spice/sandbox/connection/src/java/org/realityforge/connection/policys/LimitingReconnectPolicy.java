e7773d1b0a96eb6f828c0c3fb577c1a48b6f78d1
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-21 02:17:33 +0000

    Add in reconnection policy that does time delay limiting

---
+++ trunk/spice/sandbox/connection/src/java/org/realityforge/connection/policys/LimitingReconnectPolicy.java
@@ -0,0 +1,74 @@
+package org.realityforge.connection.policys;
+
+import org.realityforge.connection.ReconnectionPolicy;
+
+/**
+ * A Policy that will always attempt to limit
+ * the number of connection attempts in a
+ * period of time.
+ *
+ * <p>After N connection attempts it will force a
+ * delay of T between successive connection
+ * attempts. This is an attempt not to overload
+ * the resource being connected to.</p>
+ */
+public class LimitingReconnectPolicy
+   implements ReconnectionPolicy
+{
+   /**
+    * The number of attempts allowed before delay will kick in.
+    */
+   private final int _attempts;
+
+   /**
+    * The delay between successive connection attempts.
+    */
+   private final long _delay;
+
+   /**
+    * Create a policy instance.
+    *
+    * @param attempts the number of attempts before the delay is enabled.
+    * @param delay the delay
+    */
+   public LimitingReconnectPolicy( final int attempts,
+                                   final long delay )
+   {
+      _attempts = attempts;
+      _delay = delay;
+   }
+
+   /**
+    * @see ReconnectionPolicy#attemptConnection
+    */
+   public boolean attemptConnection( final long lastConnectionAttempt,
+                                     final int connectionAttempts )
+   {
+      if( connectionAttempts > _attempts )
+      {
+         final long now = System.currentTimeMillis();
+         final long nextAttempt = lastConnectionAttempt + _delay;
+         if( now < nextAttempt )
+         {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   /**
+    * @see ReconnectionPolicy#disconnectOnError
+    */
+   public boolean disconnectOnError( final Throwable t )
+   {
+      return true;
+   }
+
+   /**
+    * @see ReconnectionPolicy#reconnectOnDisconnect
+    */
+   public boolean reconnectOnDisconnect()
+   {
+      return true;
+   }
+}
