63618800ad46564961295b13f99e535152068b22
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-21 02:07:42 +0000

    Add in policy to always reconnect

---
+++ spice/sandbox/connection/src/java/org/realityforge/connection/policys/AlwaysReconnectPolicy.java
@@ -0,0 +1,40 @@
+package org.realityforge.connection.policys;
+
+import org.realityforge.connection.ReconnectionPolicy;
+
+/**
+ * A Policy that will always attempt to reconnect
+ * regardless of how many failures or errors.
+ */
+public class AlwaysReconnectPolicy
+   implements ReconnectionPolicy
+{
+   /**
+    * Constant containing instance of policy.
+    */
+   public static final AlwaysReconnectPolicy POLICY = new AlwaysReconnectPolicy();
+
+   /**
+    * @see ReconnectionPolicy#attemptConnection
+    */
+   public boolean attemptConnection( final int connectionAttempts )
+   {
+      return true;
+   }
+
+   /**
+    * @see ReconnectionPolicy#disconnectOnError
+    */
+   public boolean disconnectOnError( final Throwable t )
+   {
+      return false;
+   }
+
+   /**
+    * @see ReconnectionPolicy#reconnectOnDisconnect
+    */
+   public boolean reconnectOnDisconnect()
+   {
+      return true;
+   }
+}
