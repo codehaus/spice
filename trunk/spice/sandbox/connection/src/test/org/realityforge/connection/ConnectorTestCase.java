50ba26fbf3d09d3075fe96e4539f3887d5285248
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-21 03:27:21 +0000

    Tests away

--- trunk/spice/sandbox/connection/src/test/org/realityforge/connection/ConnectorTestCase.java
+++ trunk/spice/sandbox/connection/src/test/org/realityforge/connection/ConnectorTestCase.java
@@ -68,6 +68,22 @@ public class ConnectorTestCase
       assertEquals( "connection", connection, connector.getConnection() );
    }
 
+   public void testGetConnectionPreSet()
+      throws Exception
+   {
+      final Connector connector = new Connector();
+      try
+      {
+         connector.getConnection();
+      }
+      catch ( final NullPointerException npe )
+      {
+         assertEquals( "npe.message", "connection", npe.getMessage() );
+         return;
+      }
+      fail( "Expected to fail due to NPE" );
+   }
+
    public void testSetNullConnection()
       throws Exception
    {
@@ -82,5 +98,48 @@ public class ConnectorTestCase
          return;
       }
       fail( "Expected to fail due to NPE" );
+   }
+
+   public void testTransmissionOccured()
+      throws Exception
+   {
+      final Connector connector = new Connector();
+      final Object message = new Object();
+      assertEquals( "time", 0, connector.getLastTxTime() );
+      assertEquals( "message", null, connector.getLastTxMessage() );
+      connector.transmissionOccured( message );
+      final long now = System.currentTimeMillis();
+      assertEquals( "time", now, connector.getLastTxTime(), 5.0 );
+      assertEquals( "message", message, connector.getLastTxMessage() );
+   }
+
+   public void testReceiveOccured()
+      throws Exception
+   {
+      final Connector connector = new Connector();
+      final Object message = new Object();
+      assertEquals( "time", 0, connector.getLastRxTime() );
+      assertEquals( "message", null, connector.getLastRxMessage() );
+      connector.receiveOccured( message );
+      final long now = System.currentTimeMillis();
+      assertEquals( "time", now, connector.getLastRxTime(), 5.0 );
+      assertEquals( "message", message, connector.getLastRxMessage() );
+   }
+
+   public void testCommOccured()
+      throws Exception
+   {
+      final Connector connector = new Connector();
+      final Object message = new Object();
+      assertEquals( "rx time", 0, connector.getLastRxTime() );
+      assertEquals( "rx message", null, connector.getLastRxMessage() );
+      assertEquals( "tx time", 0, connector.getLastTxTime() );
+      assertEquals( "tx message", null, connector.getLastTxMessage() );
+      connector.commOccured( message );
+      final long now = System.currentTimeMillis();
+      assertEquals( "time", now, connector.getLastRxTime(), 5.0 );
+      assertEquals( "tx vs rx time", connector.getLastTxTime(), connector.getLastRxTime() );
+      assertEquals( "rx message", message, connector.getLastRxMessage() );
+      assertEquals( "tx message", message, connector.getLastTxMessage() );
    }
 }
