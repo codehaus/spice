a93a9bf7bd84d23558d7835db45cb2a62661fa50
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-21 01:53:18 +0000

    Connector no longer needs to be abstract

--- trunk/spice/sandbox/connection/src/java/org/realityforge/connection/AbstractConnector.java
+++ trunk/spice/sandbox/connection/src/java/org/realityforge/connection/AbstractConnector.java
@@ -8,7 +8,7 @@ package org.realityforge.connection;
  *
  * @mx.component
  */
-public abstract class AbstractConnector
+public class AbstractConnector
 {
    /**
     * The associated reconnection policy for connector.
