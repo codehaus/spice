cd9b637be10884f916155d5b6c3300b11e25f2a6
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-22 00:59:01 +0000

    Javadoc

--- spice/sandbox/connector/src/java/org/realityforge/connector/NeverPingPolicy.java
+++ spice/sandbox/connector/src/java/org/realityforge/connector/NeverPingPolicy.java
@@ -1,13 +1,22 @@
 package org.realityforge.connector;
 
+/**
+ * Ping policy for never performing ping.
+ */
 public class NeverPingPolicy
    implements PingPolicy
 {
+   /**
+    * @see PingPolicy#checkPingConnection
+    */
    public boolean checkPingConnection( Connector connector )
    {
       return false;
    }
 
+   /**
+    * @see PingPolicy#nextPingCheck
+    */
    public long nextPingCheck( final long lastPingTime )
    {
       return Long.MAX_VALUE;
