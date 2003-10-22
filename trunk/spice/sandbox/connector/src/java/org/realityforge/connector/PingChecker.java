6e0ed43c8d179bb4911fcd51bb41b594f4c5b302
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-22 00:50:31 +0000

    Add in first cut of interface to determine when pinging occurs

---
+++ trunk/spice/sandbox/connector/src/java/org/realityforge/connector/PingChecker.java
@@ -0,0 +1,24 @@
+package org.realityforge.connector;
+
+/**
+ * Callback used to determine whether
+ * to ping a Connection.
+ */
+public interface PingChecker
+{
+   /**
+    * Return true if need to ping connection.
+    *
+    * @param connector the associated Connector
+    * @return true if need to ping connection.
+    */
+   boolean checkPingConnection( Connector connector );
+
+   /**
+    * Return the time at which the next ping should occur.
+    *
+    * @param lastPingTime the time of last ping
+    * @return the time that ping should be checked
+    */
+   long nextPingCheck( long lastPingTime );
+}
