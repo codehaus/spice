9a938995fac21b236d0cedfbb519c359fc1b2bed
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-22 00:58:05 +0000

    Update javadocs

--- trunk/spice/sandbox/connector/src/java/org/realityforge/connector/PingPolicy.java
+++ trunk/spice/sandbox/connector/src/java/org/realityforge/connector/PingPolicy.java
@@ -16,6 +16,8 @@ public interface PingPolicy
 
    /**
     * Return the time at which the next ping should occur.
+    * If the value {@link Long#MAX_VALUE} is returned
+    * then no pinging will ever occur.
     *
     * @param lastPingTime the time of last ping
     * @return the time that ping should be checked
