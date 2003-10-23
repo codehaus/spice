90a443299139a91e7b18181d8adfec7684f071be
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-23 06:10:49 +0000

    Unit test more conditions

--- trunk/spice/components/netserve/src/test/org/jcomponent/netserve/selector/SelectorManagerTestCase.java
+++ trunk/spice/components/netserve/src/test/org/jcomponent/netserve/selector/SelectorManagerTestCase.java
@@ -1,5 +1,9 @@
 package org.jcomponent.netserve.selector;
 
+import java.nio.channels.Selector;
+
+import com.mockobjects.dynamic.C;
+import com.mockobjects.dynamic.Mock;
 import junit.framework.TestCase;
 
 public class SelectorManagerTestCase
@@ -58,8 +62,19 @@ public class SelectorManagerTestCase
    public void testStartupAndShutdownSelectorManager()
       throws Exception
    {
+      final Mock mockMonitor = new Mock( SelectorMonitor.class );
+
+      mockMonitor.expect( "selectorStartup", C.NO_ARGS );
+      mockMonitor.expect( "enteringSelectorLoop", C.NO_ARGS );
+      mockMonitor.expect( "enteringSelect", C.NO_ARGS );
+      mockMonitor.expect( "selectCompleted", C.args( C.eq( 0 ) ) );
+      mockMonitor.expect( "exitingSelectorLoop", C.NO_ARGS );
+      mockMonitor.expect( "selectorShutdown", C.NO_ARGS );
+      final SelectorMonitor monitor = (SelectorMonitor) mockMonitor.proxy();
+
       final SelectorManager manager = new SelectorManager();
-      manager.setTimeout( 400 );
+      manager.setTimeout( 4000 );
+      manager.setMonitor( monitor );
       assertEquals( "isRunning pre start", false, manager.isRunning() );
       assertNullSelector( manager );
       manager.startup();
@@ -69,18 +84,49 @@ public class SelectorManagerTestCase
       manager.shutdown();
       assertNullSelector( manager );
       assertEquals( "isRunning post shutdown", false, manager.isRunning() );
+
+      mockMonitor.verify();
    }
 
    public void testShutdownSelectorManagerWithoutStatrup()
       throws Exception
    {
+      final Mock mockMonitor = new Mock( SelectorMonitor.class );
+      final SelectorMonitor monitor = (SelectorMonitor) mockMonitor.proxy();
+
       final SelectorManager manager = new SelectorManager();
+      manager.setMonitor( monitor );
       manager.setTimeout( 400 );
       assertEquals( "isRunning pre shutdown", false, manager.isRunning() );
       assertNullSelector( manager );
       manager.shutdown();
       assertNullSelector( manager );
       assertEquals( "isRunning post shutdown", false, manager.isRunning() );
+
+      mockMonitor.verify();
+   }
+
+
+   public void testShutdownSelectorInErrorState()
+      throws Exception
+   {
+      final Mock mockMonitor = new Mock( SelectorMonitor.class );
+
+      mockMonitor.expect( "selectorShutdown", C.NO_ARGS );
+      mockMonitor.expect( "errorClosingSelector", C.args( C.eq( MockSelector.EXCEPTION ) ) );
+      final SelectorMonitor monitor = (SelectorMonitor) mockMonitor.proxy();
+
+      final SelectorManager manager = new SelectorManager();
+      manager.setTimeout( 4000 );
+      manager.setMonitor( monitor );
+
+      final Selector selector = new MockSelector();
+      System.out.println( "selector = " + selector );
+      manager.setSelector( selector );
+      manager.shutdownSelector();
+      assertNotNull( "getSelector post start", manager.getSelector() );
+
+      mockMonitor.verify();
    }
 
    private void assertNullSelector( final SelectorManager manager )
