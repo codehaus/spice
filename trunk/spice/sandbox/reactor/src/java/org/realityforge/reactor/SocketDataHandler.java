7603ebaeb2d1100e2e9cdb1d69169e89704c5e4f
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-23 01:19:50 +0000

    Add in initial cut of data handler

---
+++ trunk/spice/sandbox/reactor/src/java/org/realityforge/reactor/SocketDataHandler.java
@@ -0,0 +1,9 @@
+package org.realityforge.reactor;
+
+import java.nio.channels.SocketChannel;
+
+public interface SocketDataHandler
+{
+   void dataReceived( String name, SocketChannel channel, Object passBack );
+   void dataTransmitted( String name, SocketChannel channel, Object passBack );
+}
