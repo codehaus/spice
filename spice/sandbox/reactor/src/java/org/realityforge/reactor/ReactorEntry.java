7542f96b5c69590950e1a87e1a420c880f7bc207
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-23 01:27:10 +0000

    Add in reactor entry holder

---
+++ spice/sandbox/reactor/src/java/org/realityforge/reactor/ReactorEntry.java
@@ -0,0 +1,54 @@
+package org.realityforge.reactor;
+
+import java.nio.channels.SocketChannel;
+
+public class ReactorEntry
+{
+   private final String _name;
+   private final SocketChannel _channel;
+   private final SocketDataHandler _handler;
+   private final Object _passback;
+
+   public ReactorEntry( final String name,
+                        final SocketChannel channel,
+                        final SocketDataHandler handler,
+                        final Object passback )
+   {
+      if ( null == name )
+      {
+         throw new NullPointerException( "name" );
+      }
+      if ( null == channel )
+      {
+         throw new NullPointerException( "channel" );
+      }
+      if ( null == handler )
+      {
+         throw new NullPointerException( "handler" );
+      }
+      _name = name;
+      _channel = channel;
+      _handler = handler;
+      _passback = passback;
+   }
+
+   public String getName()
+   {
+      return _name;
+   }
+
+   public SocketChannel getChannel()
+   {
+      return _channel;
+   }
+
+   public SocketDataHandler getHandler()
+   {
+      return _handler;
+   }
+
+   public Object getPassback()
+   {
+      return _passback;
+   }
+}
