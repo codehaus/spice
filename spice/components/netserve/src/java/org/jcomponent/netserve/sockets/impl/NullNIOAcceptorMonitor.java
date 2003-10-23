b536fec61201d5af5b61a95fcb9d5687e957ad05
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-23 05:07:42 +0000

    Extend NullSelectorMonitor which already noops all selector messages

--- spice/components/netserve/src/java/org/jcomponent/netserve/sockets/impl/NullNIOAcceptorMonitor.java
+++ spice/components/netserve/src/java/org/jcomponent/netserve/sockets/impl/NullNIOAcceptorMonitor.java
@@ -11,84 +11,52 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
 
+import org.jcomponent.netserve.selector.NullSelectorMonitor;
+
 /**
  * A noop monitor.
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.3 $ $Date: 2003-10-10 04:06:12 $
+ * @version $Revision: 1.4 $ $Date: 2003-10-23 05:07:42 $
  */
 public class NullNIOAcceptorMonitor
-    implements NIOAcceptorMonitor
+   extends NullSelectorMonitor
+   implements NIOAcceptorMonitor
 {
-    /**
-     * Add constant for instance of Null Monitor.
-     */
-    public static final NullNIOAcceptorMonitor MONITOR = new NullNIOAcceptorMonitor();
+   /**
+    * Add constant for instance of Null Monitor.
+    */
+   public static final NullNIOAcceptorMonitor MONITOR = new NullNIOAcceptorMonitor();
 
-    /**
-     * @see NIOAcceptorMonitor#acceptorCreated
-     */
-    public void acceptorCreated( final String name,
-                                 final ServerSocket serverSocket )
-    {
-    }
+   /**
+    * @see NIOAcceptorMonitor#acceptorCreated
+    */
+   public void acceptorCreated( final String name,
+                                final ServerSocket serverSocket )
+   {
+   }
 
-    /**
-     * @see NIOAcceptorMonitor#acceptorClosing
-     */
-    public void acceptorClosing( final String name )
-    {
-    }
+   /**
+    * @see NIOAcceptorMonitor#acceptorClosing
+    */
+   public void acceptorClosing( final String name )
+   {
+   }
 
-    /**
-     * @see NIOAcceptorMonitor#errorAcceptingConnection
-     */
-    public void errorAcceptingConnection( final String name,
-                                          final IOException ioe )
-    {
-    }
+   /**
+    * @see NIOAcceptorMonitor#errorAcceptingConnection
+    */
+   public void errorAcceptingConnection( final String name,
+                                         final IOException ioe )
+   {
+   }
 
-    /**
-     * @see NIOAcceptorMonitor#selectorShutdown
-     */
-    public void selectorShutdown()
-    {
-    }
-
-    /**
-     * @see NIOAcceptorMonitor#errorClosingSelector
-     */
-    public void errorClosingSelector( final IOException ioe )
-    {
-    }
-
-    /**
-     * @see NIOAcceptorMonitor#exitingSelectorLoop
-     */
-    public void exitingSelectorLoop()
-    {
-    }
-
-    /**
-     * @see NIOAcceptorMonitor#enteringSelect
-     */
-    public void enteringSelect()
-    {
-    }
-
-    /**
-     * @see NIOAcceptorMonitor#selectCompleted
-     */
-    public void selectCompleted( final int count )
-    {
-    }
-
-    /**
-     * @see NIOAcceptorMonitor#handlingConnection
-     */
-    public void handlingConnection( final String name,
-                                    final ServerSocket serverSocket,
-                                    final Socket socket )
-    {
-    }
+   /**
+    * @see NIOAcceptorMonitor#handlingConnection
+    */
+   public void handlingConnection( final String name,
+                                   final ServerSocket serverSocket,
+                                   final Socket socket )
+   {
+   }
 }
