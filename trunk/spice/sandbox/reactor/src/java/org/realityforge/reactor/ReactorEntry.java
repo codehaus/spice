3d2f90925e4b2b1d5e09ad674250c92bd48b0b6a
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-23 01:32:19 +0000

    Javadoc

--- trunk/spice/sandbox/reactor/src/java/org/realityforge/reactor/ReactorEntry.java
+++ trunk/spice/sandbox/reactor/src/java/org/realityforge/reactor/ReactorEntry.java
@@ -2,13 +2,42 @@ package org.realityforge.reactor;
 
 import java.nio.channels.SocketChannel;
 
+/**
+ * A class containing data relevent for a particular
+ * connection.
+ */
 class ReactorEntry
 {
+   /**
+    * Arbitary name associated with conenction.
+    */
    private final String _name;
+
+   /**
+    * The channel associated with connection.
+    */
    private final SocketChannel _channel;
+
+   /**
+    * The handler that gets called when data
+    * is either transmitted or received.
+    */
    private final SocketDataHandler _handler;
+
+   /**
+    * A object passed back to the handler specified
+    * by client when registering entry.
+    */
    private final Object _passback;
 
+   /**
+    * Create an instance of ReactorEntry.
+    *
+    * @param name the name
+    * @param channel the channel
+    * @param handler the handler
+    * @param passback the passback object
+    */
    public ReactorEntry( final String name,
                         final SocketChannel channel,
                         final SocketDataHandler handler,
@@ -32,21 +61,41 @@ class ReactorEntry
       _passback = passback;
    }
 
+   /**
+    * Return the name associated with connection.
+    *
+    * @return the name associated with connection.
+    */
    public String getName()
    {
       return _name;
    }
 
+   /**
+    * Return the channel that represents connection.
+    *
+    * @return the channel that represents connection.
+    */
    public SocketChannel getChannel()
    {
       return _channel;
    }
 
+   /**
+    * Return the associated handler.
+    *
+    * @return the associated handler.
+    */
    public SocketDataHandler getHandler()
    {
       return _handler;
    }
 
+   /**
+    * Return the user specified passback object.
+    *
+    * @return the user specified passback object.
+    */
    public Object getPassback()
    {
       return _passback;
