ad9cf68d85b648868f9b8efd3cae21614d83d975
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-24 08:19:21 +0000

    Extract superclass for all request handlers

---
+++ trunk/spice/components/netserve/src/java/org/jcomponent/netserve/connection/handlers/AbstractRequestHandler.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcomponent.netserve.connection.handlers;
+
+import org.jcomponent.netserve.connection.ConnectionHandler;
+import java.net.Socket;
+import java.io.IOException;
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-24 08:19:21 $
+ */
+public abstract class AbstractRequestHandler
+    implements ConnectionHandler
+{
+    /**
+     * Handle a connection.
+     *
+     * @param socket the socket
+     */
+    public void handleConnection( Socket socket )
+    {
+        performRequest( socket );
+    }
+
+    /**
+     * Perform the request for socket by delegating to
+     * underlying handler.
+     *
+     * @param socket the socket to handle
+     */
+    protected void performRequest( final Socket socket )
+    {
+        setupThreadName( socket );
+        try
+        {
+            doPerformRequest( socket );
+        }
+        catch( final Throwable t )
+        {
+            errorHandlingConnection( socket, t );
+        }
+        finally
+        {
+            endConnection( socket );
+        }
+    }
+
+    protected abstract void doPerformRequest( Socket socket )
+        throws Exception;
+
+    /**
+     * Setup the name of the thread.
+     *
+     * @param socket the socket associated with request
+     */
+    protected void setupThreadName( final Socket socket )
+    {
+        final String name = getThreadName( socket );
+        Thread.currentThread().setName( name );
+    }
+
+    /**
+     * End connection for socket.
+     *
+     * @param socket the socket
+     */
+    protected void endConnection( final Socket socket )
+    {
+        if( socket.isConnected() )
+        {
+            try
+            {
+                socket.close();
+            }
+            catch( final IOException ioe )
+            {
+                errorClosingConnection( socket, ioe );
+            }
+        }
+    }
+
+    /**
+     * Create Runnable to perform the request.
+     *
+     * @param socket the socket to handle
+     * @return thee runnable
+     */
+    protected Runnable createRunnable( final Socket socket )
+    {
+        return new Runnable()
+        {
+            public void run()
+            {
+                performRequest( socket );
+            }
+        };
+    }
+
+    /**
+     * Return the name should be set for current thread.
+     *
+     * @param socket the socket being handled in thread
+     * @return the thread name.
+     */
+    protected String getThreadName( final Socket socket )
+    {
+        return "RequestHandler for " +
+            socket.getInetAddress().getHostAddress() + ":" +
+            socket.getPort();
+    }
+
+    /**
+     * Notify handler of an error handling socket.
+     *
+     * @param socket the socket
+     * @param t the error
+     */
+    protected void errorHandlingConnection( final Socket socket,
+                                            final Throwable t )
+    {
+    }
+
+    /**
+     * Notify handler of an error closing socket.
+     *
+     * @param socket the socket
+     * @param t the error
+     */
+    protected void errorClosingConnection( final Socket socket,
+                                           final Throwable t )
+    {
+        errorHandlingConnection( socket, t );
+    }
+}
