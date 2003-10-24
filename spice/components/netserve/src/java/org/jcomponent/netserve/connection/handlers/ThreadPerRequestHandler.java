da22cff942db844babe9a25fe5c4e05c9146e44e
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-24 08:10:58 +0000

    Spec out

--- spice/components/netserve/src/java/org/jcomponent/netserve/connection/handlers/ThreadPerRequestHandler.java
+++ spice/components/netserve/src/java/org/jcomponent/netserve/connection/handlers/ThreadPerRequestHandler.java
@@ -9,11 +9,13 @@ package org.jcomponent.netserve.connection.handlers;
 
 import org.jcomponent.netserve.connection.ConnectionHandlerManager;
 import org.jcomponent.threadpool.ThreadPool;
+import java.net.Socket;
 
 /**
+ * A Handler that uses a thread from a pool for each different request.
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-10-24 08:05:21 $
+ * @version $Revision: 1.2 $ $Date: 2003-10-24 08:10:58 $
  */
 public class ThreadPerRequestHandler
     extends DefaultRequestHandler
@@ -23,6 +25,12 @@ public class ThreadPerRequestHandler
      */
     private final ThreadPool m_threadPool;
 
+    /**
+     * Create handler.
+     *
+     * @param handlerManager the underlying handler
+     * @param threadPool the thread pool to use to create handler threads
+     */
     public ThreadPerRequestHandler( final ConnectionHandlerManager handlerManager,
                                     final ThreadPool threadPool )
     {
@@ -32,5 +40,16 @@ public class ThreadPerRequestHandler
             throw new NullPointerException( "threadPool" );
         }
         m_threadPool = threadPool;
+    }
+
+    /**
+     * Execute each request in a separate thread.
+     *
+     * @param socket the socket to handle
+     */
+    public void handleConnection( final Socket socket )
+    {
+        final Runnable runnable = createRunnable( socket );
+        m_threadPool.execute( runnable );
     }
 }
