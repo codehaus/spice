54bc65e0e80630f00c9337e2357a292c519c0439
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-26 11:02:19 +0000

    Fix javadocs

--- spice/sandbox/metaclass/src/java/org/realityforge/metaclass/introspector/DefaultMetaClassAccessor.java
+++ spice/sandbox/metaclass/src/java/org/realityforge/metaclass/introspector/DefaultMetaClassAccessor.java
@@ -15,27 +15,28 @@ import org.realityforge.metaclass.model.ClassDescriptor;
 
 /**
  * This is the default mechanism for loading ClassDescriptor
- * and PackageDescriptor objects. This class follows the following
- * steps to locate the ClassDescriptor.
+ * objects. This class follows the following steps to locate
+ * the ClassDescriptor.
  *
  * <ul>
- *   <li>Look for a file with extension ".mad" sitting side-by-side
- *       class file. ie If class is named com.biz.Foo it has a class
- *       file named <tt>com/biz/Foo.class</tt> and a Attributes file
- *       named <tt>com/biz/Foo.mad</tt>. If found load descriptor
- *       from said file assuming binary format.</li>
+ *   <li>Look for a file with extension "-meta.binary" sitting
+ *       side-by-side the class file. ie If class is named
+ *       com.biz.Foo it has a class file named
+ *       <tt>com/biz/Foo.class</tt> and an descriptor file
+ *       named <tt>com/biz/Foo-meta.binary</tt>. Assume the
+ *       descriptor is in binary format.</li>
+ *   <li>Look for a file with extension "-meta.xml" sitting
+ *       side-by-side the class file. ie If class is named
+ *       com.biz.Foo it has a class file named
+ *       <tt>com/biz/Foo.class</tt> and an descriptor file
+ *       named <tt>com/biz/Foo-meta.xml</tt>. Assume the
+ *       descriptor is in xml format.</li>
  *   <li>If unable to locate descriptor throw a
  *       <tt>MetaClassException</tt>.</li>
  * </ul>
  *
- * <p>Similar steps are followed to locate the PackageDescriptor
- * except that ".package" is postfixed to the package name prior
- * to following the above steps. ie the package <tt>com.biz</tt>
- * may result in loading the file <tt>com/biz/package.mad</tt> to
- * look for attributes.</p>
- *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.3 $ $Date: 2003-10-04 01:41:12 $
+ * @version $Revision: 1.4 $ $Date: 2003-10-26 11:02:19 $
  */
 public class DefaultMetaClassAccessor
     implements MetaClassAccessor
