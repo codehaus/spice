3309127917a6e8380a45f25db11c6d128ca5dcc7
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-28 00:34:36 +0000

    Unit test monitor setting

--- spice/components/netserve/src/test/org/jcomponent/netserve/connection/impl/DNAAcceptorManagerTestCase.java
+++ spice/components/netserve/src/test/org/jcomponent/netserve/connection/impl/DNAAcceptorManagerTestCase.java
@@ -2,6 +2,7 @@ package org.jcomponent.netserve.connection.impl;
 
 import junit.framework.TestCase;
 import org.jcontainer.dna.impl.DefaultConfiguration;
+import org.jcontainer.dna.impl.ConsoleLogger;
 
 public class DNAAcceptorManagerTestCase
    extends TestCase
@@ -10,10 +11,17 @@ public class DNAAcceptorManagerTestCase
       throws Exception
    {
       final DNAAcceptorManager manager = new DNAAcceptorManager();
-      final DefaultConfiguration root = new DefaultConfiguration("root","","");
-      final DefaultConfiguration configuration = new DefaultConfiguration("shutdownTimeout","","");
+      final DefaultConfiguration root = new DefaultConfiguration( "root", "", "" );
+      final DefaultConfiguration configuration = new DefaultConfiguration( "shutdownTimeout", "", "" );
       configuration.setValue( "22" );
       root.addChild( configuration );
+
+      final ConsoleLogger logger = new ConsoleLogger( ConsoleLogger.LEVEL_NONE );
+      manager.enableLogging( logger );
+      final AcceptorMonitor monitor = manager.getMonitor();
+      assertTrue( "getMonitor() instanceof DNAAcceptorMonitor", monitor instanceof DNAAcceptorMonitor );
+      final DNAAcceptorMonitor dnaMonitor = (DNAAcceptorMonitor) monitor;
+      assertEquals( "logger", logger, dnaMonitor.getLogger() );
       manager.configure( root );
       assertEquals( "getShutdownTimeout()", 22, manager.getShutdownTimeout() );
       manager.initialize();
