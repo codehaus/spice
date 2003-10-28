e90c8b72f7c12668d09f4bbaaf9ff6bf25fecb73
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-28 00:34:57 +0000

    *** empty log message ***

--- spice/components/netserve/src/test/org/jcomponent/netserve/connection/impl/DefaultAcceptorManagerTestCase.java
+++ spice/components/netserve/src/test/org/jcomponent/netserve/connection/impl/DefaultAcceptorManagerTestCase.java
@@ -7,15 +7,22 @@
  */
 package org.jcomponent.netserve.connection.impl;
 
-import org.jcomponent.netserve.connection.SocketAcceptorManager;
+import java.net.Socket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Random;
+
+import junit.framework.TestCase;
 
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.2 $ $Date: 2003-10-27 23:55:11 $
+ * @version $Revision: 1.3 $ $Date: 2003-10-28 00:34:57 $
  */
 public class DefaultAcceptorManagerTestCase
-   extends AbstractAcceptorManagerTestCase
+   extends TestCase
 {
    public void testConnectAndDisconnectWithTimeoutNotSet()
       throws Exception
@@ -73,10 +80,51 @@ public class DefaultAcceptorManagerTestCase
       assertEquals( "isConnected post disconnect", false, manager.isConnected( name ) );
    }
 
+   public void testConnectAndAcceptAndDisconnectWithTimeoutSet()
+      throws Exception
+   {
+      final DefaultAcceptorManager manager = new DefaultAcceptorManager();
+      manager.setMonitor( NullAcceptorMonitor.MONITOR );
+      manager.setSoTimeout( 10 );
+      final String name = "name";
+      assertEquals( "isConnected pre connect", false, manager.isConnected( name ) );
+      final ServerSocketChannel channel = ServerSocketChannel.open();
+      final ServerSocket serverSocket = channel.socket();
+      serverSocket.setReuseAddress( true );
+      final InetAddress localAddress = InetAddress.getLocalHost();
+      final Random random = new Random();
+      final int port = Math.abs( random.nextInt() % 5000 ) + 1024;
+      final InetSocketAddress address = new InetSocketAddress( localAddress, port );
+      serverSocket.bind( address );
+      while ( !serverSocket.isBound() )
+      {
+         System.out.print( "." );
+         Thread.yield();
+      }
+
+      manager.connect( name,
+                       serverSocket,
+                       new MockSocketConnectionHandler() );
+
+      final Socket clientSocket = new Socket( localAddress, port );
+      System.out.print( "Socket Connecting to Key with InvalidAttachment" );
+      while ( !clientSocket.isConnected() )
+      {
+         System.out.print( "." );
+         Thread.yield();
+      }
+      System.out.println( " - Connected" );
+      clientSocket.close();
+
+      assertEquals( "isConnected pre disconnect", true, manager.isConnected( name ) );
+      manager.disconnect( name );
+      assertEquals( "isConnected post disconnect", false, manager.isConnected( name ) );
+   }
+
    public void testDuplicateConnect()
       throws Exception
    {
-      final SocketAcceptorManager manager = createAcceptorManager();
+      final DefaultAcceptorManager manager = new DefaultAcceptorManager();
       final String name = "name";
       assertEquals( "isConnected pre connect", false, manager.isConnected( name ) );
       final ExceptOnAcceptServerSocket socket = new ExceptOnAcceptServerSocket( true );
@@ -97,19 +145,94 @@ public class DefaultAcceptorManagerTestCase
       finally
       {
          socket.close();
-         shutdownAcceptorManager( manager );
+         manager.shutdownAcceptors();
          assertEquals( "isConnected post disconnect", false, manager.isConnected( name ) );
       }
       fail( "Expected to fail due to duplicate connect" );
    }
 
-   protected SocketAcceptorManager createAcceptorManager()
+   public void testDisconnectNonExistent()
+      throws Exception
    {
-      return new DefaultAcceptorManager();
+      final DefaultAcceptorManager manager = new DefaultAcceptorManager();
+      try
+      {
+         manager.disconnect( "NonExistent" );
+      }
+      catch ( IllegalArgumentException iae )
+      {
+         return;
+      }
+      finally
+      {
+         manager.shutdownAcceptors();
+      }
+      fail( "Expected to fail to disconnect non existent acceptor" );
    }
 
-   protected void shutdownAcceptorManager( final SocketAcceptorManager manager )
+   public void testConnectWithNullName()
+      throws Exception
    {
-      ( (DefaultAcceptorManager) manager ).shutdownAcceptors();
+      final DefaultAcceptorManager manager = new DefaultAcceptorManager();
+      try
+      {
+         manager.connect( null,
+                          new ExceptOnAcceptServerSocket( true ),
+                          new MockSocketConnectionHandler() );
+      }
+      catch ( final NullPointerException npe )
+      {
+         assertEquals( "npe.message", "name", npe.getMessage() );
+         return;
+      }
+      finally
+      {
+         manager.shutdownAcceptors();
+      }
+      fail( "expected NPE due to null name in connect" );
+   }
+
+   public void testConnectWithNullSocket()
+      throws Exception
+   {
+      final DefaultAcceptorManager manager = new DefaultAcceptorManager();
+      try
+      {
+         manager.connect( "name",
+                          null,
+                          new MockSocketConnectionHandler() );
+      }
+      catch ( final NullPointerException npe )
+      {
+         assertEquals( "npe.message", "socket", npe.getMessage() );
+         return;
+      }
+      finally
+      {
+         manager.shutdownAcceptors();
+      }
+      fail( "expected NPE due to null socket in connect" );
+   }
+
+   public void testConnectWithNullHandler()
+      throws Exception
+   {
+      final DefaultAcceptorManager manager = new DefaultAcceptorManager();
+      try
+      {
+         manager.connect( "name",
+                          new ExceptOnAcceptServerSocket( true ),
+                          null );
+      }
+      catch ( final NullPointerException npe )
+      {
+         assertEquals( "npe.message", "handler", npe.getMessage() );
+         return;
+      }
+      finally
+      {
+         manager.shutdownAcceptors();
+      }
+      fail( "expected NPE due to null handler in connect" );
    }
 }
