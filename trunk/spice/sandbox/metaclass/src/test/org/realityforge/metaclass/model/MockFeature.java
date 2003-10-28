cca6dcd575f34c71768e357ba13196e47774a4a6
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-10-28 11:02:16 +0000

    Add in concrete implementation of Feature to use as Mock

---
+++ trunk/spice/sandbox/metaclass/src/test/org/realityforge/metaclass/model/MockFeature.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.realityforge.metaclass.model;
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-10-28 11:02:16 $
+ */
+public class MockFeature
+    extends FeatureDescriptor
+{
+    public MockFeature( final Attribute[] declaredAttributes,
+                        final Attribute[] attributes )
+    {
+        super( declaredAttributes, attributes );
+    }
+}
