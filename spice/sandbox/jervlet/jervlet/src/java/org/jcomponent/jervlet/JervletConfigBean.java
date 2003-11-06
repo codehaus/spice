c56e6412dcf9165659317503607777866a9c08a4
paul-h <paul-h@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-11-06 20:32:44 +0000

    licenses and stub config class

--- spice/sandbox/jervlet/jervlet/src/java/org/jcomponent/jervlet/JervletConfigBean.java
+++ spice/sandbox/jervlet/jervlet/src/java/org/jcomponent/jervlet/JervletConfigBean.java
@@ -1,50 +1,29 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+
 package org.jcomponent.jervlet;
 
-
-public class JervletConfigBean implements JervletConfig {
-
-    /** Virtual host to bind the Jetty to.  null implies all hosts are in context. */
-    private String m_hostName;
-
-    private int m_port;
-    private int m_minThreads;
-    private int m_maxThreads;
-    private boolean m_extractWebArchive;
-
-    public boolean getExtractWarFile() {
-        return m_extractWebArchive;
-    }
+public class JervletConfigBean extends JervletConfigStub {
 
     public void setExtractWarFile(boolean extractWebArchive) {
         this.m_extractWebArchive = extractWebArchive;
-    }
-
-    public int getMaxThreads() {
-        return m_maxThreads;
     }
 
     public void setMaxThreads(int maxThreads) {
         this.m_maxThreads = maxThreads;
     }
 
-    public int getMinThreads() {
-        return m_minThreads;
-    }
-
     public void setMinThreads(int minThreads) {
         this.m_minThreads = minThreads;
     }
 
-    public int getPort() {
-        return m_port;
-    }
-
     public void setPort(int port) {
         this.m_port = port;
-    }
-
-    public String getHostName() {
-        return m_hostName;
     }
 
     public void setHostName(String hostName) {
--- spice/sandbox/jervlet/jervlet/src/java/org/jcomponent/jervlet/JervletMonitor.java
+++ spice/sandbox/jervlet/jervlet/src/java/org/jcomponent/jervlet/JervletMonitor.java
@@ -1,6 +1,15 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+
 package org.jcomponent.jervlet;
 
 public interface JervletMonitor {
+
     void startException(Class reportingClass, String context, Exception e) throws JervletException;
 
     void stopException(Class reportingClass, String context, Exception e) throws JervletException;
