95d52a9d4a00be408e2d58f61765692c9b90be36
donaldp <donaldp@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-11-10 05:54:18 +0000

    Checkin junk code and fix later

--- trunk/spice/sandbox/mesnet/src/java/org/realityforge/mesnet/MesnetSelectorEventHandler.java
+++ trunk/spice/sandbox/mesnet/src/java/org/realityforge/mesnet/MesnetSelectorEventHandler.java
@@ -2,47 +2,210 @@ package org.realityforge.mesnet;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.BufferOverflowException;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.LinkedList;
 import org.realityforge.sca.selector.SelectorEventHandler;
+import org.realityforge.sca.selector.SelectorManager;
 
 /**
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-11-05 04:08:36 $
+ * @version $Revision: 1.2 $ $Date: 2003-11-10 05:54:18 $
  */
 public class MesnetSelectorEventHandler
     implements SelectorEventHandler
 {
+    private static final byte[] MAGIC = new byte[]
+    {
+        'm', 'a', 'g', 'i', 'c', '0', '1'
+    };
+    private static final int MAX_CONTROL_SIZE = MAGIC.length + 8 + 8;
+
+    private final ByteBuffer _message =
+        ByteBuffer.allocateDirect( MAX_CONTROL_SIZE );
+
+    private final SelectorManager _selectorManager;
+    private final SessionManager _sessionManager;
+
+    public MesnetSelectorEventHandler( final SelectorManager selectorManager,
+                                    final SessionManager sessionManager )
+    {
+        _selectorManager = selectorManager;
+        _sessionManager = sessionManager;
+    }
+
     public void handleSelectorEvent( final SelectionKey key,
                                      final Object userData )
     {
-        final Session session = (Session)userData;
-        handleSessionEvent( key, session );
+        if( key.isAcceptable() )
+        {
+            performAccept( userData, key );
+        }
+        else
+        {
+            final TcpTransport transport = (TcpTransport)userData;
+            final Session session = transport.getSession();
+            if( null == session )
+            {
+                handleSessionlessEvent( transport );
+            }
+            else
+            {
+                handleSessionEvent( transport );
+            }
+        }
     }
 
-    public void handleSessionEvent( final SelectionKey key,
-                                    final Session session )
+    void handleSessionlessEvent( final TcpTransport transport )
     {
+        if( transport.getKey().isReadable() )
+        {
+            final SocketChannel channel = transport.getChannel();
+            try
+            {
+                final int count = channel.socket().getInputStream().available();
+                if( count >= MAX_CONTROL_SIZE )
+                {
+                    _message.clear();
+                    channel.read( _message );
+                    for( int i = 0; i < MAGIC.length; i++ )
+                    {
+                        final byte data = _message.get();
+                        if( data != MAGIC[ i ] )
+                        {
+                            //TODO: Notify about bad magic number
+                            sendControlMessage( channel,
+                                                MessageUtils.MESSAGE_BAD_MAGIC );
+                            transport.close();
+                            return;
+                        }
+                    }
+                    final long id = _message.getLong();
+                    final long auth = _message.getLong();
+                    if( id == -1 )
+                    {
+                        final Session session = _sessionManager.newSession();
+                        session.setStatus( Session.STATUS_CONNECTED );
+
+                        _message.putShort( (short)(9 * -1) ); //Size of message & fact it is control
+                        _message.put( MessageUtils.MESSAGE_CONNECTED ); //message
+                        _message.putLong( id );
+                        _message.putLong( auth );
+                        writeMessage( _message, channel );
+                    }
+                    else
+                    {
+                        final Session session = _sessionManager.findSession( id );
+                        if( null == session )
+                        {
+                            sendControlMessage( channel,
+                                                MessageUtils.MESSAGE_BAD_SESSION );
+                            transport.close();
+                            return;
+                        }
+
+                        final Long sessionAuth = (Long)
+                            session.getProperty( MessageUtils.AUTH_KEY );
+                        if( null != sessionAuth &&
+                            auth != sessionAuth.longValue() )
+                        {
+                            sendControlMessage( channel,
+                                                MessageUtils.MESSAGE_BAD_AUTH );
+                            transport.close();
+                            return;
+                        }
+                        else
+                        {
+                            session.setStatus( Session.STATUS_ESTABLISHED );
+                            transport.setSession( session );
+
+                            sendControlMessage( channel,
+                                                MessageUtils.MESSAGE_ESTABLISHED );
+                        }
+                    }
+                }
+            }
+            catch( final BufferOverflowException boe )
+            {
+                //TODO: Notify about buffer overflow
+                transport.close();
+            }
+            catch( final IOException ioe )
+            {
+                //TODO: Notify about esablishing connection
+                transport.close();
+            }
+        }
+    }
+
+    private void sendControlMessage( final SocketChannel channel,
+                                     final byte message )
+        throws IOException
+    {
+        _message.clear();
+        _message.putShort( (short)(1 * -1) );
+        _message.put( message );
+        writeMessage( _message, channel );
+    }
+
+    private void writeMessage( final ByteBuffer message,
+                               final SocketChannel channel )
+        throws IOException
+    {
+        final int available = message.remaining();
+        final int writeCount = channel.write( message );
+        if( available != writeCount )
+        {
+            throw new BufferOverflowException();
+        }
+    }
+
+    void performAccept( final Object userData,
+                        final SelectionKey key )
+    {
+        final ServerSocketChannel channel = (ServerSocketChannel)key.channel();
+        try
+        {
+            final SocketChannel socketChannel = channel.accept();
+            _selectorManager.registerChannel( socketChannel,
+                                              SelectionKey.OP_READ |
+                                              SelectionKey.OP_WRITE,
+                                              this,
+                                              userData );
+        }
+        catch( final IOException e )
+        {
+            //TODO: Note that problem with accept
+        }
+    }
+
+    public void handleSessionEvent( final TcpTransport transport )
+    {
+        final SelectionKey key = transport.getKey();
         if( key.isReadable() )
         {
-            performRead( session );
+            performRead( transport );
         }
 
         if( key.isValid() && key.isWritable() )
         {
-            performWrite( session );
+            performWrite( transport );
         }
     }
 
-    void performRead( final Session session )
+    void performRead( final TcpTransport transport )
     {
         try
         {
-            final ByteBuffer buffer = session.getReadBuffer();
-            final int count = session.getChannel().read( buffer );
+            final DataChannel dataChannel =
+                transport.getSession().getDataChannel();
+            final ByteBuffer buffer = dataChannel.getReadBuffer();
+            final int count = transport.getChannel().read( buffer );
             if( -1 == count )
             {
-                endConnection( session );
+                transport.close();
                 return;
             }
             //TODO: Notify Monitor that data received
@@ -67,14 +230,14 @@ public class MesnetSelectorEventHandler
         }
         catch( final IOException e )
         {
-            endConnection( session );
-            return;
+            transport.close();
         }
     }
 
-    void performWrite( final Session session )
+    void performWrite( final TcpTransport transport )
     {
-        final LinkedList queue = session.getWriteQueue();
+        final ByteBuffer writeBuffer =
+            transport.getSession().getDataChannel().getWriteBuffer();
         synchronized( queue )
         {
             if( queue.size() > 0 )
@@ -99,28 +262,9 @@ public class MesnetSelectorEventHandler
                 catch( final IOException e )
                 {
                     queue.addFirst( data );
-                    endConnection( session );
+                    transport.close();
                 }
             }
         }
     }
-
-    void endConnection( final Session session )
-    {
-        //TODO: Notify ending connection
-        session.getKey().cancel();
-        try
-        {
-            session.getChannel().close();
-        }
-        catch( IOException e )
-        {
-            // TODO: Note error closing channel
-        }
-        session.setKey( null );
-        session.setChannel( null );
-        //TODO: Attempt to resestablish connection with client
-        //or wait till they connect?
-    }
-
 }
