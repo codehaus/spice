cd34d8387d48aa6e8f0254f8017a2b55c97e459b
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-11-20 10:00:39 +0000

    Add tasks to add one pluginSet to another

--- trunk/spice/sandbox/metaclass/src/test/org/realityforge/metaclass/tools/tasks/MockProject.java
+++ trunk/spice/sandbox/metaclass/src/test/org/realityforge/metaclass/tools/tasks/MockProject.java
@@ -7,25 +7,27 @@
  */
 package org.realityforge.metaclass.tools.tasks;
 
+import java.util.HashMap;
 import org.apache.tools.ant.Project;
 
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-11-20 06:42:49 $
+ * @version $Revision: 1.2 $ $Date: 2003-11-20 10:00:39 $
  */
 class MockProject
     extends Project
 {
-    private final Object m_reference;
+    private final HashMap m_references = new HashMap();
 
-    MockProject( final Object reference )
+    void bindReference( final String key,
+                        final Object value )
     {
-        m_reference = reference;
+        m_references.put( key, value );
     }
 
     public Object getReference( final String key )
     {
-        return m_reference;
+        return m_references.get( key );
     }
 }
