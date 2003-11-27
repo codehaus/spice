6c36796ace2e0ac0c505b7dadc9495abee3491e8
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-11-27 23:51:58 +0000

    Styled

--- trunk/spice/sandbox/sca/src/java/org/realityforge/sca/connector/LimitingReconnectPolicy.java
+++ trunk/spice/sandbox/sca/src/java/org/realityforge/sca/connector/LimitingReconnectPolicy.java
@@ -1,72 +1,66 @@
 package org.realityforge.sca.connector;
 
 /**
- * A Policy that will always attempt to limit
- * the number of connection attempts in a
- * period of time.
- *
- * <p>After N connection attempts it will force a
- * delay of T between successive connection
- * attempts. This is an attempt not to overload
- * the resource being connected to.</p>
+ * A Policy that will always attempt to limit the number of connection attempts
+ * in a period of time.
+ * 
+ * <p>After N connection attempts it will force a delay of T between successive
+ * connection attempts. This is an attempt not to overload the resource being
+ * connected to.</p>
  */
 public class LimitingReconnectPolicy
-   implements ReconnectionPolicy
+    implements ReconnectionPolicy
 {
-   /**
-    * The number of attempts allowed before delay will kick in.
-    */
-   private final int _attempts;
+    /** The number of attempts allowed before delay will kick in. */
+    private final int m_attempts;
 
-   /**
-    * The delay between successive connection attempts.
-    */
-   private final long _delay;
+    /** The delay between successive connection attempts. */
+    private final long m_delay;
 
-   /**
-    * Create a policy instance.
-    *
-    * @param attempts the number of attempts before the delay is enabled.
-    * @param delay the delay
-    */
-   public LimitingReconnectPolicy( final int attempts,
-                                   final long delay )
-   {
-      _attempts = attempts;
-      _delay = delay;
-   }
+    /**
+     * Create a policy instance.
+     * 
+     * @param attempts the number of attempts before the delay is enabled.
+     * @param delay the delay
+     */
+    public LimitingReconnectPolicy( final int attempts,
+                                    final long delay )
+    {
+        m_attempts = attempts;
+        m_delay = delay;
+    }
 
-   /**
-    * @see org.realityforge.sca.connector.ReconnectionPolicy#attemptConnection
-    */
-   public boolean attemptConnection( final long lastConnectionAttempt,
-                                     final int connectionAttempts )
-   {
-      if( connectionAttempts >= _attempts )
-      {
-         final long now = System.currentTimeMillis();
-         final long nextAttempt = lastConnectionAttempt + _delay;
-         if( now < nextAttempt )
-         {
-            return false;
-         }
-      }
-      return true;
-   }
+    /**
+     * @see ReconnectionPolicy#attemptConnection(long, int)
+     */
+    public boolean attemptConnection( final long lastConnectionAttempt,
+                                      final int connectionAttempts )
+    {
+        if( connectionAttempts >= m_attempts )
+        {
+            final long now = System.currentTimeMillis();
+            final long nextAttempt = lastConnectionAttempt + m_delay;
+            if( now < nextAttempt )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
-   /**
-    * @see org.realityforge.sca.connector.ReconnectionPolicy#disconnectOnError
-    */
-   public boolean disconnectOnError( final Throwable t )
-   {
-      return true;
-   }
+    /**
+     * @see ReconnectionPolicy#disconnectOnError(Throwable)
+     */
+    public boolean disconnectOnError( final Throwable t )
+    {
+        return true;
+    }
 
-   /**
-    * @see org.realityforge.sca.connector.ReconnectionPolicy#reconnectOnDisconnect
-    */
-   public boolean reconnectOnDisconnect()
-   {
-      return true;
-   }
+    /**
+     * @see ReconnectionPolicy#reconnectOnDisconnect()
+     */
+    public boolean reconnectOnDisconnect()
+    {
+        return true;
+    }
 }
