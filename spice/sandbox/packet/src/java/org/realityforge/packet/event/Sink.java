f574686f9a9174f459c43031336aaab9e8375806
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-11-28 04:31:38 +0000

    Fix license

--- spice/sandbox/packet/src/java/org/realityforge/packet/event/Sink.java
+++ spice/sandbox/packet/src/java/org/realityforge/packet/event/Sink.java
@@ -1,17 +1,17 @@
 /*
-* Copyright (C) The Spice Group. All rights reserved.
-*
-* This software is published under the terms of the Spice
-* Software License version 1.1, a copy of which has been included
-* with this distribution in the LICENSE.txt file.
-*/
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
 package org.realityforge.packet.event;
 
 /**
  * A Sink represents the destination for events.
  * 
  * @author Peter Donald
- * @version $Revision: 1.2 $ $Date: 2003-11-28 04:25:16 $
+ * @version $Revision: 1.3 $ $Date: 2003-11-28 04:31:38 $
  */
 public interface Sink
 {
--- spice/sandbox/packet/src/java/org/realityforge/packet/event/Source.java
+++ spice/sandbox/packet/src/java/org/realityforge/packet/event/Source.java
@@ -1,17 +1,17 @@
 /*
-* Copyright (C) The Spice Group. All rights reserved.
-*
-* This software is published under the terms of the Spice
-* Software License version 1.1, a copy of which has been included
-* with this distribution in the LICENSE.txt file.
-*/
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
 package org.realityforge.packet.event;
 
 /**
  * The Source represents a source of events.
  * 
  * @author Peter Donald
- * @version $Revision: 1.1 $ $Date: 2003-11-28 03:43:36 $
+ * @version $Revision: 1.2 $ $Date: 2003-11-28 04:31:38 $
  */
 public interface Source
 {
@@ -26,7 +26,7 @@ public interface Source
     /**
      * Return a set of events from source. The maximum number of events returned
      * is specified by the count parameter.
-     *
+     * 
      * @param count the maximum number of events returned
      * @return the set of events
      */
