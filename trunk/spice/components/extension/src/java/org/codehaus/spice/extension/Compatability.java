cb1290b2d130e851b097411a2d94a21857ca10cc
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-12-02 07:56:59 +0000

    Migrate to the codehaus package naming scheme

--- trunk/spice/components/extension/src/java/org/codehaus/spice/extension/Compatability.java
+++ trunk/spice/components/extension/src/java/org/codehaus/spice/extension/Compatability.java
@@ -5,14 +5,14 @@
  * Software License version 1.1, a copy of which has been included
  * with this distribution in the LICENSE.txt file.
  */
-package org.realityforge.extension;
+package org.codehaus.spice.extension;
 /**
  * Enum used in {@link Extension} to indicate the compatability
  * of one extension to another. See {@link Extension} for instances
  * of object.
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.2 $ $Date: 2003-08-04 01:52:52 $
+ * @version $Revision: 1.1 $ $Date: 2003-12-02 07:56:59 $
  * @see Extension
  */
 public final class Compatability
