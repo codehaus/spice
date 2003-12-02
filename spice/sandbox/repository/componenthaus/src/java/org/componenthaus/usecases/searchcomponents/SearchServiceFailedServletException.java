d381fdc01aa93b632f78d8cff3b94ff08173ae1e
nobody <nobody@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-12-02 08:37:58 +0000

    This commit was manufactured by cvs2svn to create tag 'CONVERTER_1_0-tag'.

--- spice/sandbox/repository/componenthaus/src/java/org/componenthaus/usecases/searchcomponents/SearchServiceFailedServletException.java
+++
@@ -1,9 +0,0 @@
-package org.componenthaus.usecases.searchcomponents;
-
-import javax.servlet.ServletException;
-
-public class SearchServiceFailedServletException extends ServletException {
-    public SearchServiceFailedServletException(String message, Throwable cause) {
-        super(message,cause);
-    }
-}
