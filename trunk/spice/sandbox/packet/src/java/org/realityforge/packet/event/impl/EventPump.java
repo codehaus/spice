3e612c8a47f8e681f2ad641cf9d41b0024db3a3e
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-12-09 04:47:25 +0000

    Add in an Event Pumper that pums from a source to a handler

---
+++ trunk/spice/sandbox/packet/src/java/org/realityforge/packet/event/impl/EventPump.java
@@ -0,0 +1,88 @@
+package org.realityforge.packet.event.impl;
+
+import org.realityforge.packet.event.EventHandler;
+import org.realityforge.packet.event.EventSource;
+
+/**
+ * Pump Events from an EventSource to an EventHandler. The Pump attempts to
+ * transmit batchSize events every time it performs a refresh.
+ * 
+ * @author Peter Donald
+ * @version $Revision: 1.1 $ $Date: 2003-12-09 04:47:25 $
+ */
+public class EventPump
+{
+    /** The number events to try to pump from Source to Handler each refresh. */
+    private int m_batchSize;
+
+    /** The source of events. */
+    final EventSource m_source;
+
+    /** The handler for events. */
+    final EventHandler m_handler;
+
+    /**
+     * Create a pump from specified source to specified handler.
+     * 
+     * @param source the source
+     * @param handler the handler
+     */
+    public EventPump( final EventSource source,
+                      final EventHandler handler )
+    {
+        if( null == source )
+        {
+            throw new NullPointerException( "source" );
+        }
+        if( null == handler )
+        {
+            throw new NullPointerException( "handler" );
+        }
+        m_source = source;
+        m_handler = handler;
+    }
+
+    /**
+     * Perform a single refresh and pump events from source to the handler.
+     */
+    public void refresh()
+    {
+        final int batchSize = getBatchSize();
+        if( batchSize < 2 )
+        {
+            final Object event = m_source.getEvent();
+            if( null != event )
+            {
+                m_handler.handleEvent( event );
+            }
+        }
+        else
+        {
+            final Object[] events = m_source.getEvents( batchSize );
+            if( 0 != events.length )
+            {
+                m_handler.handleEvents( events );
+            }
+        }
+    }
+
+    /**
+     * Return the number events pumped every refresh.
+     * 
+     * @return the number events pumped every refresh.
+     */
+    public int getBatchSize()
+    {
+        return m_batchSize;
+    }
+
+    /**
+     * Set the number events pumped every refresh.
+     * 
+     * @param batchSize the number events pumped every refresh.
+     */
+    public void setBatchSize( final int batchSize )
+    {
+        m_batchSize = batchSize;
+    }
+}
---
+++ trunk/spice/sandbox/packet/src/test/org/realityforge/packet/event/impl/EventPumpTestCase.java
@@ -0,0 +1,166 @@
+package org.realityforge.packet.event.impl;
+
+import junit.framework.TestCase;
+import org.jmock.C;
+import org.jmock.Mock;
+import org.realityforge.packet.event.EventHandler;
+import org.realityforge.packet.event.EventSource;
+
+/**
+ * @author Peter Donald
+ * @version $Revision: 1.1 $ $Date: 2003-12-09 04:47:25 $
+ */
+public class EventPumpTestCase
+    extends TestCase
+{
+    public void testNull_source_PassedIntoCtor()
+        throws Exception
+    {
+        final Mock mockHandler = new Mock( EventHandler.class );
+        final EventHandler handler = (EventHandler)mockHandler.proxy();
+        try
+        {
+            new EventPump( null, handler );
+        }
+        catch( final NullPointerException npe )
+        {
+            assertEquals( "npe.getMessage()", "source", npe.getMessage() );
+            return;
+        }
+        fail( "Expected a NPE when passing source into Ctor" );
+    }
+
+    public void testNull_handler_PassedIntoCtor()
+        throws Exception
+    {
+        final Mock mockSource = new Mock( EventSource.class );
+        final EventSource source = (EventSource)mockSource.proxy();
+        try
+        {
+            new EventPump( source, null );
+        }
+        catch( final NullPointerException npe )
+        {
+            assertEquals( "npe.getMessage()", "handler", npe.getMessage() );
+            return;
+        }
+        fail( "Expected a NPE when passing handler into Ctor" );
+    }
+
+    public void testCreation()
+        throws Exception
+    {
+        final Mock mockSource = new Mock( EventSource.class );
+        final EventSource source = (EventSource)mockSource.proxy();
+
+        final Mock mockHandler = new Mock( EventHandler.class );
+        final EventHandler handler = (EventHandler)mockHandler.proxy();
+
+        final EventPump pump = new EventPump( source, handler );
+        assertEquals( "pump.m_source", source, pump.m_source );
+        assertEquals( "pump.m_handler", handler, pump.m_handler );
+
+        mockHandler.verify();
+        mockSource.verify();
+    }
+
+    public void testSetBatchCount()
+        throws Exception
+    {
+        final Mock mockSource = new Mock( EventSource.class );
+        final EventSource source = (EventSource)mockSource.proxy();
+
+        final Mock mockHandler = new Mock( EventHandler.class );
+        final EventHandler handler = (EventHandler)mockHandler.proxy();
+
+        final EventPump pump = new EventPump( source, handler );
+        assertEquals( "batchSize", 0, pump.getBatchSize() );
+        pump.setBatchSize( 15 );
+        assertEquals( "batchSize", 15, pump.getBatchSize() );
+
+        mockHandler.verify();
+        mockSource.verify();
+    }
+
+    public void testRefreshNonBatchedWithHandling()
+        throws Exception
+    {
+        final Object event = new Object();
+        final Mock mockSource = new Mock( EventSource.class );
+        mockSource.expectAndReturn( "getEvent", event );
+        final EventSource source = (EventSource)mockSource.proxy();
+
+        final Mock mockHandler = new Mock( EventHandler.class );
+        mockHandler.expect( "handleEvent", C.args( C.eq( event ) ) );
+        final EventHandler handler = (EventHandler)mockHandler.proxy();
+
+        final EventPump pump = new EventPump( source, handler );
+        pump.setBatchSize( 1 );
+
+        pump.refresh();
+
+        mockHandler.verify();
+        mockSource.verify();
+    }
+
+    public void testRefreshNonBatchedWithNoHandling()
+        throws Exception
+    {
+        final Mock mockSource = new Mock( EventSource.class );
+        mockSource.expectAndReturn( "getEvent", null );
+        final EventSource source = (EventSource)mockSource.proxy();
+
+        final Mock mockHandler = new Mock( EventHandler.class );
+        final EventHandler handler = (EventHandler)mockHandler.proxy();
+
+        final EventPump pump = new EventPump( source, handler );
+        pump.setBatchSize( 1 );
+
+        pump.refresh();
+
+        mockHandler.verify();
+        mockSource.verify();
+    }
+
+    public void testRefreshBatchedWithHandling()
+        throws Exception
+    {
+        final Object event = new Object();
+        final Object[] events = new Object[]{event};
+        final Mock mockSource = new Mock( EventSource.class );
+        mockSource.expectAndReturn( "getEvents", C.args( C.eq( 20 ) ), events );
+        final EventSource source = (EventSource)mockSource.proxy();
+
+        final Mock mockHandler = new Mock( EventHandler.class );
+        mockHandler.expect( "handleEvents", C.args( C.eq( events ) ) );
+        final EventHandler handler = (EventHandler)mockHandler.proxy();
+
+        final EventPump pump = new EventPump( source, handler );
+        pump.setBatchSize( 20 );
+
+        pump.refresh();
+
+        mockHandler.verify();
+        mockSource.verify();
+    }
+
+    public void testRefreshBatchedWithNoHandling()
+        throws Exception
+    {
+        final Object[] events = new Object[ 0 ];
+        final Mock mockSource = new Mock( EventSource.class );
+        mockSource.expectAndReturn( "getEvents", C.args( C.eq( 20 ) ), events );
+        final EventSource source = (EventSource)mockSource.proxy();
+
+        final Mock mockHandler = new Mock( EventHandler.class );
+        final EventHandler handler = (EventHandler)mockHandler.proxy();
+
+        final EventPump pump = new EventPump( source, handler );
+        pump.setBatchSize( 20 );
+
+        pump.refresh();
+
+        mockHandler.verify();
+        mockSource.verify();
+    }
+}
