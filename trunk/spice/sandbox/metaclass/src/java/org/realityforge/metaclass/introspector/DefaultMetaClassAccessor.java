db7d13ad6cb47c60dc38de5948cf935bc180d979
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2003-12-10 22:43:32 +0000

    Use constants for IO rather than instances

--- trunk/spice/sandbox/metaclass/src/java/org/realityforge/metaclass/introspector/DefaultMetaClassAccessor.java
+++ trunk/spice/sandbox/metaclass/src/java/org/realityforge/metaclass/introspector/DefaultMetaClassAccessor.java
@@ -8,58 +8,36 @@
 package org.realityforge.metaclass.introspector;
 
 import java.io.InputStream;
-import org.realityforge.metaclass.io.MetaClassIO;
 import org.realityforge.metaclass.io.MetaClassIOBinary;
 import org.realityforge.metaclass.io.MetaClassIOXml;
 import org.realityforge.metaclass.model.ClassDescriptor;
 
 /**
- * This is the default mechanism for loading ClassDescriptor
- * objects. This class follows the following steps to locate
- * the ClassDescriptor.
+ * This is the default mechanism for loading ClassDescriptor objects. This class
+ * follows the following steps to locate the ClassDescriptor.
  *
- * <ul>
- *   <li>Look for a file with extension "-meta.binary" sitting
- *       side-by-side the class file. ie If class is named
- *       com.biz.Foo it has a class file named
- *       <tt>com/biz/Foo.class</tt> and an descriptor file
- *       named <tt>com/biz/Foo-meta.binary</tt>. Assume the
- *       descriptor is in binary format.</li>
- *   <li>Look for a file with extension "-meta.xml" sitting
- *       side-by-side the class file. ie If class is named
- *       com.biz.Foo it has a class file named
- *       <tt>com/biz/Foo.class</tt> and an descriptor file
- *       named <tt>com/biz/Foo-meta.xml</tt>. Assume the
- *       descriptor is in xml format.</li>
- *   <li>If unable to locate descriptor throw a
- *       <tt>MetaClassException</tt>.</li>
+ * <ul> <li>Look for a file with extension "-meta.binary" sitting side-by-side
+ * the class file. ie If class is named com.biz.Foo it has a class file named
+ * <tt>com/biz/Foo.class</tt> and an descriptor file named
+ * <tt>com/biz/Foo-meta.binary</tt>. Assume the descriptor is in binary
+ * format.</li> <li>Look for a file with extension "-meta.xml" sitting
+ * side-by-side the class file. ie If class is named com.biz.Foo it has a class
+ * file named <tt>com/biz/Foo.class</tt> and an descriptor file named
+ * <tt>com/biz/Foo-meta.xml</tt>. Assume the descriptor is in xml format.</li>
+ * <li>If unable to locate descriptor throw a <tt>MetaClassException</tt>.</li>
  * </ul>
  *
  * @author Peter Donald
- * @version $Revision: 1.8 $ $Date: 2003-11-27 08:09:53 $
+ * @version $Revision: 1.9 $ $Date: 2003-12-10 22:43:32 $
  */
 public class DefaultMetaClassAccessor
     implements MetaClassAccessor
 {
-    /**
-     * Extension of metadata files that are in binary format.
-     */
+    /** Extension of metadata files that are in binary format. */
     public static final String BINARY_EXT = "-meta.binary";
 
-    /**
-     * Extension of metadata files that are in xml format.
-     */
+    /** Extension of metadata files that are in xml format. */
     public static final String XML_EXT = "-meta.xml";
-
-    /**
-     * Class used to read the MetaData in binary format.
-     */
-    private static final MetaClassIO c_binaryIO = new MetaClassIOBinary();
-
-    /**
-     * Class used to read the MetaData in XML format.
-     */
-    private static final MetaClassIO c_xmlIO = new MetaClassIOXml();
 
     /**
      * @see MetaClassAccessor#getClassDescriptor
@@ -91,11 +69,11 @@ public class DefaultMetaClassAccessor
         {
             if( isXML )
             {
-                return c_xmlIO.deserializeClass( inputStream );
+                return MetaClassIOXml.IO.deserializeClass( inputStream );
             }
             else
             {
-                return c_binaryIO.deserializeClass( inputStream );
+                return MetaClassIOBinary.IO.deserializeClass( inputStream );
             }
         }
         catch( final Exception e )
