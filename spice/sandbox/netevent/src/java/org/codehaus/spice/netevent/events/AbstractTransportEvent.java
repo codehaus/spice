8144aba87c4f388de928929d98c9d61bdcc2aae8
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-01-07 04:01:50 +0000

    Add in base level events

---
+++ spice/sandbox/netevent/src/java/org/codehaus/spice/netevent/events/AbstractTransportEvent.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.codehaus.spice.netevent.events;
+
+import java.net.SocketAddress;
+import org.codehaus.spice.netevent.transport.TcpTransport;
+
+/**
+ * An Event related to a particular transport.
+ * 
+ * @author Peter Donald
+ * @version $Revision: 1.1 $ $Date: 2004-01-07 04:01:50 $
+ */
+public abstract class AbstractTransportEvent
+{
+    /** The transport. */
+    private final TcpTransport _transport;
+
+    /**
+     * Create event for specified transport.
+     * 
+     * @param transport the transport
+     */
+    public AbstractTransportEvent( final TcpTransport transport )
+    {
+        if( null == transport )
+        {
+            throw new NullPointerException( "transport" );
+        }
+        _transport = transport;
+    }
+
+    /**
+     * Return the transport.
+     * 
+     * @return the transport.
+     */
+    public TcpTransport getTransport()
+    {
+        return _transport;
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    public String toString()
+    {
+        final SocketAddress address =
+            getTransport().getChannel().socket().getRemoteSocketAddress();
+        return getClass().getName() + "[" + address + "]";
+    }
+}
