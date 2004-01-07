a77c55e8c7a104fe2aa48a81eb2a1ad3c30bfa01
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-01-07 04:28:07 +0000

    Add in IO event hierarchy

---
+++ spice/sandbox/netevent/src/java/org/codehaus/spice/netevent/events/IOEvent.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.codehaus.spice.netevent.events;
+
+import java.net.SocketAddress;
+import org.codehaus.spice.netevent.transport.TcpTransport;
+
+/**
+ * Event indicating IO occured.
+ * 
+ * @author Peter Donald
+ * @version $Revision: 1.1 $ $Date: 2004-01-07 04:28:07 $
+ */
+public abstract class IOEvent
+    extends AbstractTransportEvent
+{
+    /** The number of bytes transferred. */
+    private final int _count;
+
+    /**
+     * Create event.
+     * 
+     * @param transport the transport
+     * @param count the byte count
+     */
+    public IOEvent( final TcpTransport transport, final int count )
+    {
+        super( transport );
+        _count = count;
+    }
+
+    /**
+     * Return the number of bytes transferred.
+     * 
+     * @return the number of bytes transferred.
+     */
+    public int getCount()
+    {
+        return _count;
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    public String toString()
+    {
+        final SocketAddress address =
+            getTransport().getChannel().socket().getRemoteSocketAddress();
+        return getClass().getName() + "[" + getCount() +
+               " bytes to " + address + "]";
+    }
+}
