f9295fe9f0f773f53805ab6944fcbd90faf85003
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-01-16 00:17:04 +0000

    Rework ServerSocket related events to be about generic channels and also add in event relating to ConnectPossible so that outgoing connection events can be generated from this setup

---
+++ trunk/spice/sandbox/netevent/src/java/org/codehaus/spice/netevent/events/ChannelEvent.java
@@ -0,0 +1,63 @@
+package org.codehaus.spice.netevent.events;
+
+import java.nio.channels.Channel;
+
+/**
+ * Abstract event about Channels.
+ * 
+ * @author Peter Donald
+ * @version $Revision: 1.3 $ $Date: 2004-01-16 00:17:04 $
+ */
+public abstract class ChannelEvent
+    extends AbstractEvent
+{
+    /** The Channel. */
+    private final Channel m_channel;
+
+    /** The associated userdata. */
+    private final Object m_userData;
+
+    /**
+     * Create Event.
+     * 
+     * @param channel the channel
+     */
+    public ChannelEvent( final Channel channel,
+                         final Object userData )
+    {
+        if( null == channel )
+        {
+            throw new NullPointerException( "channel" );
+        }
+        m_channel = channel;
+        m_userData = userData;
+    }
+
+    /**
+     * Return the socket channel.
+     * 
+     * @return the socket channel.
+     */
+    public Channel getChannel()
+    {
+        return m_channel;
+    }
+
+    /**
+     * Return the associated user data.
+     * 
+     * @return the associated user data.
+     */
+    public Object getUserData()
+    {
+        return m_userData;
+    }
+
+    /**
+     * @see AbstractEvent#getEventDescription()
+     */
+    protected String getEventDescription()
+    {
+        return String.valueOf( m_channel );
+    }
+}
