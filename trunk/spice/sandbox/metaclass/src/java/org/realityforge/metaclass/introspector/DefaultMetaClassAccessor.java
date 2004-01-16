adb111162f516565e1cce42f1dd2e220228ce792
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-01-16 00:41:56 +0000

    Integrate ASM IO into default accessor strategy

--- trunk/spice/sandbox/metaclass/src/java/org/realityforge/metaclass/introspector/DefaultMetaClassAccessor.java
+++ trunk/spice/sandbox/metaclass/src/java/org/realityforge/metaclass/introspector/DefaultMetaClassAccessor.java
@@ -8,6 +8,8 @@
 package org.realityforge.metaclass.introspector;
 
 import java.io.InputStream;
+import org.realityforge.metaclass.io.MetaClassIO;
+import org.realityforge.metaclass.io.MetaClassIOASM;
 import org.realityforge.metaclass.io.MetaClassIOBinary;
 import org.realityforge.metaclass.io.MetaClassIOXml;
 import org.realityforge.metaclass.model.ClassDescriptor;
@@ -15,7 +17,7 @@ import org.realityforge.metaclass.model.ClassDescriptor;
 /**
  * This is the default mechanism for loading ClassDescriptor objects. This class
  * follows the following steps to locate the ClassDescriptor.
- *
+ * 
  * <ul> <li>Look for a file with extension "-meta.binary" sitting side-by-side
  * the class file. ie If class is named com.biz.Foo it has a class file named
  * <tt>com/biz/Foo.class</tt> and an descriptor file named
@@ -26,49 +28,82 @@ import org.realityforge.metaclass.model.ClassDescriptor;
  * <tt>com/biz/Foo-meta.xml</tt>. Assume the descriptor is in xml format.</li>
  * <li>If unable to locate descriptor throw a <tt>MetaClassException</tt>.</li>
  * </ul>
- *
+ * 
  * @author Peter Donald
- * @version $Revision: 1.10 $ $Date: 2003-12-11 08:41:50 $
+ * @version $Revision: 1.11 $ $Date: 2004-01-16 00:41:56 $
  */
 public class DefaultMetaClassAccessor
     implements MetaClassAccessor
 {
+    /** Class based IO. May be null if the ASM toolkit is not in classloader. */
+    private static final MetaClassIO ASM_IO = getMetaClassIOASM();
+
     /**
      * @see MetaClassAccessor#getClassDescriptor
      */
-    public ClassDescriptor getClassDescriptor( final String classname,
-                                               final ClassLoader classLoader,
-                                               final MetaClassAccessor accessor )
+    public ClassDescriptor
+        getClassDescriptor( final String classname,
+                            final ClassLoader classLoader,
+                            final MetaClassAccessor accessor )
         throws MetaClassException
     {
-        boolean isXML = false;
-        final String baseName = classname.replace( '.', '/' );
-        String resource = baseName + MetaClassIOBinary.EXTENSION;
-        InputStream inputStream = classLoader.getResourceAsStream( resource );
-        if( null == inputStream )
+        ClassDescriptor descriptor;
+
+        descriptor = loadClassDescriptor( MetaClassIOXml.IO,
+                                          classname,
+                                          classLoader );
+        if( null != descriptor )
         {
-            resource = baseName + MetaClassIOXml.EXTENSION;
-            inputStream = classLoader.getResourceAsStream( resource );
-            isXML = true;
+            return descriptor;
+        }
+        descriptor = loadClassDescriptor( MetaClassIOBinary.IO,
+                                          classname,
+                                          classLoader );
+        if( null != descriptor )
+        {
+            return descriptor;
         }
 
-        if( null == inputStream )
+        if( null != ASM_IO )
         {
-            final String message =
-                "Missing Attributes for " + classname;
-            throw new MetaClassException( message );
+            descriptor = loadClassDescriptor( ASM_IO,
+                                              classname,
+                                              classLoader );
+            if( null != descriptor )
+            {
+                return descriptor;
+            }
         }
 
+        final String message =
+            "Missing Attributes for " + classname;
+        throw new MetaClassException( message );
+    }
+
+    /**
+     * Attempt to load ClassDescriptor using specified MetaClassIOXml.
+     * 
+     * @param io the MetaClassIOXml
+     * @param classname the classname of class
+     * @param classLoader the classloader to load resources from
+     * @return the ClassDescriptor if descriptor is found, else null
+     * @throws MetaClassException if error loading descriptor
+     */
+    private ClassDescriptor loadClassDescriptor( final MetaClassIO io,
+                                                 final String classname,
+                                                 final ClassLoader classLoader )
+        throws MetaClassException
+    {
+        final String resourceName = io.getResourceName( classname );
+        final InputStream inputStream =
+            classLoader.getResourceAsStream( resourceName );
+        if( null == inputStream )
+        {
+            return null;
+        }
         try
         {
-            if( isXML )
-            {
-                return MetaClassIOXml.IO.deserializeClass( inputStream );
-            }
-            else
-            {
-                return MetaClassIOBinary.IO.deserializeClass( inputStream );
-            }
+            return io.deserializeClass( inputStream );
         }
         catch( final Exception e )
         {
@@ -77,4 +112,22 @@ public class DefaultMetaClassAccessor
             throw new MetaClassException( message, e );
         }
     }
+
+    /**
+     * Return ASM IO if it can be created (and thus ASM toolkit in
+     * classloader).
+     */
+    private static MetaClassIO getMetaClassIOASM()
+    {
+        try
+        {
+            return MetaClassIOASM.IO;
+        }
+        catch( final Exception e )
+        {
+            //ie ClassNotFoundException
+            return null;
+        }
+    }
+
 }
