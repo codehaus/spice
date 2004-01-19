5c076bacbe23cf08c7df8ac3137961a8a066d7d1
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-01-19 04:41:53 +0000

    Rework so packet messages have super class that outputs sequence number

--- trunk/spice/sandbox/packet/src/java/org/realityforge/packet/events/PacketWriteRequestEvent.java
+++ trunk/spice/sandbox/packet/src/java/org/realityforge/packet/events/PacketWriteRequestEvent.java
@@ -7,14 +7,11 @@ import org.realityforge.packet.session.Session;
  * Event request a packet be written.
  * 
  * @author Peter Donald
- * @version $Revision: 1.1 $ $Date: 2004-01-13 05:56:23 $
+ * @version $Revision: 1.2 $ $Date: 2004-01-19 04:41:53 $
  */
 public class PacketWriteRequestEvent
-    extends SessionEvent
+    extends PacketEvent
 {
-    /** The associated packet. */
-    private final Packet _packet;
-
     /**
      * Create event.
      * 
@@ -24,21 +21,6 @@ public class PacketWriteRequestEvent
     public PacketWriteRequestEvent( final Session session,
                                     final Packet packet )
     {
-        super( session );
-        if( null == packet )
-        {
-            throw new NullPointerException( "packet" );
-        }
-        _packet = packet;
-    }
-
-    /**
-     * Return the packet.
-     * 
-     * @return the packet.
-     */
-    public Packet getPacket()
-    {
-        return _packet;
+        super( session, packet );
     }
 }
