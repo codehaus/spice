57cbd6916bf639f84f50e351a39a753e8b1ec8b9
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-01-20 03:18:19 +0000

    Extend channel event

--- trunk/spice/sandbox/netevent/src/java/org/codehaus/spice/netevent/events/AcceptEvent.java
+++ trunk/spice/sandbox/netevent/src/java/org/codehaus/spice/netevent/events/AcceptEvent.java
@@ -6,14 +6,11 @@ import java.nio.channels.Channel;
  * Event to indicate Channel was accepted.
  * 
  * @author Peter Donald
- * @version $Revision: 1.1 $ $Date: 2004-01-08 04:03:58 $
+ * @version $Revision: 1.2 $ $Date: 2004-01-20 03:18:19 $
  */
 public class AcceptEvent
-    extends AbstractEvent
+    extends ChannelEvent
 {
-    /** The Channel. */
-    private final Channel m_channel;
-
     /**
      * Create Event.
      * 
@@ -21,28 +18,6 @@ public class AcceptEvent
      */
     public AcceptEvent( final Channel channel )
     {
-        if( null == channel )
-        {
-            throw new NullPointerException( "channel" );
-        }
-        m_channel = channel;
-    }
-
-    /**
-     * Return the socket channel.
-     * 
-     * @return the socket channel.
-     */
-    public Channel getChannel()
-    {
-        return m_channel;
-    }
-
-    /**
-     * @see AbstractEvent#getEventDescription()
-     */
-    protected String getEventDescription()
-    {
-        return String.valueOf( m_channel );
+        super( channel, null );
     }
 }
