33ebb2a4af27e2b0d63f0776bdfbba256f77246b
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-01-21 04:26:19 +0000

    Improve toString for debugging purposes.

--- trunk/spice/sandbox/packet/src/java/org/realityforge/packet/events/DataPacketReadyEvent.java
+++ trunk/spice/sandbox/packet/src/java/org/realityforge/packet/events/DataPacketReadyEvent.java
@@ -7,12 +7,14 @@ import org.realityforge.packet.session.Session;
  * Event indicating a packet is ready to be processed by next layer.
  * 
  * @author Peter Donald
- * @version $Revision: 1.1 $ $Date: 2004-01-14 01:42:10 $
+ * @version $Revision: 1.2 $ $Date: 2004-01-21 04:26:19 $
  */
 public class DataPacketReadyEvent
     extends SessionEvent
 {
-    /** The associated packet. */
+    /**
+     * The associated packet.
+     */
     private final Packet _packet;
 
     /**
@@ -40,5 +42,15 @@ public class DataPacketReadyEvent
     public Packet getPacket()
     {
         return _packet;
+    }
+
+    /**
+     * @see SessionEvent#getEventDescription()
+     */
+    protected String getEventDescription()
+    {
+        return super.getEventDescription() +
+               ", Sequence=" + _packet.getSequence() +
+               ", Size=" + _packet.getData().limit();
     }
 }
