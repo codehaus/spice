6a6ed84fd8ce2b2be61365b3a4d5c9f3c9cc7126
nobody <nobody@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-03-22 00:27:54 +0000

    This commit was manufactured by cvs2svn to create tag 'EXTENSION_PRE_REMOVE-tag'.

--- spice/sandbox/repository/componenthaus/src/java/org/componenthaus/ant/NoInterfacesSpecifiedBuildException.java
+++
@@ -1,9 +0,0 @@
-package org.componenthaus.ant;
-
-import org.apache.tools.ant.BuildException;
-
-public class NoInterfacesSpecifiedBuildException extends BuildException {
-    public NoInterfacesSpecifiedBuildException() {
-        super("No interfaces specified to ant task");
-    }
-}
