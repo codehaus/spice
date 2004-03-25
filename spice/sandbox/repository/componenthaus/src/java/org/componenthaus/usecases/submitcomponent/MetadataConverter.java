54d0691b9481c8638a9e2d3376ce9ff42827d75a
nobody <nobody@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-03-25 01:17:35 +0000

    This commit was manufactured by cvs2svn to create tag 'ALCHEMIST_1_0-tag'.

--- spice/sandbox/repository/componenthaus/src/java/org/componenthaus/usecases/submitcomponent/MetadataConverter.java
+++
@@ -1,9 +0,0 @@
-package org.componenthaus.usecases.submitcomponent;
-
-import org.componenthaus.ant.metadata.ComponentMetadata;
-
-import java.util.Collection;
-
-public interface MetadataConverter {
-    Collection convert(ComponentMetadata componentMetadata);
-}
