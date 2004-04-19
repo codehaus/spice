11bf0aa857b8f42d6b9944a35567377368ee3efe
pdonald <pdonald@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-04-19 08:09:52 +0000

    Test toList() method

--- spice/sandbox/event/src/test/org/codehaus/spice/event/impl/collections/FifoBufferTestCase.java
+++ spice/sandbox/event/src/test/org/codehaus/spice/event/impl/collections/FifoBufferTestCase.java
@@ -7,17 +7,17 @@
  */
 package org.codehaus.spice.event.impl.collections;
 
+import java.util.List;
 import junit.framework.TestCase;
 
 /**
  * @author Peter Donald
- * @version $Revision: 1.1 $ $Date: 2003-12-16 02:03:12 $
+ * @version $Revision: 1.2 $ $Date: 2004-04-19 08:09:52 $
  */
 public class FifoBufferTestCase
     extends TestCase
 {
-    public void testNegativeSizedBuffer()
-        throws Exception
+    public void testNegativeSizedBuffer() throws Exception
     {
         try
         {
@@ -25,15 +25,13 @@ public class FifoBufferTestCase
         }
         catch( final IllegalArgumentException iae )
         {
-            assertEquals( "iae.getMessage()",
-                          "size <= 0", iae.getMessage() );
+            assertEquals( "iae.getMessage()", "size <= 0", iae.getMessage() );
             return;
         }
         fail( "Exected to fail due to negative buffer size " );
     }
 
-    public void testCreate()
-        throws Exception
+    public void testCreate() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         assertEquals( "buffer.size()", 0, buffer.size() );
@@ -41,11 +39,11 @@ public class FifoBufferTestCase
         assertEquals( "buffer.m_head", 0, buffer.m_head );
         assertEquals( "buffer.m_tail", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer",
-                      false, buffer.m_isWrappedBuffer );
+                      false,
+                      buffer.m_isWrappedBuffer );
     }
 
-    public void testSimpleAdd()
-        throws Exception
+    public void testSimpleAdd() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object = new Object();
@@ -54,19 +52,20 @@ public class FifoBufferTestCase
         assertEquals( "buffer.m_head", 0, buffer.m_head );
         assertEquals( "buffer.m_tail", 1, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer",
-                      false, buffer.m_isWrappedBuffer );
+                      false,
+                      buffer.m_isWrappedBuffer );
         assertEquals( "buffer.m_buffer[ 0 ]", object, buffer.m_buffer[ 0 ] );
         assertEquals( "buffer.add( object )", true, buffer.add( object ) );
         assertEquals( "buffer.size()", 2, buffer.size() );
         assertEquals( "buffer.m_head", 0, buffer.m_head );
         assertEquals( "buffer.m_tail", 2, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer",
-                      false, buffer.m_isWrappedBuffer );
+                      false,
+                      buffer.m_isWrappedBuffer );
         assertEquals( "buffer.m_buffer[ 1 ]", object, buffer.m_buffer[ 1 ] );
     }
 
-    public void testAddWithWrap()
-        throws Exception
+    public void testAddWithWrap() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object1 = new Object();
@@ -74,50 +73,45 @@ public class FifoBufferTestCase
         final Object object3 = new Object();
         final Object object4 = new Object();
 
-        assertEquals( "buffer.add( object1 )",
-                      true,
-                      buffer.add( object1 ) );
-        assertEquals( "buffer.add( object2 )",
-                      true,
-                      buffer.add( object2 ) );
-        assertEquals( "buffer.add( object3 )",
-                      true,
-                      buffer.add( object3 ) );
+        assertEquals( "buffer.add( object1 )", true, buffer.add( object1 ) );
+        assertEquals( "buffer.add( object2 )", true, buffer.add( object2 ) );
+        assertEquals( "buffer.add( object3 )", true, buffer.add( object3 ) );
         assertEquals( "buffer.size()", 3, buffer.size() );
         assertEquals( "buffer.m_head", 0, buffer.m_head );
         assertEquals( "buffer.m_tail", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
 
         assertEquals( "buffer.peek()", object1, buffer.peek() );
         assertEquals( "buffer.size() After Peek", 3, buffer.size() );
         assertEquals( "buffer.m_head After Peek", 0, buffer.m_head );
         assertEquals( "buffer.m_tail After Peek", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer After Peek",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
 
         assertEquals( "buffer.pop()", object1, buffer.pop() );
         assertEquals( "buffer.size() After Pop", 2, buffer.size() );
         assertEquals( "buffer.m_head After Pop", 1, buffer.m_head );
         assertEquals( "buffer.m_tail After Pop", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer After Pop",
-                      true, buffer.m_isWrappedBuffer );
-
-        assertEquals( "buffer.add( object4 )",
                       true,
-                      buffer.add( object4 ) );
+                      buffer.m_isWrappedBuffer );
+
+        assertEquals( "buffer.add( object4 )", true, buffer.add( object4 ) );
         assertEquals( "buffer.size() After Add", 3, buffer.size() );
         assertEquals( "buffer.m_head After Add", 1, buffer.m_head );
         assertEquals( "buffer.m_tail After Add", 1, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer After Add",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
         assertEquals( "buffer.m_buffer[ 0 ]", object4, buffer.m_buffer[ 0 ] );
         assertEquals( "buffer.m_buffer[ 1 ]", object2, buffer.m_buffer[ 1 ] );
         assertEquals( "buffer.m_buffer[ 2 ]", object3, buffer.m_buffer[ 2 ] );
     }
 
-    public void testSimpleAddAll()
-        throws Exception
+    public void testSimpleAddAll() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object = new Object();
@@ -129,32 +123,33 @@ public class FifoBufferTestCase
         assertEquals( "buffer.m_head", 0, buffer.m_head );
         assertEquals( "buffer.m_tail", 1, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer",
-                      false, buffer.m_isWrappedBuffer );
+                      false,
+                      buffer.m_isWrappedBuffer );
         assertEquals( "buffer.m_buffer[ 0 ]", object, buffer.m_buffer[ 0 ] );
     }
 
-    public void testFillingAddAll()
-        throws Exception
+    public void testFillingAddAll() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object1 = new Object();
         final Object object2 = new Object();
         final Object object3 = new Object();
         final Object[] objects = new Object[]{object1, object2, object3};
-        assertEquals( "buffer.addAll( objects )", true,
+        assertEquals( "buffer.addAll( objects )",
+                      true,
                       buffer.addAll( objects ) );
         assertEquals( "buffer.size()", 3, buffer.size() );
         assertEquals( "buffer.m_head", 0, buffer.m_head );
         assertEquals( "buffer.m_tail", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
         assertEquals( "buffer.m_buffer[ 0 ]", object1, buffer.m_buffer[ 0 ] );
         assertEquals( "buffer.m_buffer[ 1 ]", object2, buffer.m_buffer[ 1 ] );
         assertEquals( "buffer.m_buffer[ 2 ]", object3, buffer.m_buffer[ 2 ] );
     }
 
-    public void testAddAllWithWrap()
-        throws Exception
+    public void testAddAllWithWrap() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object1 = new Object();
@@ -162,34 +157,31 @@ public class FifoBufferTestCase
         final Object object3 = new Object();
         final Object object4 = new Object();
 
-        assertEquals( "buffer.add( object1 )",
-                      true,
-                      buffer.add( object1 ) );
-        assertEquals( "buffer.add( object2 )",
-                      true,
-                      buffer.add( object2 ) );
-        assertEquals( "buffer.add( object3 )",
-                      true,
-                      buffer.add( object3 ) );
+        assertEquals( "buffer.add( object1 )", true, buffer.add( object1 ) );
+        assertEquals( "buffer.add( object2 )", true, buffer.add( object2 ) );
+        assertEquals( "buffer.add( object3 )", true, buffer.add( object3 ) );
         assertEquals( "buffer.size()", 3, buffer.size() );
         assertEquals( "buffer.m_head", 0, buffer.m_head );
         assertEquals( "buffer.m_tail", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
 
         assertEquals( "buffer.peek()", object1, buffer.peek() );
         assertEquals( "buffer.size() After Peek", 3, buffer.size() );
         assertEquals( "buffer.m_head After Peek", 0, buffer.m_head );
         assertEquals( "buffer.m_tail After Peek", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer After Peek",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
 
         assertEquals( "buffer.pop()", object1, buffer.pop() );
         assertEquals( "buffer.size() After Pop", 2, buffer.size() );
         assertEquals( "buffer.m_head After Pop", 1, buffer.m_head );
         assertEquals( "buffer.m_tail After Pop", 0, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer After Pop",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
 
         assertEquals( "buffer.addAll( new Object[]{object4} )",
                       true,
@@ -198,15 +190,15 @@ public class FifoBufferTestCase
         assertEquals( "buffer.m_head After Add", 1, buffer.m_head );
         assertEquals( "buffer.m_tail After Add", 1, buffer.m_tail );
         assertEquals( "buffer.m_isWrappedBuffer After Add",
-                      true, buffer.m_isWrappedBuffer );
+                      true,
+                      buffer.m_isWrappedBuffer );
 
         assertEquals( "buffer.m_buffer[ 0 ]", object4, buffer.m_buffer[ 0 ] );
         assertEquals( "buffer.m_buffer[ 1 ]", object2, buffer.m_buffer[ 1 ] );
         assertEquals( "buffer.m_buffer[ 2 ]", object3, buffer.m_buffer[ 2 ] );
     }
 
-    public void testObjectsPassedIntoAddAll()
-        throws Exception
+    public void testObjectsPassedIntoAddAll() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         try
@@ -221,8 +213,7 @@ public class FifoBufferTestCase
         fail( "Expected a NPE when passing objects into AddAll" );
     }
 
-    public void testObjectPassedIntoAdd()
-        throws Exception
+    public void testObjectPassedIntoAdd() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         try
@@ -237,16 +228,32 @@ public class FifoBufferTestCase
         fail( "Expected a NPE when passing object into Add" );
     }
 
-    public void testPopOnEmpty()
-        throws Exception
+    public void testPopOnEmpty() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object element = buffer.pop();
         assertEquals( "element", null, element );
     }
 
-    public void testPopAndWrap()
-        throws Exception
+    public void testToList()
+    {
+        final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
+        final Object object1 = new Object();
+        final Object object2 = new Object();
+        final Object object3 = new Object();
+        buffer.add( object1 );
+        buffer.add( object2 );
+        buffer.add( object3 );
+
+        final List list = buffer.toList();
+
+        assertEquals( "list.size()", 3, list.size() );
+        assertEquals( "list.get(0)", object1, list.get( 0 ) );
+        assertEquals( "list.get(0)", object2, list.get( 1 ) );
+        assertEquals( "list.get(0)", object3, list.get( 2 ) );
+    }
+
+    public void testPopAndWrap() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object1 = new Object();
@@ -268,8 +275,7 @@ public class FifoBufferTestCase
         assertEquals( "element4", object4, element4 );
     }
 
-    public void testOverAddOnBoundedBuffer()
-        throws Exception
+    public void testOverAddOnBoundedBuffer() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object1 = new Object();
@@ -283,33 +289,37 @@ public class FifoBufferTestCase
         assertEquals( "buffer.add( object4 )", false, buffer.add( object4 ) );
     }
 
-    public void testOverAddAllOnBoundedBuffer()
-        throws Exception
+    public void testOverAddAllOnBoundedBuffer() throws Exception
     {
         final AbstractFifoBuffer buffer = new BoundedFifoBuffer( 3 );
         final Object object1 = new Object();
         final Object object2 = new Object();
         final Object object3 = new Object();
         final Object object4 = new Object();
-        final Object[] objects =
-            new Object[]{object1, object2, object3, object4};
+        final Object[] objects = new Object[]{object1,
+                                              object2,
+                                              object3,
+                                              object4};
 
-        assertEquals( "buffer.addAll( objects )", false,
+        assertEquals( "buffer.addAll( objects )",
+                      false,
                       buffer.addAll( objects ) );
     }
 
-    public void testOverAddAllOnUnBoundedBuffer()
-        throws Exception
+    public void testOverAddAllOnUnBoundedBuffer() throws Exception
     {
         final AbstractFifoBuffer buffer = new UnboundedFifoBuffer( 3 );
         final Object object1 = new Object();
         final Object object2 = new Object();
         final Object object3 = new Object();
         final Object object4 = new Object();
-        final Object[] objects =
-            new Object[]{object1, object2, object3, object4};
+        final Object[] objects = new Object[]{object1,
+                                              object2,
+                                              object3,
+                                              object4};
 
-        assertEquals( "buffer.addAll( objects )", true,
+        assertEquals( "buffer.addAll( objects )",
+                      true,
                       buffer.addAll( objects ) );
         assertEquals( "buffer.m_buffer.length", 5, buffer.m_buffer.length );
         assertEquals( "buffer.m_buffer[ 0 ]", object1, buffer.m_buffer[ 0 ] );
