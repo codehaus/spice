77a3b229d09cf0b9d9ac52e33a0a41d72c3689f9
nobody <nobody@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-04-25 00:44:47 +0000

    This commit was manufactured by cvs2svn to create tag 'LOGGERSTORE_1_0-tag'.

---
+++ tags/LOGGERSTORE_1_0-tag/spice/components/loggerstore/src/java/org/codehaus/spice/loggerstore/factories/PropertyLog4JLoggerStoreFactory.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.codehaus.spice.loggerstore.factories;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import org.codehaus.spice.loggerstore.LoggerStore;
+import org.codehaus.spice.loggerstore.stores.Log4JLoggerStore;
+
+/**
+ * PropertyLog4JLoggerStoreFactory is an implementation of LoggerStoreFactory
+ * for the Log4J Logger using a property configuration resource.
+ *
+ * @author Mauro Talevi
+ * @author Peter Donald
+ * @version $Revision: 1.3 $ $Date: 2004-02-28 21:13:22 $
+ */
+public class PropertyLog4JLoggerStoreFactory
+    extends AbstractLoggerStoreFactory
+{
+    /**
+     * Creates a LoggerStore from a given set of configuration parameters.
+     *
+     * @param config the Map of parameters for the configuration of the store
+     * @return the LoggerStore
+     * @throws Exception if unable to create the LoggerStore
+     */
+    protected LoggerStore doCreateLoggerStore( final Map config )
+        throws Exception
+    {
+        final Properties properties = (Properties)config.get(
+            Properties.class.getName() );
+        if( null != properties )
+        {
+            return new Log4JLoggerStore( properties );
+        }
+
+        final InputStream resource = getInputStream( config );
+        if( null != resource )
+        {
+            return new Log4JLoggerStore(
+                createPropertiesFromStream( resource ) );
+        }
+
+        return missingConfiguration();
+    }
+
+    private Properties createPropertiesFromStream( final InputStream resource )
+        throws Exception
+    {
+        final Properties properties = new Properties();
+        properties.load( resource );
+        return properties;
+    }
+}
