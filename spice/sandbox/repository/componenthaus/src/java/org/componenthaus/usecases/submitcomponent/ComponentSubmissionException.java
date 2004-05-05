4190840a82b7bfa590b553714784abb6be935a05
nobody <nobody@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-05-05 22:59:03 +0000

    This commit was manufactured by cvs2svn to create tag 'ALCHEMIST_1_1-tag'.

--- spice/sandbox/repository/componenthaus/src/java/org/componenthaus/usecases/submitcomponent/ComponentSubmissionException.java
+++
@@ -1,7 +0,0 @@
-package org.componenthaus.usecases.submitcomponent;
-
-public class ComponentSubmissionException extends Exception {
-    public ComponentSubmissionException(String message, Exception e) {
-        super(message,e);
-    }
-}
