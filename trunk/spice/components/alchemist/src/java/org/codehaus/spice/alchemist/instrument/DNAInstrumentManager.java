ad1a860f3dec8a71ab7eb00970e70ad1d1601b68
mauro <mauro@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-06-17 22:26:53 +0000

    Changed to work to interfaces.

--- trunk/spice/components/alchemist/src/java/org/codehaus/spice/alchemist/instrument/DNAInstrumentManager.java
+++ trunk/spice/components/alchemist/src/java/org/codehaus/spice/alchemist/instrument/DNAInstrumentManager.java
@@ -1,96 +1,99 @@
 /*
- * Copyright (C) The Loom Group. All rights reserved.
- *
- * This software is published under the terms of the Loom
- * Software License version 1.1, a copy of which has been included
- * with this distribution in the LICENSE.txt file.
+ * Copyright (C) The Spice Group. All rights reserved. This software is
+ * published under the terms of the Spice Software License version 1.1, a copy
+ * of which has been included with this distribution in the LICENSE.txt file.
  */
 package org.codehaus.spice.alchemist.instrument;
 
 import org.apache.excalibur.instrument.InstrumentManager;
 import org.apache.excalibur.instrument.Instrumentable;
-import org.apache.excalibur.instrument.manager.DefaultInstrumentManager;
 import org.codehaus.dna.Active;
 import org.codehaus.dna.Configurable;
 import org.codehaus.dna.Configuration;
 import org.codehaus.dna.ConfigurationException;
 import org.codehaus.dna.LogEnabled;
 import org.codehaus.dna.Logger;
+import org.codehaus.spice.alchemist.activity.ActivityAlchemist;
 import org.codehaus.spice.alchemist.configuration.ConfigurationAlchemist;
 import org.codehaus.spice.alchemist.logger.LoggerAlchemist;
 
-
 /**
- * DNA facade for the Excalibur <code>DefaultInstrumentManager</code>
- *
+ * DNA facade for the Excalibur <code>InstrumentManager</code>.
+ * It assumes the InstrumentManager honours the Avalon lifecycle methods
+ * and attempts to honour the DNA lifecycle methods via the appropriate
+ * Alchemist.
+ * 
  * @author Johan Sjoberg
  * @author Mauro Talevi
- * @version $Revision: 1.1 $ $Date: 2004-06-13 13:35:14 $
+ * @version $Revision: 1.2 $ $Date: 2004-06-17 22:26:53 $
  */
-public class DNAInstrumentManager 
-    implements InstrumentManager, LogEnabled, Configurable, Active
-{
+public class DNAInstrumentManager implements InstrumentManager, LogEnabled,
+        Configurable, Active {
 
-    /** The Excalibur DefaultInstrumentManager */
-    private final DefaultInstrumentManager _manager;
-    
+    /** The Excalibur InstrumentManager */
+    private final InstrumentManager _manager;
+
     /**
      * Create a DNAInstrumentManager
-     *
-     * @param manager the Excalibur DefaultInstrumentManager
+     * 
+     * @param manager the Excalibur InstrumentManager
      */
-    public DNAInstrumentManager( final DefaultInstrumentManager manager )
-    {
+    public DNAInstrumentManager( final InstrumentManager manager ) {
         _manager = manager;
     }
 
     /**
      * @see LogEnabled#enableLogging(Logger)
      */
-     public void enableLogging( Logger logger )
-     {
-         _manager.enableLogging( LoggerAlchemist.toAvalonLogger( logger ) );
-     }
-
+    public void enableLogging( Logger logger ) {
+        if ( LoggerAlchemist.isAvalonLogEnabled( _manager ) ) {
+            LoggerAlchemist.toAvalonLogEnabled( _manager ).enableLogging(
+                    LoggerAlchemist.toAvalonLogger( logger ) );
+        }
+    }
 
     /**
      * @see Configurable#configure(Configuration)
      */
-    public void configure( Configuration configuration ) throws ConfigurationException
-    {
-        try
-        {
-            _manager.configure( ConfigurationAlchemist.toAvalonConfiguration( configuration ) );
-        }
-        catch( Exception e )
-        {
+    public void configure( Configuration configuration )
+            throws ConfigurationException {
+        try {
+            ConfigurationAlchemist.toAvalonConfigurable( _manager )
+                    .configure( ConfigurationAlchemist
+                            .toAvalonConfiguration( configuration ) );
+        } catch ( Exception e ) {
             throw new ConfigurationException( e.getMessage(), e );
         }
     }
 
-
     /**
      * @see org.codehaus.dna.Active#initialize()
      */
-    public void initialize() throws Exception
-    {
-        _manager.initialize();
+    public void initialize() throws Exception {
+        try {
+            ActivityAlchemist.toAvalonInitializable( _manager ).initialize();
+        } catch ( IllegalArgumentException e ){
+            // _manager is not Avalon Initialisable
+        }
     }
-
 
     /**
      * @see org.codehaus.dna.Active#dispose()
      */
-    public void dispose()
-    {
-        _manager.dispose();
+    public void dispose() {
+        try {
+            ActivityAlchemist.toAvalonDisposable( _manager ).dispose();
+        } catch ( IllegalArgumentException e ){
+            // _manager is not Avalon Disposable
+        }
     }
 
-
     /**
-     * @see org.apache.excalibur.instrument.InstrumentManager#registerInstrumentable(org.apache.excalibur.instrument.Instrumentable, java.lang.String)
+     * @see org.apache.excalibur.instrument.InstrumentManager#registerInstrumentable(org.apache.excalibur.instrument.Instrumentable,
+     *      java.lang.String)
      */
-    public void registerInstrumentable( Instrumentable arg0, String arg1 ) throws Exception {
+    public void registerInstrumentable( Instrumentable arg0, String arg1 )
+            throws Exception {
         _manager.registerInstrumentable( arg0, arg1 );
     }
 }
