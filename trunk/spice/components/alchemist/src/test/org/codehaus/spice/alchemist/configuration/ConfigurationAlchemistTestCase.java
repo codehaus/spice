343d11019acadfafbc0b93835db15c44dc11d362
mauro <mauro@be3e68a6-5752-0410-bb16-fdda5dde7748>
2004-06-20 17:05:21 +0000

    Completed test coverage for configuration package.

--- trunk/spice/components/alchemist/src/test/org/codehaus/spice/alchemist/configuration/ConfigurationAlchemistTestCase.java
+++ trunk/spice/components/alchemist/src/test/org/codehaus/spice/alchemist/configuration/ConfigurationAlchemistTestCase.java
@@ -43,25 +43,93 @@ public class ConfigurationAlchemistTestCase
         }
     }
 
-    public void testAvalonConfiguration()
+    public void testAvalonConfigurationWithChild()
     	throws Exception
    	{
         org.apache.avalon.framework.configuration.Configuration configuration =
-            ConfigurationAlchemist.toAvalonConfiguration( new DefaultConfiguration( "name", "location", "") );
-        assertEquals( "configuration name", "name", configuration.getName() );
-        assertEquals( "configuration location", "location", configuration.getLocation() );
-        assertEquals( "configuration namespace", "", configuration.getNamespace() );
+            ConfigurationAlchemist.toAvalonConfiguration( createDNAConfigurationWithChild() );
+        assertEquals( "name", "element", configuration.getName() );
+        assertEquals( "location", "location", configuration.getLocation() );
+        assertEquals( "namespace", "", configuration.getNamespace() );
+        assertEquals( "attribute 1", "value1", configuration.getAttribute( "key1" ) );
+        assertEquals( "attribute 2", "value2", configuration.getAttribute( "key2" ) );
+        assertEquals( "child name", "child", configuration.getChild( "child" ).getName() );        
    	}
+
+    public void testAvalonConfigurationWithValue()
+    	throws Exception
+	{
+	    org.apache.avalon.framework.configuration.Configuration configuration =
+	        ConfigurationAlchemist.toAvalonConfiguration( createDNAConfigurationWithValue() );
+	    assertEquals( "name", "element", configuration.getName() );
+	    assertEquals( "location", "location", configuration.getLocation() );
+	    assertEquals( "namespace", "", configuration.getNamespace() );
+	    assertEquals( "attribute 1", "value1", configuration.getAttribute( "key1" ) );
+	    assertEquals( "attribute 2", "value2", configuration.getAttribute( "key2" ) );
+	    assertEquals( "value", "value", configuration.getValue( null ) );
+	}
+
+    private Configuration createDNAConfigurationWithChild() {
+        final DefaultConfiguration configuration =
+            new DefaultConfiguration( "element", "location", "" );
+        configuration.setAttribute( "key1", "value1" );
+        configuration.setAttribute( "key2", "value2" );
+        configuration.addChild( new DefaultConfiguration( "child", "location", "/element" ) );
+        return configuration;
+    }    
+
+    private Configuration createDNAConfigurationWithValue() {
+        final DefaultConfiguration configuration =
+            new DefaultConfiguration( "element", "location", "" );
+        configuration.setAttribute( "key1", "value1" );
+        configuration.setAttribute( "key2", "value2" );
+        configuration.setValue( "value" );
+        return configuration;
+    }    
     
-    public void testDNAConfiguration()
+    public void testDNAConfigurationWithChild()
     	throws Exception
 	{
         Configuration configuration = ConfigurationAlchemist.toDNAConfiguration( 
-                new org.apache.avalon.framework.configuration.DefaultConfiguration( "name", "location") );
-        assertEquals( "configuration name", "name", configuration.getName() );
-        assertEquals( "configuration location", "location", configuration.getLocation() );
-        assertEquals( "configuration path", "", configuration.getPath() );
+                createAvalonConfigurationWithChild() );
+        assertEquals( "name", "element", configuration.getName() );
+        assertEquals( "location", "location", configuration.getLocation() );
+        assertEquals( "path", "", configuration.getPath() );
+        assertEquals( "attribute 1", "value1", configuration.getAttribute( "key1" ) );
+        assertEquals( "attribute 2", "value2", configuration.getAttribute( "key2" ) );
+        assertEquals( "child name", "child", configuration.getChild( "child" ).getName() );        
 	}
+    
+    public void testDNAConfigurationWithValue()
+    	throws Exception
+	{
+	    Configuration configuration = ConfigurationAlchemist.toDNAConfiguration( 
+	            createAvalonConfigurationWithValue() );
+	    assertEquals( "name", "element", configuration.getName() );
+	    assertEquals( "location", "location", configuration.getLocation() );
+	    assertEquals( "path", "", configuration.getPath() );
+	    assertEquals( "attribute 1", "value1", configuration.getAttribute( "key1" ) );
+	    assertEquals( "attribute 2", "value2", configuration.getAttribute( "key2" ) );
+	    assertEquals( "value", "value", configuration.getValue( null ) );
+	}
+    
+    private org.apache.avalon.framework.configuration.Configuration createAvalonConfigurationWithChild() {
+        final org.apache.avalon.framework.configuration.DefaultConfiguration configuration =
+            new org.apache.avalon.framework.configuration.DefaultConfiguration( "element", "location" );
+        configuration.setAttribute( "key1", "value1" );
+        configuration.setAttribute( "key2", "value2" );
+        configuration.addChild( new org.apache.avalon.framework.configuration.DefaultConfiguration( "child", "location" ) );
+        return configuration;
+    }    
+
+    private org.apache.avalon.framework.configuration.Configuration createAvalonConfigurationWithValue() {
+        final org.apache.avalon.framework.configuration.DefaultConfiguration configuration =
+            new org.apache.avalon.framework.configuration.DefaultConfiguration( "element", "location" );
+        configuration.setAttribute( "key1", "value1" );
+        configuration.setAttribute( "key2", "value2" );
+        configuration.setValue( "value" );
+        return configuration;
+    }    
     
     public void testIsAvalonConfigurable()
     {
