041a6c113ce946cdfd9a5e61b72c8a9ce2e4df79
proyal <proyal@be3e68a6-5752-0410-bb16-fdda5dde7748>
2005-03-02 19:32:08 +0000

    first cut of message routing component

---
+++ trunk/spice/components/message/src/java/org/spice/message/DestinationEventHandler.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.spice.message;
+
+import org.d_haven.event.EventHandler;
+
+/**
+ * EventHandler that passes messages on to a Destination
+ *
+ * @author <a href="mailto:peter.royal@pobox.com">peter royal</a>
+ */
+class DestinationEventHandler implements EventHandler
+{
+    private final Destination m_destination;
+
+    public DestinationEventHandler( final Destination destination )
+    {
+        m_destination = destination;
+    }
+
+    public void handleEvent( final Object element )
+    {
+        m_destination.deliver( element );
+    }
+
+    public void handleEvents( final Object[] elements )
+    {
+        for( int i = 0; i < elements.length; i++ )
+        {
+            handleEvent( elements[i] );
+        }
+    }
+}
---
+++ trunk/spice/components/message/src/java/org/spice/message/MessageException.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.spice.message;
+
+/**
+ * @author <a href="mailto:peter.royal@pobox.com">peter royal</a>
+ */
+public class MessageException extends Exception
+{
+    public MessageException( final String message )
+    {
+        super( message );
+    }
+}
