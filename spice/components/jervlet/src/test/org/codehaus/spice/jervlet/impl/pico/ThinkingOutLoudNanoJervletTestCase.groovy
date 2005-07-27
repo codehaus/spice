37cc4cbb9deef2e1bb42963ecab292984771fe5c
paul <paul@be3e68a6-5752-0410-bb16-fdda5dde7748>
2005-07-27 03:36:56 +0000

    wqork in progress

---
+++ spice/components/jervlet/src/test/org/codehaus/spice/jervlet/impl/pico/ThinkingOutLoudNanoJervletTestCase.groovy
@@ -0,0 +1,28 @@
+package org.codehaus.spice.jervlet.impl.pico
+
+//import org.codehaus.spice.jervlet.impl.pico.NanoGroovyWebContainerBuilder
+//import org.picocontainer.defaults.DefaultPicoContainer
+
+class ThinkingOutLoudNanoJervletTestCase extends GroovyTestCase {
+
+    void testSomething() {
+
+//        pc = new DefaultPicoContainer()
+//        pc.registerComponentInstance(String.class, "Foo")
+
+//        builder = new NanoGroovyWebContainerBuilder()
+
+//        webServer = builder.picoEnabledWebServer(pico:pc, host:"localhost", port:8099) {
+//            webApp(warPath:"foo/bar")
+//            webApp {
+//              servlet(context:"/foo", servletClass:"foo.bar.AServlet")
+//            }
+//        }
+
+//        webServer.start()
+
+//        assertEquals("?","?")
+    }
+
+
+}
